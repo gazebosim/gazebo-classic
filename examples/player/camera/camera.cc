ee1d3fa3c5dd97898f487fb71c22f49f00b0412b
natepak <devnull@localhost>
2008-03-13 16:43:53 +0000

    Added camera example

---
+++ examples/player/camera/camera.cc
@@ -0,0 +1,25 @@
+#include <libplayerc++/playerc++.h>
+#include <iostream>
+
+int main()
+{
+  // We throw exceptions on creation if we fail
+  try
+  {
+    using namespace PlayerCc;
+    using namespace std;
+
+    // Create a player client object, using the variables assigned by the
+    // call to parse_args()
+    PlayerClient robot(PlayerCc::PLAYER_HOSTNAME, PlayerCc::PLAYER_PORTNUM);
+
+    // Subscribe to the camera proxy 
+    CameraProxy cp(&robot, 0);
+
+  }
+  catch (PlayerCc::PlayerError e)
+  {
+    std::cerr << "Error:" << e << std::endl;
+    return -1;
+  }
+}
