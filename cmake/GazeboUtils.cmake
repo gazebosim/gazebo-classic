3a77521f3fbfb4cbf34eb2d443934bd72db58ca3
hsujohnhsu <devnull@localhost>
2009-05-13 22:55:12 +0000

    comment out STRING(STRIP...) for cmake 2.4.

--- cmake/GazeboUtils.cmake
+++ cmake/GazeboUtils.cmake
@@ -6,7 +6,7 @@ MACRO (APPEND_TO_CACHED_STRING _string _cacheDesc)
   FOREACH (newItem ${ARGN})
     SET (${_string} "${${_string}} ${newItem}" CACHE STRING ${_cacheDesc} FORCE)
   ENDFOREACH (newItem ${ARGN})
-  STRING(STRIP ${${_string}} ${_string})
+  #STRING(STRIP ${${_string}} ${_string})
 ENDMACRO (APPEND_TO_CACHED_STRING)
                  
 ################################################################################
@@ -58,7 +58,7 @@ MACRO (LIST_TO_STRING _string _list)
     FOREACH (_item ${_list})
       SET (${_string} "${${_string}} ${_item}")
     ENDFOREACH (_item)
-    STRING(STRIP ${${_string}} ${_string})
+    #STRING(STRIP ${${_string}} ${_string})
 ENDMACRO (LIST_TO_STRING)
 
 ###############################################################################
