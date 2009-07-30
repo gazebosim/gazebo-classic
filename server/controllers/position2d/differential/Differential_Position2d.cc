316fa786ce26dc60bed13148074fe9de46a8813e
hsujohnhsu <devnull@localhost>
2009-07-30 02:33:46 +0000

    comment out debug message.

--- server/controllers/position2d/differential/Differential_Position2d.cc
+++ server/controllers/position2d/differential/Differential_Position2d.cc
@@ -195,7 +195,7 @@ void Differential_Position2d::UpdateChild()
     this->joints[LEFT]->SetParam( dParamFMax, **(this->torqueP) );
     this->joints[RIGHT]->SetParam( dParamFMax, **(this->torqueP) );
 
-    printf("Set Speed[%f %f]\n",this->wheelSpeed[LEFT], this->wheelSpeed[RIGHT]);
+    //printf("Set Speed[%f %f]\n",this->wheelSpeed[LEFT], this->wheelSpeed[RIGHT]);
   }
   /*else
   {
