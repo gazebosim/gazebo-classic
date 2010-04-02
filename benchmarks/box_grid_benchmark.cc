f88366ae7cf1d7428d810d4ea46a27320c53e2b1
natepak <devnull@localhost>
2010-04-02 15:07:45 +0000

    Added benchmarks

---
+++ benchmarks/box_grid_benchmark.cc
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <gazebo/gazebo.h>
+
+gazebo::Client *client = NULL;
+gazebo::SimulationIface *simIface = NULL;
+gazebo::FactoryIface *factoryIface = NULL;
+int laser_count = 0;
+
+std::string test_name="Box Grid Benchmark";
+std::string xlabel = "Box Count";
+std::string ylabel = "Simtime / Realtime";
+std::string data_filename = "/tmp/" + test_name + ".data";
+
+void make_plot()
+{
+  std::ostringstream cmd;
+
+  cmd << "echo \"";
+  cmd << "set xlabel '" << xlabel << "'\n";
+  cmd << "set ylabel '" << ylabel << "'\n";
+  cmd << "set title '" << test_name << "'\n";
+  cmd << "set terminal png\n";
+  cmd << "set output '" << test_name << ".png'\n";
+  cmd << "plot '" << data_filename << "' with lines\n";
+  cmd << "\" | gnuplot";
+
+  if (system(cmd.str().c_str() ) < 0)
+    std::cerr << "Error\n";
+}
+
+void spawn_box(double x, double y, double z=.25)
+{
+  std::ostringstream model;
+
+  model << "<model:physical name='box" << laser_count++ << "'>";
+  model << "  <xyz>" << x << " " << y << " " << z << "</xyz>";
+  model << "  <rpy>0 0 0</rpy>";
+  model << "  <body:box name='body'>";
+
+  model << "    <geom:box name='geom'>";
+  model << "      <size>0.5 0.5 0.5</size>";
+  model << "      <mass>1</mass>";
+  model << "      <visual>";
+  model << "        <size>0.5 0.5 0.5</size>";
+  model << "        <mesh>unit_box</mesh>";
+  model << "        <material>Gazebo/Rocky</material>";
+  model << "      </visual>";
+  model << "    </geom:box>";
+
+  model << "  </body:box>";
+  model << "</model:physical>";
+
+  factoryIface->Lock(1);
+  strcpy( (char*)factoryIface->data->newModel, model.str().c_str() );
+  factoryIface->Unlock();
+}
+
+int main()
+{
+  client = new gazebo::Client();
+  simIface = new gazebo::SimulationIface();
+  factoryIface = new gazebo::FactoryIface();
+
+  try
+  {
+    client->ConnectWait(0, GZ_CLIENT_ID_USER_FIRST);
+  }
+  catch(std::string e)
+  {
+    std::cerr << "Gazebo Error: Unable to connect: " << e << "\n";
+    return -1;
+  }
+
+  /// Open the sim iface
+  try
+  {
+    simIface->Open(client, "default");
+  }
+  catch (std::string e)
+  {
+    std::cerr << "Gazebo error: Unable to connect to sim iface:" << e << "\n";
+    return -1;
+  }
+
+  // Open the factory iface
+  try
+  {
+    factoryIface->Open(client, "default");
+  }
+  catch( std::string e)
+  {
+    std::cerr << "Gazebo error: Unable to connect to the factory Iface:" 
+              << e << "\n";
+    return -1;
+  }
+
+  FILE *out = fopen(data_filename.c_str(), "w");
+
+  // 400 blocks in a grid patter
+  for (int y=-10; y < 10; y++)
+  {
+    for ( int x=-10; x < 10; x++)
+    {
+      spawn_box(x, y);
+      double simTime = 0;
+      double realTime = 0;
+
+      for (unsigned int i=0; i < 30; i++)
+      {
+        simTime += simIface->data->simTime;
+        realTime += simIface->data->realTime;
+
+        /// Wait .1 seconds 
+        usleep(100000);
+      }
+
+      double percent = simTime / realTime;
+      fprintf(out,"%f\n",percent);
+      std::cout << "Index[" << y <<  " " << x <<  "] " << percent << "\n";
+    }
+  }
+
+  fclose(out);
+
+  make_plot();
+
+  factoryIface->Close();
+  simIface->Close();
+  client->Disconnect();
+}
---
+++ benchmarks/box_pile_benchmark.cc
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <gazebo/gazebo.h>
+
+gazebo::Client *client = NULL;
+gazebo::SimulationIface *simIface = NULL;
+gazebo::FactoryIface *factoryIface = NULL;
+int laser_count = 0;
+
+std::string test_name="Boxes Benchmark";
+std::string xlabel = "Box Count";
+std::string ylabel = "Simtime / Realtime";
+std::string data_filename = "/tmp/" + test_name + ".data";
+
+void make_plot()
+{
+  std::ostringstream cmd;
+
+  cmd << "echo \"";
+  cmd << "set xlabel '" << xlabel << "'\n";
+  cmd << "set ylabel '" << ylabel << "'\n";
+  cmd << "set title '" << test_name << "'\n";
+  cmd << "set terminal png\n";
+  cmd << "set output '" << test_name << ".png'\n";
+  cmd << "plot '" << data_filename << "' with lines\n";
+  cmd << "\" | gnuplot";
+
+  if (system(cmd.str().c_str() ) < 0)
+    std::cerr << "Error\n";
+}
+
+void spawn_box(double x, double y, double z=10)
+{
+  std::ostringstream model;
+
+  model << "<model:physical name='box" << laser_count++ << "'>";
+  model << "  <xyz>" << x << " " << y << " " << z << "</xyz>";
+  model << "  <rpy>30 40 0</rpy>";
+  model << "  <body:box name='body'>";
+
+  model << "    <geom:box name='geom'>";
+  model << "      <size>0.5 0.5 0.5</size>";
+  model << "      <mass>1</mass>";
+  model << "      <visual>";
+  model << "        <size>0.5 0.5 0.5</size>";
+  model << "        <mesh>unit_box</mesh>";
+  model << "        <material>Gazebo/Rocky</material>";
+  model << "      </visual>";
+  model << "    </geom:box>";
+
+  model << "  </body:box>";
+  model << "</model:physical>";
+
+  factoryIface->Lock(1);
+  strcpy( (char*)factoryIface->data->newModel, model.str().c_str() );
+  factoryIface->Unlock();
+}
+
+int main()
+{
+  client = new gazebo::Client();
+  simIface = new gazebo::SimulationIface();
+  factoryIface = new gazebo::FactoryIface();
+
+  try
+  {
+    client->ConnectWait(0, GZ_CLIENT_ID_USER_FIRST);
+  }
+  catch(std::string e)
+  {
+    std::cerr << "Gazebo Error: Unable to connect: " << e << "\n";
+    return -1;
+  }
+
+  /// Open the sim iface
+  try
+  {
+    simIface->Open(client, "default");
+  }
+  catch (std::string e)
+  {
+    std::cerr << "Gazebo error: Unable to connect to sim iface:" << e << "\n";
+    return -1;
+  }
+
+  // Open the factory iface
+  try
+  {
+    factoryIface->Open(client, "default");
+  }
+  catch( std::string e)
+  {
+    std::cerr << "Gazebo error: Unable to connect to the factory Iface:" 
+              << e << "\n";
+    return -1;
+  }
+
+  FILE *out = fopen(data_filename.c_str(), "w");
+
+  for (unsigned int j=0; j < 400; j++)
+  {
+    spawn_box(0, 0);
+
+    double simTime = 0;
+    double realTime = 0;
+
+    for (unsigned int i=0; i < 50; i++)
+    {
+      simTime += simIface->data->simTime;
+      realTime += simIface->data->realTime;
+
+      /// Wait .1 seconds 
+      usleep(100000);
+    }
+
+    double percent = simTime / realTime;
+    fprintf(out,"%f\n",percent);
+
+    std::cout << "Index[" << j << "] = " << percent << "\n";
+  }
+  fclose(out);
+
+  make_plot();
+
+  factoryIface->Close();
+  simIface->Close();
+  client->Disconnect();
+}
