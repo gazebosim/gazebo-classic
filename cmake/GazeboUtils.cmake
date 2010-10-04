cdaaf28968f47e254bdb29f2dc479c6b6dcfc283
natepak <devnull@localhost>
2010-10-04 16:53:48 +0000

    Cleaned up the build messages

--- cmake/GazeboUtils.cmake
+++ cmake/GazeboUtils.cmake
@@ -76,11 +76,20 @@ ENDMACRO (LIST_TO_STRING)
 
 macro (BUILD_ERROR)
   foreach (str ${ARGN})
-    SET (msg "ERROR: ${str}" )
+    SET (msg "\t${str}" )
     MESSAGE (STATUS ${msg} )
     APPEND_TO_CACHED_LIST(build_errors "build errors" ${msg})
   endforeach (str ${ARGN})
 endmacro (BUILD_ERROR)
+
+macro (BUILD_WARNING)
+  foreach (str ${ARGN})
+    SET (msg "\t${str}" )
+    MESSAGE (STATUS ${msg} )
+    APPEND_TO_CACHED_LIST(build_warnings "build warning" ${msg})
+  endforeach (str ${ARGN})
+endmacro (BUILD_WARNING)
+
 
 ###############################################################################
 # Reset lists
