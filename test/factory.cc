0fb737c41745d196fa0b3870ba4ccff9c4031de1
Nate Koenig <nkoenig@willowgarage.com>
2011-05-25 17:47:09 -0700

    Cleaned up the factory test case

--- test/factory.cc
+++ test/factory.cc
@@ -54,15 +54,40 @@ const std::string sphere_xml =
   </link>\
 </model>";
 
+const std::string cylinder_xml = 
+"<?xml version='1.0'?>\
+<model name='factory_cylinder_test'>\
+  <origin xyz='2.89366 2.99339 1.57'/>\
+  <link name='body'>\
+    <collision name='geom'>\
+      <geometry>\
+        <box size='1.78731 1.98679 3.14'/>\
+      </geometry>\
+      <mass>0.5</mass>\
+    </collision>\
+    <visual>\
+      <geometry>\
+        <box size='1.78731 1.98679 3.14'/>\
+      </geometry>\
+      <material name='Gazebo/Grey'/>\
+      <cast_shadows>true</cast_shadows>\
+      <shader>pixel</shader>\
+    </visual>\
+  </link>\
+</model>";
+
+
 
 std::string model_name;
 
 boost::mutex mutex;
 boost::condition_variable cond;
+bool triggered;
 
 void OnNewEntity(const boost::shared_ptr<msgs::Entity const> &msg)
 {
   model_name = msg->name();
+  triggered = true;
   cond.notify_one();
 }
 
@@ -85,56 +110,55 @@ TEST(Factory, BoxCreate)
   transport::PublisherPtr pub = node->Advertise<msgs::Factory>("~/factory");
   transport::SubscriberPtr sub = node->Subscribe("~/new_entity", OnNewEntity);
 
+  //------------------ Test Box Creation --------------------------------
   msgs::Factory msg;
   ASSERT_NO_THROW(common::Message::Init(msg, "new_box"));
   msg.set_xml( box_xml );
 
+  triggered = false;
   // Publish the message
   ASSERT_NO_THROW(pub->Publish(msg));
 
   // Wait for the new entity message
-  cond.timed_wait(lock, td);
+  if (!triggered)
+    cond.timed_wait(lock, td);
  
   // Check the right model was created 
   ASSERT_STREQ(model_name.c_str(), "factory_box_test");
-  
-  ASSERT_NO_THROW(combined->Stop());
 
-  delete combined;
-}
 
-TEST(Factory, SphereCreate)
-{
-  Combined *combined = NULL;
-
-  boost::unique_lock<boost::mutex> lock(mutex);
-  boost::posix_time::time_duration td = boost::posix_time::seconds(1);
-
-  ASSERT_NO_THROW(combined = new Combined());
-  ASSERT_NO_THROW(combined->Load(""));
-  ASSERT_NO_THROW(combined->Init());
-
-  combined->Start();
-
-  transport::NodePtr node = transport::NodePtr( new transport::Node() );
-  ASSERT_NO_THROW(node->Init("default"));
-
-  transport::PublisherPtr pub = node->Advertise<msgs::Factory>("~/factory");
-  transport::SubscriberPtr sub = node->Subscribe("~/new_entity", OnNewEntity);
-
-  msgs::Factory msg;
-  ASSERT_NO_THROW(common::Message::Init(msg, "new_box"));
+  //------------------ Test Sphere Creation --------------------------------
+  ASSERT_NO_THROW(common::Message::Init(msg, "new_sphere"));
   msg.set_xml( sphere_xml );
+
+  triggered = false;
 
   // Publish the message
   ASSERT_NO_THROW(pub->Publish(msg));
 
   // Wait for the new entity message
-  cond.timed_wait(lock, td);
+  if (!triggered)
+    cond.timed_wait(lock, td);
  
   // Check the right model was created 
   ASSERT_STREQ(model_name.c_str(), "factory_sphere_test");
-  
+
+
+  //------------------ Test Cylinder Creation --------------------------------
+  ASSERT_NO_THROW(common::Message::Init(msg, "new_cylinder"));
+  msg.set_xml( cylinder_xml );
+
+  triggered = false;
+  // Publish the message
+  ASSERT_NO_THROW(pub->Publish(msg));
+
+  // Wait for the new entity message
+  if (!triggered)
+    cond.timed_wait(lock, td);
+ 
+  // Check the right model was created 
+  ASSERT_STREQ(model_name.c_str(), "factory_cylinder_test");
+
   ASSERT_NO_THROW(combined->Stop());
 
   delete combined;
