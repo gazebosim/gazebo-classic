46cddd876a7bcc38a02a9a4aa59b076db7908907
Nate Koenig <nkoenig@willowgarage.com>
2011-07-13 10:17:07 -0700

    Refined gzsdf checking utility

--- tools/check_sdf.cc
+++ tools/check_sdf.cc
@@ -1,32 +1,66 @@
 
 #include "sdf/parser/parser.hh"
 
+std::vector<std::string> params;
+
 using namespace sdf;
+
+void help()
+{
+  std::cout << "gzsdf is a command-line tool for getting information about SDF files.\n\n";
+  std::cout << "gzsdf <option> [sdf file]\n\n";
+  std::cout << "Commands:\n";
+  std::cout << "\tgzsdf describe\tPrint the SDF format\n";
+  std::cout << "\tgzsdf check\tCheck the SDF format for the given file\n\n";
+}
+
 int main(int argc, char** argv)
 {
-  if (argc < 2)
+  // Get parameters from command line
+  for (int i=1; i < argc; i++)
   {
-    std::cerr << "Expect xml file to parse" << std::endl;
-    return -1;
+    std::string p = argv[i];
+    boost::trim(p);
+    params.push_back( p );
   }
 
   boost::shared_ptr<SDF> sdf(new SDF());
-
   if (!init(sdf))
   {
     std::cerr << "ERROR: SDF parsing the xml failed" << std::endl;
     return -1;
   }
-  
-  if (!readFile(argv[1],sdf))
-  {
-    std::cerr << "Error: SDF parsing the xml failed\n";
-    return -1;
-  }
-  
 
-//  sdf->PrintDescription();
-  sdf->PrintValues();
+  if (params.size() == 0 || params[0] == "help" || params[0] == "h")
+  {
+    help();
+  }
+  else if (params[0] == "check")
+  {
+    if (params.size() < 2)
+    {
+      std::cerr << "Error: Expecting an xml file to parse\n\n";
+      help();
+      return -1;
+    }
+
+    if (!readFile(params[1], sdf))
+    {
+      std::cerr << "Error: SDF parsing the xml failed\n";
+      return -1;
+    }
+    std::cout << "Check complete\n";
+    //sdf->PrintValues();
+  }
+  else if (params[0] == "describe")
+  {
+    sdf->PrintDescription();
+  }
+  else
+  {
+    std::cerr << "Error: Unknown option[" << params[0] << "]\n";
+    help();
+  }
 
   return 0;
 }
