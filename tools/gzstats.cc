04496402216ce3a34b86e2806202665b780204a8
Nate Koenig <nkoenig@willowgarage.com>
2011-07-21 09:06:53 -0700

    Added a gzstats tool

---
+++ tools/gzstats.cc
@@ -0,0 +1,45 @@
+#include <google/protobuf/message.h>
+
+#include "transport/Transport.hh"
+#include "transport/TransportTypes.hh"
+#include "transport/Node.hh"
+
+#include "gazebo_config.h"
+
+using namespace gazebo;
+
+void cb(const boost::shared_ptr<msgs::WorldStatistics const> &_msg)
+{
+  double percent = 0;
+  char paused;
+  common::Time simTime  = msgs::Convert( _msg->sim_time() );
+  common::Time realTime = msgs::Convert( _msg->real_time() );
+
+  if (_msg->paused())
+    paused = 'T';
+  else
+    paused = 'F';
+
+  if (simTime.Double() > 0)
+    percent  = (simTime / realTime).Double();
+
+  printf("Factor[%4.2f] SimTime[%4.2f] RealTime[%4.2f] Paused[%c]\n",
+      percent, simTime.Double(), realTime.Double(), paused);
+}
+
+int main(int /*argc*/, char ** /*argv*/)
+{
+  transport::init();
+
+  transport::NodePtr node(new transport::Node());
+  node->Init("");
+
+  std::string topic = "/gazebo/default/world_stats";
+
+  transport::SubscriberPtr sub = node->Subscribe(topic, cb);
+  transport::run();
+  while(true)
+    usleep(10000);
+
+  transport::fini();
+}
