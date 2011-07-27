834ef527479d6865e59957be9665193be3d38bf9
Nate Koenig <nkoenig@willowgarage.com>
2011-07-27 15:41:33 -0700

    Added world_edit example plugin

---
+++ plugins/world_edit.cc
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2011 Nate Koenig & Andrew Howard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#include <boost/bind.hpp>
+#include "physics/physics.h"
+#include "transport/transport.h"
+#include "msgs/msgs.h"
+#include "gazebo.h"
+
+namespace gazebo
+{
+  class WorldEdit : public Plugin
+  {
+    public: WorldEdit() : Plugin() {}
+
+    public: void Load( sdf::ElementPtr &_sdf )
+    {
+      transport::NodePtr node(new transport::Node());
+      node->Init(_sdf->GetWorldName());
+     
+      transport::PublisherPtr scenePub = node->Advertise<msgs::Scene>("~/scene");
+      transport::PublisherPtr physicsPub = node->Advertise<msgs::Physics>("~/physics");
+
+      // Set the ambient color to red
+      msgs::Scene sceneMsg;
+      msgs::Init(sceneMsg);
+      sceneMsg.mutable_ambient()->set_r(1.0);
+      sceneMsg.mutable_ambient()->set_g(0.0);
+      sceneMsg.mutable_ambient()->set_b(0.0);
+      sceneMsg.mutable_ambient()->set_a(1.0);
+      scenePub->Publish( sceneMsg );
+
+      msgs::Physics physicsMsg;
+      msgs::Init(physicsMsg);
+      physicsMsg.set_type(msgs::Physics::ODE);
+      physicsMsg.set_dt(0.01);
+      physicsPub->Publish( physicsMsg );
+    }
+  };
+
+  // Register this plugin with the simulator
+  GZ_REGISTER_PLUGIN(WorldEdit)
+}
