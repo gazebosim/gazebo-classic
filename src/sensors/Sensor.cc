c0561099469728b217c40dcf05bb96110968595c
Nate Koenig <nkoenig@willowgarage.com>
2011-10-19 07:42:55 -0700

    Added set sensor update rate from sdf

--- src/sensors/Sensor.cc
+++ src/sensors/Sensor.cc
@@ -74,6 +74,8 @@ void Sensor::Load()
 /// Initialize the sensor
 void Sensor::Init()
 {
+  this->SetUpdateRate( this->sdf->GetValueDouble("update_rate")  );
+
   // Load the plugins
   if (this->sdf->HasElement("plugin"))
   {
