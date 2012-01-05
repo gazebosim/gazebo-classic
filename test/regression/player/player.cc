74f17e31fd7fc472d0ca8feaeaae17e949118957
nkoenig <devnull@localhost>
2012-01-04 21:16:42 -0800

    Added player test

---
+++ test/regression/player/player.cc
@@ -0,0 +1,14 @@
+#include <libplayerc++/playerc++.h>
+
+int main(int /*argc*/, char ** /*argv*/)
+{
+  PlayerCc::PlayerClient client(PlayerCc::PLAYER_HOSTNAME,
+                                PlayerCc::PLAYER_PORTNUM);
+  PlayerCc::SimulationProxy sp(&client, 0);
+  PlayerCc::Position2dProxy pp(&client, 0);
+
+  sp.SetPose2d("box_model1",1,1,0);
+  sp.SetPose3d("box_model1",-1,-1,1,0,0,0.707);
+
+  return 1;
+}
