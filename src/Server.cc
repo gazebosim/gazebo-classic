bdee5e4cc13292b616043b764fbb0cda20911994
Nate Koenig <nkoenig@willowgarage.com>
2012-02-20 09:34:36 -0800

    Moved plugin examples

--- src/Server.cc
+++ src/Server.cc
@@ -62,6 +62,12 @@ void Server::LoadPlugin(const std::string &_filename)
 
 bool Server::Load(const std::string &_filename)
 {
+  // Quick test for a valid file
+  FILE *test = fopen(_filename.c_str(), "r");
+  if (!test)
+    return false;
+  fclose(test);
+
   std::string host = "";
   unsigned int port = 0;
 
