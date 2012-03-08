ed26aceb51b9de332528b13703b0d256c081b890
Nate Koenig <nkoenig@willowgarage.com>
2012-03-08 05:55:35 -0800

    Improvements to cmake

--- cmake/gazebo_cpack.cmake
+++ cmake/gazebo_cpack.cmake
@@ -7,6 +7,8 @@ if ("${CMAKE_SYSTEM}" MATCHES "Linux")
   if (EXISTS ${DPKG_PROGRAM})
     list (APPEND CPACK_GENERATOR "DEB")
   endif(EXISTS ${DPKG_PROGRAM})
+
+  find_program(RPMBUILD_PROGRAM rpmbuild)
 endif()
 
 list (APPEND CPACK_SOURCE_GENERATOR "TBZ2")
@@ -18,29 +20,6 @@ include (InstallRequiredSystemLibraries)
 #execute_process(COMMAND dpkg --print-architecture _NPROCE)
 set (DEBIAN_PACKAGE_DEPENDS "libogre-dev, libfreeimage-dev, libqt4-dev, libprotobuf-dev, libprotoc-dev, libtbb2, libxml2, libboost-all-dev")
 
+set (RPM_PACKAGE_DEPENDS "libogre-dev, libfreeimage-dev, libqt4-dev, libprotobuf-dev, libprotoc-dev, libtbb2, libxml2, libboost-all-dev")
+
 set (GAZEBO_CPACK_CFG_FILE "${PROJECT_BINARY_DIR}/cpack_options.cmake")
-
-################################################################################
-# Make the CPack input file
-#macro(GAZEBO_MAKE_CPACK_INPUT)
-#  set(_cpack_cfg_in "${gazebo_cmake_dir}/cpack_options.cmake.in")
-#
-#  #Prepare the components list
-#  #GAZEBO_CPACK_MAKE_COMPS_OPTS(GAZEBO_CPACK_COMPONENTS "${_comps}")
-#
-#  configure_file(${_cpack_cfg_in} ${GAZEBO_CPACK_CFG_FILE} @ONLY)
-#endmacro(GAZEBO_MAKE_CPACK_INPUT)
-
-
-#macro(GAZEBO_CPACK_MAKE_COMPS_OPTS _var _current)
-#    set(_comps_list)
-#    foreach(_ss ${GAZEBO_SUBSYSTEMS})
-#        GAZEBO_GET_SUBSYS_STATUS(_status ${_ss})
-#        if(_status)
-#            set(_comps_list "${_comps_list} ${_ss}")
-#            GAZEBO_CPACK_ADD_COMP_INFO(${_var} ${_ss})
-#        endif(_status)
-#    endforeach(_ss)
-#    set(${_var} "${${_var}}\nset(CPACK_COMPONENTS_ALL${_comps_list})\n")
-#endmacro(GAZEBO_CPACK_MAKE_COMPS_OPTS)
-
