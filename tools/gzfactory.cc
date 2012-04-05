ed0c9eaca7ff258fc0a7e883cdddf78faa3b27c7
Nate Koenig <nkoenig@willowgarage.com>
2012-04-05 08:13:32 -0700

    More gazebo tool updates

--- tools/gzfactory.cc
+++ tools/gzfactory.cc
@@ -15,11 +15,14 @@
  *
 */
 #include <transport/transport.h>
+#include <boost/program_options.hpp>
 #include <fstream>
 #include <string>
 
 using namespace gazebo;
+namespace po = boost::program_options;
 
+/////////////////////////////////////////////////
 void help()
 {
   std::cerr << "This tool spawns models into a running Gazebo simulation.\n\n"
@@ -29,15 +32,51 @@ void help()
             << "    modle_filename : Filename of the SDF model.\n\n";
 }
 
+/////////////////////////////////////////////////
 int main(int argc, char **argv)
 {
-  if (argc < 3)
+  po::options_description v_desc("Allowd options");
+  v_desc.add_options()
+    ("help,h", "produce this help message")
+    ("sdf,f", po::value<std::string>(), "SDF model file")
+    ("pose-x,x", po::value<double>(), "set model x position.")
+    ("pose-x,y", po::value<double>(), "set model y position.")
+    ("pose-x,z", po::value<double>(), "set model z positione.")
+    ("pose-x,r", po::value<double>(), "set model roll orientation in radians.")
+    ("pose-x,p", po::value<double>(), "set model pitch orientation in radians.")
+    ("pose-x,y", po::value<double>(), "set model yaw orientation in radians.");
+
+  po::options_description h_desc("Hidden options");
+  h_desc.add_options()
+    ("command", po::value<std::string>(), "<spawn|delete>")
+    ("model-name", po::value<std::string>(), "overwrite SDF name of model.");
+  
+  po::options_description desc("Allowed options");
+  desc.add(v_desc).add(h_desc);
+
+  po::positional_options_description p_desc;
+  p_desc.add("command", 1);
+  p_desc.add("model-name", 2);
+
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc,
+        argv).options(desc).positional(p_desc).run(), vm);
+  po::notify(vm);
+
+  if (vm.count("help") || argc < 2)
   {
     help();
+    std::cout << v_desc << "\n";
     return -1;
   }
 
-  std::ifstream ifs(argv[2]);
+  return 0;
+
+  std::ifstream ifs;
+  if (vm.count("input-filename"))
+    ifs.open(vm["input-filename"].as<std::string>().c_str());
+
+
   if (!ifs)
   {
     printf("Error: Unable to open file[%s]\n", argv[2]);
