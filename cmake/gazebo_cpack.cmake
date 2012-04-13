83070e65c943f2ad1b188ad07ed4c49a05462bad
Nathan Koenig <nkoenig@willowgarage.com>
2012-04-13 15:43:36 -0700

    Updates to cmake

--- cmake/gazebo_cpack.cmake
+++ cmake/gazebo_cpack.cmake
@@ -11,8 +11,7 @@ if ("${CMAKE_SYSTEM}" MATCHES "Linux")
   find_program(RPMBUILD_PROGRAM rpmbuild)
 endif()
 
-list (APPEND CPACK_SOURCE_GENERATOR "TBZ2")
-list (APPEND CPACK_SOURCE_GENERATOR "ZIP")
+list (APPEND CPACK_SOURCE_GENERATOR "TGZ")
 list (APPEND CPACK_SOURCE_IGNORE_FILES ";Ogre.log;TODO;/.hg/;.swp$;/build/")
 
 include (InstallRequiredSystemLibraries)
