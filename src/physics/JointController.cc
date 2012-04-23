9853606a351484d7d9e277923dfe87fbd70be9f6
Nate Koenig <nkoenig@willowgarage.com>
2012-04-23 10:47:39 -0700

    Added start of built-in joint controller for models

---
+++ src/physics/JointController.cc
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2011 Nate Koenig & Andrew Howard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include "transport/Node.hh"
+#include "transport/Subscriber.hh"
+#include "physics/Model.hh"
+#include "physics/World.hh"
+#include "physics/Joint.hh"
+#include "physics/JointController.hh"
+
+using namespace gazebo;
+using namespace physics;
+
+/////////////////////////////////////////////////
+JointController::JointController(ModelPtr _model)
+  : model(_model)
+{
+  this->node = transport::NodePtr(new transport::Node());
+  this->node->Init(this->model->GetWorld()->GetName());
+
+  this->jointCmdSub = this->node->Subscribe(std::string("~/") +
+      this->model->GetName() + "/joint_cmd",
+      &JointController::OnJointCmd, this);
+}
+
+/////////////////////////////////////////////////
+void JointController::AddJoint(JointPtr _joint)
+{
+  this->joints.push_back(_joint);
+}
+
+/////////////////////////////////////////////////
+void JointController::OnJointCmd(ConstJointCmdPtr &_msg)
+{
+}
---
+++ src/physics/JointController.hh
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2011 Nate Koenig & Andrew Howard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#ifndef JOINTCONTROLLER_HH
+#define JOINTCONTROLLER_HH
+
+#include "physics/PhysicsTypes.hh"
+#include "transport/TransportTypes.hh"
+#include "msgs/msgs.h"
+
+namespace gazebo
+{
+  namespace physics
+  {
+    class JointController
+    {
+      public: JointController(ModelPtr _model);
+      public: void AddJoint(JointPtr _joint);
+
+      private: void OnJointCmd(ConstJointCmdPtr &_msg);
+
+      private: ModelPtr model;
+      private: std::vector<JointPtr> joints;
+      private: transport::NodePtr node;
+      private: transport::SubscriberPtr jointCmdSub;
+    };
+  }
+}
+#endif
