e1bf5fc9f4f517fe03ad85ae752fc44f83e2a851
nkoenig <devnull@localhost>
2012-08-02 11:28:55 -0700

    removing old filess

--- Media/materials/programs/deferred_rendering/deferred_shading/ShadowCaster_vp.glsl
+++
@@ -1,8 +0,0 @@
-uniform mat4 worldViewProj;
-uniform mat4 worldView;
-
-void main()
-{
-  gl_Position = worldViewProj * gl_Vertex;
-  gl_TexCoord[0].xyz = (worldView * gl_Vertex).xyz;
-}
--- Media/materials/programs/laser_2nd_pass.vert
+++
@@ -1,8 +0,0 @@
-varying float tex;
-
-void main()
-{
-  gl_Position = ftransform();
-  tex = gl_Vertex.x;
-  gl_TexCoord[0] = gl_MultiTexCoord0;
-}
