b33ff349f2bc6cbe677b72f87ae4f05eea35963a
John Hsu <johnhsu@willowgarage.com>
2012-09-28 14:00:33 -0700

    adding set_pose test

---
+++ test/regression/set_pose.cc
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2011 Nate Koenig & Andrew Howard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include "ServerFixture.hh"
+#include "physics/physics.hh"
+
+using namespace gazebo;
+class PhysicsTest : public ServerFixture
+{
+};
+
+
+TEST_F(PhysicsTest, State)
+{
+  // intentionally break the joint using Link::SetWorldPose
+  // let it conflict with Physics pose updates and make sure
+  // internal model state stays consistent
+
+  Load("worlds/empty.world");
+  physics::WorldPtr world = physics::get_world("default");
+  EXPECT_TRUE(world != NULL);
+
+  physics::WorldState worldState = world->GetState();
+  physics::ModelState modelState = worldState.GetModelState(0);
+  physics::LinkState linkState = modelState.GetLinkState(0);
+
+
+
+
+
+  Unload();
+}
+
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
