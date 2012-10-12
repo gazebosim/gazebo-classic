4d5a5b0b0980849c2895e8a29b4f412b219d2a9b
John Hsu <johnhsu@willowgarage.com>
2012-10-12 16:23:53 -0700

    fixed forked killing of child processes when running gazebo.  changes were: catch first wait() return value, and mark that process as dead so we don't try to kill it again.  also, switched to execvp on gzserver and gzclient.

--- gazebo/gazebo_main.cc
+++ gazebo/gazebo_main.cc
@@ -21,14 +21,47 @@
 #include "Server.hh"
 #include "gui/Gui.hh"
 
-int status;
+bool sig_killed = false;
+int status1, status2;
 pid_t  pid1, pid2;
+bool killed1 = false;
+bool killed2 = false;
 
 void sig_handler(int /*signo*/)
 {
-  //kill(pid1, SIGINT);
-  //kill(pid2, SIGINT);
-  gzerr << "main siging\n";
+  sig_killed = true;
+  kill(pid1, SIGINT);
+  kill(pid2, SIGINT);
+  // wait some time and if not dead, escalate to SIGKILL
+  for(unsigned int i = 0; i < 5; ++i)
+  {
+    if(!killed1)
+    {
+      int p1 = waitpid(pid1, &status1, WNOHANG);
+      if (p1 == pid1)
+        killed1 = true;
+    }
+    if(!killed2)
+    {
+      int p2 = waitpid(pid2, &status2, WNOHANG);
+      if (p2 == pid2)
+        killed2 = true;
+    }
+    if(killed1 && killed2)
+      break;
+    /// @todo: fix hardcoded timeout
+    sleep(1);
+  }
+  if (!killed1)
+  {
+    gzwarn << "escalating to SIGKILL on server\n";
+    kill(pid1, SIGKILL);
+  }
+  if (!killed2)
+  {
+    gzwarn << "escalating to SIGKILL on client\n";
+    kill(pid2, SIGKILL);
+  }
 }
 
 int main(int _argc, char **_argv)
@@ -42,61 +75,45 @@ int main(int _argc, char **_argv)
 
   pid1 = fork();
 
+  char** myargv = new char*[_argc+1];
+  for(int i=0;i<_argc;i++)
+    myargv[i] = _argv[i];
+  myargv[_argc] = (char*)NULL;
+
   if (pid1)
   {
     pid2 = fork();
     if (pid2)
     {
-      // if anyone child dies
-      wait(&status);
-
-      // signal to kill both children
-      //kill(pid1, SIGINT);
-      //kill(pid2, SIGINT);
-
-      // wait 5 seconds
-      for (unsigned int i = 0; i < 5; ++i)
-      {
-        gzerr << waitpid(pid1, &status, WNOHANG) << "\n";
-        gzerr << waitpid(pid2, &status, WNOHANG) << "\n";
-        if (waitpid(pid1, &status, WNOHANG) == -1 &&
-            waitpid(pid2, &status, WNOHANG) == -1)
-          break;
-        sleep(1);
-      }
-
-      // escalate to sigkill if needed
-      if (waitpid(pid1, &status, WNOHANG) == 0)
-      {
-        gzwarn << "SIGKILL server\n";
-        kill(pid1, SIGKILL);
-      }
-      if (waitpid(pid2, &status, WNOHANG) == 0)
-      {
-        gzwarn << "SIGKILL gui\n";
-        kill(pid2, SIGKILL);
-      }
-      gzerr << "done main process\n";
+      pid_t dead_child = wait(&status1);
+      if(dead_child == pid1)
+        killed1 = true;
+      else if(dead_child == pid2)
+        killed2 = true;
+      // one of the children died
+      if (!sig_killed)
+        sig_handler(SIGINT);
     }
     else
     {
-      // pid2 in parent process
-      gazebo::gui::run(_argc, _argv);
-      gzerr << "done gui\n";
+      //gazebo::gui::run(_argc, _argv);
+      // gzclient argv
+      execvp("gzclient", myargv);
     }
   }
   else
   {
-    // pid1 in parent process
-    gazebo::Server *server = new gazebo::Server();
-    if (!server->ParseArgs(_argc, _argv))
-      return -1;
-    server->Run();
-    server->Fini();
-    delete server;
-    server = NULL;
-    gzerr << "done server\n";
+    //gazebo::Server *server = new gazebo::Server();
+    //if (!server->ParseArgs(_argc, _argv))
+      //return -1;
+    //server->Run();
+    //server->Fini();
+    //delete server;
+    //server = NULL;
+    execvp("gzserver", myargv);
   }
+  
+  delete[] myargv;
 
-  return 1;
+  return 0;
 }
