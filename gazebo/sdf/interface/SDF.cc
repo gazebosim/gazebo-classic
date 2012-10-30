5b246b2bc3c685c6920e91749e43c609c72d3af2
John Hsu <johnhsu@willowgarage.com>
2012-10-30 09:59:46 -0700

    add check so AddElement will add child element only if child element is required.

--- gazebo/sdf/interface/SDF.cc
+++ gazebo/sdf/interface/SDF.cc
@@ -733,7 +733,11 @@ ElementPtr Element::AddElement(const std::string &_name)
       for (iter2 = elem->elementDescriptions.begin();
            iter2 != elem->elementDescriptions.end(); ++iter2)
       {
-        elem->AddElement((*iter2)->name);
+        // add only required child element
+        if ((*iter2)->GetRequired() == "1" || (*iter2)->GetRequired() == "+")
+        {
+          elem->AddElement((*iter2)->name);
+        }
       }
 
       return this->elements.back();
