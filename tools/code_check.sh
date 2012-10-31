6ce6b3bf635a8a4dca18571c94ff1aff937cb0fc
Brian Gerkey <gerkey@osrfoundation.org>
2012-10-30 21:39:51 -0700

    change cppcheck invocation to work with Jenkins

--- tools/code_check.sh
+++ tools/code_check.sh
@@ -9,7 +9,7 @@ echo "*:gazebo/common/STLLoader.cc:126" >> /tmp/gazebo_cpp_check.suppress
 echo "*:gazebo/common/STLLoader.cc:149" >> /tmp/gazebo_cpp_check.suppress
 
 #cppcheck
-find ./gazebo ./tools ./plugins ./examples ./test/regression ./interfaces -name "*.cc" -print0 | xargs -0 cppcheck --enable=all -q --suppressions-list=/tmp/gazebo_cpp_check.suppress
+cppcheck --enable=all -q --suppressions-list=/tmp/gazebo_cpp_check.suppress --xml `find ./gazebo ./tools ./plugins ./examples ./test/regression ./interfaces -name "*.cc"`
 
 # cpplint
 find ./gazebo ./tools ./plugins ./examples ./test/regression ./interfaces -print0 -name "*.cc" -o -name "*.hh" -o -name "*.c" -o -name "*.h" | xargs -0 python tools/cpplint.py 2>&1
