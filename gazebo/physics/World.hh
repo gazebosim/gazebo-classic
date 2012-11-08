a52e6c4c893f3ecfa6c87376a3eede9e565d00ab
Nate Koenig <natekoenig@gmail.com>
2012-11-08 08:24:41 -0800

    Fixed the build

--- gazebo/physics/World.hh
+++ gazebo/physics/World.hh
@@ -593,6 +593,12 @@ namespace gazebo
 
       /// \brief sleep timing error offset due to clock wake up latency
       private: common::Time sleepOffset;
+
+      /// \brief Last time incoming messages were processed.
+      private: common::Time prevProcessMsgsTime;
+
+      /// \brief Period over which messages should be processed.
+      private: common::Time processMsgsPeriod;
     };
     /// \}
   }
