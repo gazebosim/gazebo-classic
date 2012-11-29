044e85b78a01cdaf10f917cba93d225a1657ae31
Steve Peters <scpeters@osrfoundation.org>
2012-11-29 10:29:30 -0800

    Previous code from issue_164, still has problem with static objects

--- gazebo/physics/Link.cc
+++ gazebo/physics/Link.cc
@@ -157,6 +157,10 @@ void Link::Load(sdf::ElementPtr _sdf)
 //////////////////////////////////////////////////
 void Link::Init()
 {
+  // Set pose for this object before its children
+  this->SetRelativePose(this->sdf->GetValuePose("pose"));
+  this->SetInitialRelativePose(this->sdf->GetValuePose("pose"));
+
   Base_V::iterator iter;
   for (iter = this->children.begin(); iter != this->children.end(); ++iter)
   {
@@ -231,10 +235,6 @@ void Link::Init()
   }*/
 
   this->enabled = true;
-
-  // DO THIS LAST!
-  this->SetRelativePose(this->sdf->GetValuePose("pose"));
-  this->SetInitialRelativePose(this->sdf->GetValuePose("pose"));
 }
 
 //////////////////////////////////////////////////
