80857a03c3ed8c8ab0d2f06fc893366164d7e4e3
Nate Koenig <natekoenig@gmail.com>
2012-12-03 16:05:29 -0800

    Added a comment

--- gazebo/common/Image.hh
+++ gazebo/common/Image.hh
@@ -35,31 +35,33 @@ namespace gazebo
 {
   namespace common
   {
-
     /// \addtogroup gazebo_common Common
     /// \{
 
-    static std::string PixelFormatNames[] = {
-                "UNKNOWN_PIXEL_FORMAT",
-                "L_INT8",
-                "L_INT16",
-                "RGB_INT8",
-                "RGBA_INT8",
-                "BGRA_INT8",
-                "RGB_INT16",
-                "RGB_INT32",
-                "BGR_INT8",
-                "BGR_INT16",
-                "BGR_INT32",
-                "R_FLOAT16",
-                "RGB_FLOAT16",
-                "R_FLOAT32",
-                "RGB_FLOAT32",
-                "BAYER_RGGB8",
-                "BAYER_RGGR8",
-                "BAYER_GBRG8",
-                "BAYER_GRBG8"
-              };
+    /// \brief String names for the pixel formats.
+    /// \sa Image::PixelFormat.
+    static std::string PixelFormatNames[] =
+    {
+      "UNKNOWN_PIXEL_FORMAT",
+      "L_INT8",
+      "L_INT16",
+      "RGB_INT8",
+      "RGBA_INT8",
+      "BGRA_INT8",
+      "RGB_INT16",
+      "RGB_INT32",
+      "BGR_INT8",
+      "BGR_INT16",
+      "BGR_INT32",
+      "R_FLOAT16",
+      "RGB_FLOAT16",
+      "R_FLOAT32",
+      "RGB_FLOAT32",
+      "BAYER_RGGB8",
+      "BAYER_RGGR8",
+      "BAYER_GBRG8",
+      "BAYER_GRBG8"
+    };
 
     /// \class Image Image.hh common/common.hh
     /// \brief Encapsulates an image
