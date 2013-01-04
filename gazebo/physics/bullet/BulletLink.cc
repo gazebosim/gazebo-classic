018df04d7afbf885066778358b869ad63cb41fa8
Steve Peters <scpeters@osrfoundation.org>
2013-01-04 10:43:47 -0800

    Give zero mass to static objects, fixes most of SpawnDropBullet

--- gazebo/physics/bullet/BulletLink.cc
+++ gazebo/physics/bullet/BulletLink.cc
@@ -65,6 +65,10 @@ void BulletLink::Init()
   Link::Init();
 
   btScalar btMass = this->inertial->GetMass();
+  // The bullet dynamics solver checks for zero mass to identify static and
+  // kinematic bodies.
+  if (this->IsStatic() || this->GetKinematic())
+    btMass = 0;
   btVector3 fallInertia(0, 0, 0);
   math::Vector3 cogVec = this->inertial->GetCoG();
 
