83a71202db4d879841d8573986c2412533073c69
Nate Koenig <natekoenig@gmail.com>
2013-01-11 14:45:35 -0800

    Allow multiple uri elements for a material script

--- gazebo/gui/BoxMaker.cc
+++ gazebo/gui/BoxMaker.cc
@@ -40,7 +40,7 @@ BoxMaker::BoxMaker()
   this->state = 0;
   this->visualMsg = new msgs::Visual();
   this->visualMsg->mutable_geometry()->set_type(msgs::Geometry::BOX);
-  this->visualMsg->mutable_material()->mutable_script()->set_uri(
+  this->visualMsg->mutable_material()->mutable_script()->add_uri(
       "gazebo://media/materials/scripts/gazebo.material");
   this->visualMsg->mutable_material()->mutable_script()->set_name(
       "Gazebo/TurquoiseGlowOutline");
