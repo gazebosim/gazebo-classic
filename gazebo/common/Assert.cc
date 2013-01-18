5fdd2e91b576305b701b486df8e74eee4d12ac3c
Nate Koenig <natekoenig@gmail.com>
2013-01-18 08:20:29 -0800

    Style fixes

--- gazebo/common/Assert.cc
+++ gazebo/common/Assert.cc
@@ -1,19 +1,37 @@
+/*
+ * Copyright 2012 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
 #include "Assert.hh"
 #include "common/Exception.hh"
 
 namespace boost
 {
-  void assertion_failed(char const * expr, char const * function,
-                        char const * file, long line)
+  void assertion_failed(char const *_expr, char const *_function,
+                        char const *_file, long _line)
   {
-    throw gazebo::common::AssertionInternalError(file, line, expr, function);
+    throw gazebo::common::AssertionInternalError(_file, _line,
+                                                 _expr, _function);
   }
 
 
-  void assertion_failed_msg(char const * expr, char const * msg,
-                            char const * function, char const * file,
-                            long line)
+  void assertion_failed_msg(char const *_expr, char const *_msg,
+                            char const *_function, char const *_file,
+                            long _line)
   {
-    throw gazebo::common::AssertionInternalError(file, line, expr, function, msg);
+    throw gazebo::common::AssertionInternalError(_file, _line, _expr,
+                                                 _function, _msg);
   }
 }
--- gazebo/common/Assert.hh
+++ gazebo/common/Assert.hh
@@ -1,5 +1,21 @@
-#ifndef _ASSERT_HH
-#define _ASSERT_HH 1
+/*
+ * Copyright 2012 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#ifndef _ASSERT_HH_
+#define _ASSERT_HH_ 1
 
 #include <boost/assert.hpp>
 
@@ -11,6 +27,6 @@ namespace boost
 }
 */
 
-#define GZB_ASSERT(expr, msg) BOOST_ASSERT_MSG(expr, msg)
+#define GZB_ASSERT(_expr, _msg) BOOST_ASSERT_MSG(_expr, _msg)
 
 #endif
