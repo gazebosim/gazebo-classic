bbbc173a8d92dd0b16fc102c5622f576d16a5241
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-01-25 19:50:14 +0100

    Use other glut code that does not show a blocking window

--- tools/gl-test.py
+++ tools/gl-test.py
@@ -1,8 +1,21 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python
 from OpenGL.GLUT import *
 import sys
 
 glutInit(sys.argv)
-glutCreateWindow("dri")
-glutMainLoop()
+
+# Select type of Display mode:
+# Double buffer
+# RGBA color
+# Depth buffer
+# Alpha blending
+glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH | GLUT_ALPHA)
+
+# get a 640 x 480 window
+glutInitWindowSize(640, 480)
+
+# the window starts at the upper left corner of the screen
+glutInitWindowPosition(0, 0)
+
+# Open a window
+window = glutCreateWindow("Testing DRI")
