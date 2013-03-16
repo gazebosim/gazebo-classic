f9d1d2878c02c85dd3edf375bd5de8abac6d54a2
John Hsu <hsu@osrfoundation.org>
2013-03-16 13:28:53 -0700

    added test to Joint.cc for checking memory leak

---
+++ gazebo/physics/Joint_TEST.cc
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2012 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <gtest/gtest.h>
+#include "gazebo/physics/physics.hh"
+#include "test/ServerFixture.hh"
+#include "gazebo/physics/Joint.hh"
+
+#define TOL 1e-6
+using namespace gazebo;
+
+class Joint_TEST : public ServerFixture
+{
+};
+
+////////////////////////////////////////////////////////////////////////
+// Test world template
+////////////////////////////////////////////////////////////////////////
+TEST_F(Joint_TEST, JointCreationDestructionTest)
+{
+  // Load our inertial test world
+  Load("worlds/joint_test.world", true);
+
+  // Get a pointer to the world, make sure world loads
+  physics::WorldPtr world = physics::get_world("default");
+  ASSERT_TRUE(world != NULL);
+
+  // Verify physics engine type
+  physics::PhysicsEnginePtr physics = world->GetPhysicsEngine();
+  ASSERT_TRUE(physics != NULL);
+  EXPECT_EQ(physics->GetType(), "ode");
+
+  // create some fake links
+  physics::ModelPtr model = world->GetModel("model_1");
+  ASSERT_TRUE(model != NULL);
+  physics::LinkPtr link = model->GetLink("link_1");
+  ASSERT_TRUE(link != NULL);
+
+  physics::LinkPtr parentLink;
+  physics::LinkPtr childLink(link);
+  physics::JointPtr joint;
+  math::Pose anchor;
+  math::Vector3 axis(1, 0, 0);
+  double upper = M_PI;
+  double lower = -M_PI;
+  for (unsigned int i = 0; i < 100; ++i)
+  {
+    // try creating a joint
+    {
+      joint = world->GetPhysicsEngine()->CreateJoint(
+        "revolute", model);
+      joint->Attach(parentLink, childLink);
+      // load adds the joint to a vector of shared pointers kept
+      // in parent and child links, preventing joint from being destroyed.
+      joint->Load(parentLink, childLink, anchor);
+      // joint->SetAnchor(0, anchor);
+      joint->SetAxis(0, axis);
+      joint->SetHighStop(0, upper);
+      joint->SetLowStop(0, lower);
+
+      if (parentLink)
+        joint->SetName(parentLink->GetName() + std::string("_") +
+                       childLink->GetName() + std::string("_joint"));
+      else
+        joint->SetName(std::string("world_") +
+                       childLink->GetName() + std::string("_joint"));
+      joint->Init();
+    }
+    // remove the joint
+    {
+      bool paused = world->IsPaused();
+      world->SetPaused(true);
+      if (joint)
+      {
+        // reenable collision between the link pair
+        physics::LinkPtr parent = joint->GetParent();
+        physics::LinkPtr child = joint->GetChild();
+        if (parent)
+          parent->SetCollideMode("all");
+        if (child)
+          child->SetCollideMode("all");
+
+        joint->Detach();
+        joint.reset();
+      }
+      world->SetPaused(paused);
+    }
+  }
+
+
+  Unload();
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
