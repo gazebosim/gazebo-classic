90fd908b44d23d007b9654f1ac179cc2240930ba
Steve Peters <scpeters@osrfoundation.org>
2013-03-20 10:52:38 -0700

    Print default values for elements in sdf documentation

--- gazebo/sdf/interface/SDF.cc
+++ gazebo/sdf/interface/SDF.cc
@@ -382,7 +382,12 @@ void Element::PrintDocRightPane(std::string &_html, int _spacing, int &_index)
 
   stream << "<font style='font-weight:bold'>Type: </font>";
   if (this->value)
-    stream << this->value->GetTypeName() << "\n";
+  {
+    stream << this->value->GetTypeName()
+           << "&nbsp;&nbsp;&nbsp;\n"
+           << "<font style='font-weight:bold'>Default: </font>"
+           << this->value->GetDefaultAsString() << '\n';
+  }
   else
     stream << "n/a\n";
 
