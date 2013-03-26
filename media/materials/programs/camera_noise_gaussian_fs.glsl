a1497f85eb55a7dcd808899f01b87d99b2a85219
Ian Chen <ichen@osrfoundation.org>
2013-03-25 18:55:09 -0700

    Get compositor working

--- media/materials/programs/camera_noise_gaussian_fs.glsl
+++ media/materials/programs/camera_noise_gaussian_fs.glsl
@@ -1,9 +1,10 @@
 //uniform float mean;
 //uniform float sigma;
-
-void main() 
+uniform sampler2D RT;
+void main()
 {
   //gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
   // Just pass through
-  gl_FragColor = gl_Color;
+  //gl_FragColor = gl_Color;
+  gl_FragColor = texture2D(RT, gl_TexCoord[0].xy);
 }
--- media/materials/programs/camera_noise_gaussian_vs.glsl
+++ media/materials/programs/camera_noise_gaussian_vs.glsl
@@ -1,5 +1,5 @@
 void main() 
 {
   gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-  gl_TexCoord[0] = gl_MultiTexCoord0;
+  gl_TexCoord[0] = gl_MultiTexCoord0;  
 }
