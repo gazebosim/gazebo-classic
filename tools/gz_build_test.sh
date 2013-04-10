d7af7e1b83876a11558142e653475461b3ee1e2e
Nate Koenig <natekoenig@gmail.com>
2013-04-10 16:06:37 -0700

    Improvements to build test script

--- tools/gz_build_test.sh
+++ tools/gz_build_test.sh
@@ -20,6 +20,8 @@ mkdir /tmp/gazebo_build
 # Clone
 hg clone https://bitbucket.org/osrf/gazebo /tmp/gazebo_build
 
+start_time=`eval date +%s`
+
 # Process each branch from the command line
 for branch in $branches
 do
@@ -34,19 +36,26 @@ do
   cmake ../
   make -j4
 
-  echo "** Branch: $branch **" >> $logfile
-
+  echo "Branch: $branch" >> $logfile
+  echo "==================================================" >> $logfile
+  echo "Test Results" >> $logfile
   # Run make test many times, only capture failures
   for i in {1..100}
   do
     cd /tmp/gazebo_build/build
-    make test | grep -i "fail" >> $logfile 
+    make test | grep "***" >> $logfile 
   done
 
+  echo "Code Check Results" >> $logfile
   # Run code checker
   cd /tmp/gazebo_build/
   sh tools/code_check.sh >> $logfile
 done
+end_time=`eval date +%s`
+duration=`expr $end_time - $start_time`
+min=`expr $duration / 60`
+sec=`expr $duration - $min \* 60`
+echo "Duration: $min min $sec sec" >> $logfile
 
 # Cleanup
 cd
