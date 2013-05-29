94cb182cc7094a219c55eed0aaaf581b39d2d4bc
Steve Peters <scpeters@osrfoundation.org>
2013-05-29 15:00:19 -0700

    Initial implementation of tactile pressure sensor

---
+++ plugins/PressurePlugin.cc
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2012 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+/*
+ * Desc: Pressure sensor plugin
+ * Author: Steve Peters
+ */
+#include "PressurePlugin.hh"
+
+using namespace gazebo;
+GZ_REGISTER_SENSOR_PLUGIN(PressurePlugin)
+
+/////////////////////////////////////////////////
+PressurePlugin::PressurePlugin() : SensorPlugin()
+{
+}
+
+/////////////////////////////////////////////////
+PressurePlugin::~PressurePlugin()
+{
+}
+
+/////////////////////////////////////////////////
+void PressurePlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr /*_sdf*/)
+{
+  // Get the parent sensor.
+  this->parentSensor =
+    boost::dynamic_pointer_cast<sensors::ContactSensor>(_sensor);
+
+  // Make sure the parent sensor is valid.
+  if (!this->parentSensor)
+  {
+    gzerr << "PressurePlugin requires a ContactSensor.\n";
+    return;
+  }
+
+  // Connect to the sensor update event.
+  this->updateConnection = this->parentSensor->ConnectUpdated(
+      boost::bind(&PressurePlugin::OnUpdate, this));
+
+  // Make sure the parent sensor is active.
+  this->parentSensor->SetActive(true);
+}
+
+/////////////////////////////////////////////////
+void PressurePlugin::OnUpdate()
+{
+  // Get all the contacts.
+  msgs::Contacts contacts;
+  contacts = this->parentSensor->GetContacts();
+  for (int i = 0; i < contacts.contact_size(); ++i)
+  {
+    std::cout << "Collision between[" << contacts.contact(i).collision1()
+              << "] and [" << contacts.contact(i).collision2() << "]\n";
+
+    for (int j = 0; j < contacts.contact(i).position_size(); ++j)
+    {
+      std::cout << j << "  Position:"
+                << contacts.contact(i).position(j).x() << " "
+                << contacts.contact(i).position(j).y() << " "
+                << contacts.contact(i).position(j).z() << "\n";
+      std::cout << "   Normal:"
+                << contacts.contact(i).normal(j).x() << " "
+                << contacts.contact(i).normal(j).y() << " "
+                << contacts.contact(i).normal(j).z() << "\n";
+      std::cout << "   Depth:" << contacts.contact(i).depth(j) << "\n";
+      std::cout << "   Normal force 1: "
+                << contacts.contact(i).normal(j).x() *
+                   contacts.contact(i).wrench(j).body_1_force().x() +
+                   contacts.contact(i).normal(j).y() *
+                   contacts.contact(i).wrench(j).body_1_force().y() +
+                   contacts.contact(i).normal(j).z() *
+                   contacts.contact(i).wrench(j).body_1_force().z() << "\n";
+    }
+  }
+}
---
+++ plugins/PressurePlugin.hh
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2012 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+/*
+ * Desc: Pressure Sensor Plugin
+ * Author: Steve Peters
+ */
+#ifndef _GAZEBO_PRESSURE_PLUGIN_HH_
+#define _GAZEBO_PRESSURE_PLUGIN_HH_
+
+#include <string>
+
+#include <gazebo/gazebo.hh>
+#include <gazebo/sensors/sensors.hh>
+
+namespace gazebo
+{
+  /// \brief A plugin for a tactile pressure sensor.
+  class PressurePlugin : public SensorPlugin
+  {
+    /// \brief Constructor.
+    public: PressurePlugin();
+
+    /// \brief Destructor.
+    public: virtual ~PressurePlugin();
+
+    /// \brief Load the sensor plugin.
+    /// \param[in] _sensor Pointer to the sensor that loaded this plugin.
+    /// \param[in] _sdf SDF element that describes the plugin.
+    public: virtual void Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf);
+
+    /// \brief Callback that recieves the contact sensor's update signal.
+    private: virtual void OnUpdate();
+
+    /// \brief Pointer to the contact sensor
+    private: sensors::ContactSensorPtr parentSensor;
+
+    /// \brief Connection that maintains a link between the contact sensor's
+    /// updated signal and the OnUpdate callback.
+    private: event::ConnectionPtr updateConnection;
+  };
+}
+#endif
