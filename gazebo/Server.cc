b26f72fd4c9ce5883a61f777de1ab836773f50cf
Nate Koenig <natekoenig@gmail.com>
2013-06-01 21:37:51 -0700

    Adding new command line tool

--- gazebo/Server.cc
+++ gazebo/Server.cc
@@ -107,9 +107,7 @@ bool Server::ParseArgs(int argc, char **argv)
 
   po::options_description h_desc("Hidden options");
   h_desc.add_options()
-    ("world_file", po::value<std::string>(), "SDF world to load.");
-
-  h_desc.add_options()
+    ("world_file", po::value<std::string>(), "SDF world to load.")
     ("pass_through", po::value<std::vector<std::string> >(),
      "not used, passed through to system plugins.");
 
