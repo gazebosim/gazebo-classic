923b235704f3f2296c21636828f08e87571aab19
Nate Koenig <natekoenig@gmail.com>
2013-06-02 17:49:43 -0700

    Fix for projector segfault

--- gazebo/sensors/SensorManager.cc
+++ gazebo/sensors/SensorManager.cc
@@ -636,7 +636,7 @@ void SensorManager::ImageSensorContainer::Update(bool _force)
   event::Events::preRender();
 
   // Tell all the cameras to render
-  event::Events::render();
+  // event::Events::render();
 
   event::Events::postRender();
 
