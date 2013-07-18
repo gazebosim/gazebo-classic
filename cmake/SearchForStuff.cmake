395d5525f3f4f252b6899673540969e480584a95
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-07-18 19:45:28 +0200

    Full support to use RONN to generate manpages

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -506,6 +506,11 @@ else (libdl_library AND libdl_include_dir)
 endif ()
 
 ########################################
+# Include man pages stuff
+include (${gazebo_cmake_dir}/Pod2Man.cmake)
+add_manpage_target()
+
+########################################
 # Find QWT (QT graphing library)
 #find_path(QWT_INCLUDE_DIR NAMES qwt.h PATHS
 #  /usr/include
