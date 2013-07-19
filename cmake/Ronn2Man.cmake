aab45b51229bf5d480035b1cc691eb7dcba64bb6
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-07-19 11:06:23 +0200

    Proper indentation

--- cmake/Ronn2Man.cmake
+++ cmake/Ronn2Man.cmake
@@ -4,54 +4,54 @@
 # Subject to the GPL, version 2.
 #
 MACRO(ADD_MANPAGE_TARGET)
-	# It is not possible add a dependency to target 'install'
-	# Run hard-coded 'make man' when 'make install' is invoked
-	INSTALL(CODE "EXECUTE_PROCESS(COMMAND make man)")
-	ADD_CUSTOM_TARGET(man)
+  # It is not possible add a dependency to target 'install'
+  # Run hard-coded 'make man' when 'make install' is invoked
+  INSTALL(CODE "EXECUTE_PROCESS(COMMAND make man)")
+  ADD_CUSTOM_TARGET(man)
 ENDMACRO(ADD_MANPAGE_TARGET)
 
 FIND_PROGRAM(RONN ronn)
 FIND_PROGRAM(GZIP gzip)
 
 IF (NOT RONN OR NOT GZIP)
-	IF (NOT RONN)
-			BUILD_WARNING ("ronn not found, manpages won't be generated")
+  IF (NOT RONN)
+    BUILD_WARNING ("ronn not found, manpages won't be generated")
   ENDIF(NOT RONN)
-	IF (NOT GZIP)
-		BUILD_WARNING ("gzip not found, manpages won't be generated")
-	ENDIF(NOT GZIP)
-	# empty macro
-	MACRO(manpage MANFILE)
-	ENDMACRO(manpage)
+  IF (NOT GZIP)
+    BUILD_WARNING ("gzip not found, manpages won't be generated")
+  ENDIF(NOT GZIP)
+  # empty macro
+  MACRO(manpage MANFILE)
+  ENDMACRO(manpage)
 ELSE (NOT RONN OR NOT GZIP)
-    MESSAGE (STATUS "Looking for ronn to generate manpages - found")
+  MESSAGE (STATUS "Looking for ronn to generate manpages - found")
 
-		MACRO(manpage RONNFILE)
-			SET(SECTION 1)
+  MACRO(manpage RONNFILE)
+    SET(SECTION 1)
 
-			ADD_CUSTOM_COMMAND(
-				OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${RONNFILE}.${SECTION}
-				DEPENDS ${RONNFILE}
-				COMMAND ${RONN}
-					ARGS -r --pipe ${RONNFILE} 
-					> ${CMAKE_CURRENT_BINARY_DIR}/${RONNFILE}.${SECTION}
-			)
-
-			ADD_CUSTOM_COMMAND(
-	    	OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${RONNFILE}.${SECTION}.gz
-        DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${RONNFILE}.${SECTION}
-	    	COMMAND ${GZIP} -c ${CMAKE_CURRENT_BINARY_DIR}/${RONNFILE}.${SECTION}
-                > ${CMAKE_CURRENT_BINARY_DIR}/${RONNFILE}.${SECTION}.gz
+    ADD_CUSTOM_COMMAND(
+      OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${RONNFILE}.${SECTION}
+      DEPENDS ${RONNFILE}
+      COMMAND ${RONN}
+         ARGS -r --pipe ${RONNFILE} 
+         > ${CMAKE_CURRENT_BINARY_DIR}/${RONNFILE}.${SECTION}
     )
 
-		SET(MANPAGE_TARGET "man-${RONNFILE}")
+    ADD_CUSTOM_COMMAND(
+      OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${RONNFILE}.${SECTION}.gz
+      DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${RONNFILE}.${SECTION}
+      COMMAND ${GZIP} -c ${CMAKE_CURRENT_BINARY_DIR}/${RONNFILE}.${SECTION}
+        > ${CMAKE_CURRENT_BINARY_DIR}/${RONNFILE}.${SECTION}.gz
+    )
+
+    SET(MANPAGE_TARGET "man-${RONNFILE}")
 
     ADD_CUSTOM_TARGET(${MANPAGE_TARGET} DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${RONNFILE}.${SECTION}.gz)
     ADD_DEPENDENCIES(man ${MANPAGE_TARGET})
 
     INSTALL(
-	    FILES ${CMAKE_CURRENT_BINARY_DIR}/${RONNFILE}.${SECTION}.gz
-	    DESTINATION share/man/man${SECTION}
+      FILES ${CMAKE_CURRENT_BINARY_DIR}/${RONNFILE}.${SECTION}.gz
+      DESTINATION share/man/man${SECTION}
     )
 	ENDMACRO(manpage RONNFILE SECTION)
 ENDIF(NOT RONN OR NOT GZIP)
