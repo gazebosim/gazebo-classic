1c88e49162b05f3a97ccb439a4a53ac7146585d3
Steve Peters <scpeters@osrfoundation.org>
2013-07-19 14:24:27 -0700

    Added Noise class to gazebo/sensors

---
+++ gazebo/sensors/Noise.cc
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2013 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+
+#include <boost/math/special_functions/round.hpp>
+
+#include "gazebo/common/Assert.hh"
+#include "gazebo/math/Rand.hh"
+
+#include "gazebo/sensors/Noise.hh"
+
+using namespace gazebo;
+using namespace sensors;
+
+//////////////////////////////////////////////////
+Noise::Noise()
+ : noiseType(NONE),
+   mean(0.0),
+   stdDev(0.0),
+   bias(0.0),
+   precision(0.0)
+{
+}
+
+//////////////////////////////////////////////////
+Noise::~Noise()
+{
+}
+
+//////////////////////////////////////////////////
+void Noise::Load(sdf::ElementPtr _sdf)
+{
+  this->sdf = _sdf;
+  GZ_ASSERT(this->sdf != NULL, "this->sdf is NULL");
+  std::string type = this->sdf->GetValueString("type");
+  if (type == "none")
+    this->noiseType = NONE;
+  else if (type == "gaussian")
+    this->noiseType = GAUSSIAN;
+  else if (type == "gaussian_quantized")
+    this->noiseType = GAUSSIAN_QUANTIZED;
+  else
+  {
+    gzerr << "Unrecognized noise type: [" << type << "]"
+          << ", using default [none]" << std::endl;
+    this->noiseType = NONE;
+  }
+
+  if (this->noiseType == GAUSSIAN ||
+      this->noiseType == GAUSSIAN_QUANTIZED)
+  {
+    this->mean = this->sdf->GetValueDouble("mean");
+    this->stdDev = this->sdf->GetValueDouble("stddev");
+    // Sample the bias
+    double biasMean = this->sdf->GetValueDouble("bias_mean");
+    double biasStdDev = this->sdf->GetValueDouble("bias_stddev");
+    this->bias = math::Rand::GetDblNormal(biasMean, biasStdDev);
+    // With equal probability, we pick a negative bias (by convention,
+    // rateBiasMean should be positive, though it would work fine if
+    // negative).
+    if (math::Rand::GetDblUniform() < 0.5)
+      this->bias = -this->bias;
+    gzlog << "applying Gaussian noise model with mean " << this->mean
+      << ", stddev " << this->stdDev
+      << ", bias " << this->bias << std::endl;
+  }
+
+  if (this->noiseType == GAUSSIAN_QUANTIZED)
+    this->precision = this->sdf->GetValueDouble("precision");
+}
+
+//////////////////////////////////////////////////
+double Noise::Apply(double _in)
+{
+  double output = 0.0;
+  if (this->noiseType == NONE)
+    output = _in;
+  else if (this->noiseType == GAUSSIAN ||
+           this->noiseType == GAUSSIAN_QUANTIZED)
+  {
+    double whiteNoise = math::Rand::GetDblNormal(this->mean, this->stdDev);
+    output = _in + this->bias + whiteNoise;
+    if (this->noiseType == GAUSSIAN_QUANTIZED)
+    {
+      // Apply this->precision
+      if (!math::equal(this->precision, 0.0, 1e-6))
+      {
+        output = boost::math::round(output * this->precision) / this->precision;
+      }
+    }
+  }
+  return output;
+}
---
+++ gazebo/sensors/Noise.hh
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2013 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef _NOISE_HH_
+#define _NOISE_HH_
+
+#include <vector>
+#include <string>
+
+#include "gazebo/sdf/sdf.hh"
+
+namespace gazebo
+{
+  namespace sensors
+  {
+    /// \addtogroup gazebo_sensors
+    /// \{
+
+    /// \class Noise Noise.hh sensors/sensors.hh
+    /// \brief Noise models for sensor output signals.
+    class Noise
+    {
+      /// \brief Constructor.
+      public: Noise();
+
+      /// \brief Destructor.
+      public: ~Noise();
+
+      /// \brief Load parameters from sdf.
+      public: void Load(sdf::ElementPtr _sdf);
+
+      /// \brief Apply noise to input data value.
+      /// \param[in] _in Input data value.
+      /// \return Data with noise applied.
+      public: double Apply(double _in);
+
+      /// \brief Which noise type we support
+      private: enum NoiseModelType
+      {
+        NONE,
+        GAUSSIAN,
+        GAUSSIAN_QUANTIZED
+      };
+
+      /// \brief 
+      private: sdf::ElementPtr sdf;
+
+      /// \brief Which type of noise we're applying
+      private: enum NoiseModelType noiseType;
+
+      /// \brief If noiseType starts with GAUSSIAN, the mean of the distibution
+      /// from which we sample when adding noise.
+      private: double mean;
+
+      /// \brief If noiseType starts with GAUSSIAN, the standard devation of the
+      /// distibution from which we sample when adding noise.
+      private: double stdDev;
+
+      /// \brief If noiseType starts with GAUSSIAN, the bias we'll add.
+      private: double bias;
+
+      /// \brief If noiseType==GAUSSIAN_QUANTIZED, the precision to which
+      /// the output signal is rounded.
+      private: double precision;
+    };
+    /// \}
+  }
+}
+#endif
