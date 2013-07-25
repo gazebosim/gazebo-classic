44e9b6e17e6e7e85274b8ea81e869c6744f54c7a
Steve Peters <scpeters@osrfoundation.org>
2013-07-25 13:04:11 -0700

    Fix header guard for GuiIface.hh

--- gazebo/gui/GuiIface.hh
+++ gazebo/gui/GuiIface.hh
@@ -14,8 +14,8 @@
  * limitations under the License.
  *
 */
-#ifndef _GAZEBO_GUI_HH_
-#define _GAZEBO_GUI_HH_
+#ifndef _GAZEBO_GUIIFACE_HH_
+#define _GAZEBO_GUIIFACE_HH_
 
 #include <string>
 #include "gazebo/rendering/RenderingIface.hh"
