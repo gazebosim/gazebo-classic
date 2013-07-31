10ef5360496d18425532aa42c7c0420e344a144b
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-07-31 16:30:20 +0200

    Remove check for unused functions

--- tools/code_check.sh
+++ tools/code_check.sh
@@ -14,7 +14,6 @@ else
   builddir=./build
 fi
 
-echo "unusedFunction" > /tmp/gazebo_cpp_check.suppress 
 echo "*:gazebo/sdf/interface/parser.cc:544" >> /tmp/gazebo_cpp_check.suppress
 echo "*:gazebo/common/STLLoader.cc:94" >> /tmp/gazebo_cpp_check.suppress
 echo "*:gazebo/common/STLLoader.cc:105" >> /tmp/gazebo_cpp_check.suppress
@@ -30,17 +29,11 @@ if [ $xmlout -eq 1 ]; then
   # Run most of the checks in parallel
   (cppcheck --xml --enable=style,performance,portability,information --rule-file=./tools/cppcheck_rules/issue_581.rule -j 4 -q --suppressions-list=/tmp/gazebo_cpp_check.suppress `find ./plugins ./gazebo ./tools ./examples ./test/regression ./interfaces -name "*.cc"`) 2> $xmldir/cppcheck.xml 
 
-  # Unused function checking must happen in one job
-  (cppcheck --xml --enable=unusedFunction -q --suppressions-list=/tmp/gazebo_cpp_check.suppress `find ./plugins ./gazebo ./tools ./examples ./test/regression ./interfaces -name "*.cc"`) 2> $xmldir/cppcheck-unused-functions.xml 
-
   # Finally, check the configuration
   (cppcheck --xml --enable=missingInclude -q -j 4 --suppressions-list=/tmp/gazebo_cpp_check.suppress `find ./plugins ./gazebo ./tools ./examples ./test/regression ./interfaces -name "*.cc"` -I gazebo/rendering/skyx/include -I . -I $builddir -I $builddir/gazebo/msgs -I deps -I deps/opende/include -I test --check-config) 2> $xmldir/cppcheck-configuration.xml
 else
   # Run most of the checks in parallel
   cppcheck --enable=style,performance,portability,information --rule-file=./tools/cppcheck_rules/issue_581.rule -j 4 -q --suppressions-list=/tmp/gazebo_cpp_check.suppress `find ./plugins ./gazebo ./tools ./examples ./test/regression ./interfaces -name "*.cc"` 2>&1
-
-  # Unused function checking must happen in one job
-  cppcheck --enable=unusedFunction -q --suppressions-list=/tmp/gazebo_cpp_check.suppress `find ./plugins ./gazebo ./tools ./examples ./test/regression ./interfaces -name "*.cc"` 2>&1
 
   # Finally, check the configuration
   cppcheck --enable=missingInclude -q -j 4 --suppressions-list=/tmp/gazebo_cpp_check.suppress `find ./plugins ./gazebo ./tools ./examples ./test/regression ./interfaces -name "*.cc"` -I gazebo/rendering/skyx/include -I . -I $builddir -I $builddir/gazebo/msgs -I deps -I deps/opende/include -I test --check-config 2>&1
