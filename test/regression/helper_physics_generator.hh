0ba9e64fdfd290eb9c1dd7256a35508b6cc02d8c
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-08-15 02:28:56 +0200

    Make the macro more scalable

--- test/regression/helper_physics_generator.hh
+++ test/regression/helper_physics_generator.hh
@@ -1,15 +1,17 @@
 #ifndef _TEST_GENERATOR_HH
 #define _TEST_GENERATOR_HH_
 
+#define ODE_SUPPORT(testclass) INSTANTIATE_TEST_CASE_P(TestODE, testclass, ::testing::Values("ode"))
+#define BULLET_SUPPORT(testclass)
+
 #ifdef HAVE_BULLET
-/// \brief Helper macro to instantiate gtest for using different physics engines
-#define INSTANTIATE_PHYSICS_ENGINES_TEST(testclass) \
-     INSTANTIATE_TEST_CASE_P(TestODE, testclass, ::testing::Values("ode"));  \
-     INSTANTIATE_TEST_CASE_P(TestBullet, testclass, ::testing::Values("bullet"))
-#else
-/// \brief Helper macro to instantiate gtest for using different physics engines
-#define INSTANTIATE_PHYSICS_ENGINES_TEST(testclass) \
-  INSTANTIATE_TEST_CASE_P(TestODE, testclass, ::testing::Values("ode"))
+#undef BULLET_SUPPORT
+#define BULLET_SUPPORT(testclass) INSTANTIATE_TEST_CASE_P(TestBullet, testclass, ::testing::Values("bullet"))
 #endif
+
+/// \brief Helper macro to instantiate gtest for using different physics engines
+#define INSTANTIATE_PHYSICS_ENGINES_TEST(testclass) \
+    ODE_SUPPORT(testclass); \
+    BULLET_SUPPORT(testclass)
 
 #endif
