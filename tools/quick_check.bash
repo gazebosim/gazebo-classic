0dc14e127ec23ced564f006a762164bf869f3ab7
Steve Peters <scpeters@osrfoundation.org>
2013-08-21 12:40:24 -0700

    Add tool for quickly running part of code_check on pending pull requests

---
+++ tools/quick_check.bash
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+hg_root=`hg root`
+while read line
+do
+  id=`echo $line | awk '{print $1}'`
+  src=`echo $line | awk '{print $2}'`
+  dest=`echo $line | awk '{print $3}'`
+  branch=`echo $line | awk '{print $4}'`
+
+  msg="$id $branch"
+  if ! hg log -r $dest &> /dev/null
+  then
+    echo $msg Unknown revision $dest, try: hg pull
+    break
+  fi
+
+  if ! hg log -r $src &> /dev/null
+  then
+    echo $msg Unknown revision $src, try: hg pull "(it could also be a fork)"
+    break
+  fi
+
+  ancestor=`hg log -r "ancestor($src,$dest)" | head -1 | sed -e 's@.*:@@'`
+  
+  if [ $dest != $ancestor ]
+  then
+    msg="$msg need to merge branch $branch with $dest"
+  fi
+
+  echo $msg
+
+  files=`$hg_root/tools/bitbucket_pullrequests -f $id | grep '\.[ch]*$'`
+  tmp=`mktemp -t asdf`
+  for f in $files
+  do
+    ext=`echo $f | sed -e 's@^.*\.@@'`
+    tmp2="$tmp.$ext"
+    hg cat -r $src $hg_root/$f > $tmp2
+    python $hg_root/tools/cpplint.py $tmp2 2>&1 \
+      | sed -e "s@$tmp2@$f@g" \
+      | grep -v 'Total errors found: 0'
+    cppcheck -q --enable=style,performance,portability,information $tmp2 2>&1 \
+      | sed -e "s@$tmp2@$f@g" \
+      | grep -v 'use --check-config for details'
+    rm $tmp2
+  done
+  rm $tmp
+done
