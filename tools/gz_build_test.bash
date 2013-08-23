238ad132380b64dd881fcd1f909fbd9d35b936ee
Steve Peters <scpeters@osrfoundation.org>
2013-08-23 14:53:50 -0700

    Make summary of test results, FORCE_GRAPHIC_TESTS_COMPILATION, add bash variables

--- tools/gz_build_test.bash
+++ tools/gz_build_test.bash
@@ -11,33 +11,51 @@ fi
 # Create a logfile based on the current time
 timestamp=`eval date +%Y_%m_%d_%R:%S`
 logfile="/tmp/gazebo_test-$timestamp.txt"
+logfileSummary="/tmp/gazebo_test-$timestamp-summary.txt"
 logfileVerbose="/tmp/gazebo_test-$timestamp-verbose.txt"
-logfileRaw=/tmp/gazebo_build/raw.log
+BUILD_ROOT=/tmp/gazebo_build
+logfileRaw=$BUILD_ROOT/raw.log
 
 # Create working directory
 cd 
-rm -rf /tmp/gazebo_build
-mkdir /tmp/gazebo_build
+rm -rf $BUILD_ROOT
+mkdir $BUILD_ROOT
 
 # Clone
-#hg clone https://bitbucket.org/osrf/gazebo /tmp/gazebo_build/source
-cd /home/scpeters/osrf/gazebo_readonly
-hg pull
-hg clone /home/scpeters/osrf/gazebo_readonly /tmp/gazebo_build/source
+ORIGIN=https://bitbucket.org/osrf/gazebo
+if [ `hostname` == t2 ]
+then
+  hg clone $HOME/osrf/gazebo_readonly $BUILD_ROOT/source
+  cd $BUILD_ROOT/source
+  hg pull $ORIGIN
+elif [ `hostname` == t1000 ]
+then
+  hg clone $HOME/code/gazebo $BUILD_ROOT/source
+  cd $BUILD_ROOT/source
+  hg pull $ORIGIN
+
+  # build against homebrew
+  export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+  export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
+  export PKG_CONFIG_LIBDIR=$PKG_CONFIG_LIBDIR:/usr/lib/pkgconfig:/usr/local/Library/ENV/pkgconfig/10.8
+  export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
+else
+  hg clone $ORIGIN $BUILD_ROOT/source
+fi
 
 start_time=`eval date +%s`
 
 export DISPLAY=:0
-export PATH=/tmp/gazebo_build/install/bin:$PATH
-export LD_LIBRARY_PATH=/tmp/gazebo_build/install/lib:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=/tmp/gazebo_build/install/lib/pkgconfig:$PKG_CONFIG_PATH
+export PATH=$BUILD_ROOT/install/bin:$PATH
+export LD_LIBRARY_PATH=$BUILD_ROOT/install/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=$BUILD_ROOT/install/lib/pkgconfig:$PKG_CONFIG_PATH
 ulimit -c unlimited
 
 # Process each branch from the command line
 for branch in $branches
 do
   # Get the correct branch
-  cd /tmp/gazebo_build/source
+  cd $BUILD_ROOT/source
   echo hg up $branch
   hg up $branch
 
@@ -45,23 +63,25 @@ do
   rm -rf build
   mkdir build
   cd build
-  cmake -DCMAKE_INSTALL_PREFIX=/tmp/gazebo_build/install ../
+  cmake .. \
+    -DCMAKE_INSTALL_PREFIX=$BUILD_ROOT/install \
+    -DFORCE_GRAPHIC_TESTS_COMPILATION=True
   make -j4 install
-  . /tmp/gazebo_build/install/share/gazebo/setup.sh
+  . $BUILD_ROOT/install/share/gazebo/setup.sh
 
   echo "Branch: $branch" >> $logfile
   echo "==================================================" >> $logfile
 
   echo "Code Check Results" >> $logfile
   # Run code checker
-  cd /tmp/gazebo_build/source
+  cd $BUILD_ROOT/source
   sh tools/code_check.sh >> $logfile
 
   echo "Test Results" >> $logfile
   # Run make test many times, only capture failures
   for i in {1..100}
   do
-    cd /tmp/gazebo_build/source/build
+    cd $BUILD_ROOT/source/build
 
     # make test with verbose output
     make test ARGS="-VV" &> $logfileRaw
@@ -81,6 +101,11 @@ do
       grep '^ *'`echo "(($f-1)/2)*2+1" | bc`':' $logfileRaw >> $logfileVerbose
       grep '^ *'`echo "(($f-1)/2)*2+2" | bc`':' $logfileRaw >> $logfileVerbose
     done
+
+    # update summary file
+    grep 'Test *#' $logfile  | \
+      sed -e 's@[0-9]*\.[0-9]* *sec$@@' -e 's@.*Test@failures:@' | \
+      sort | uniq -c | sort -rg > $logfileSummary
   done
 done
 
@@ -93,4 +118,4 @@ echo "Duration: $hour hr $min min $sec sec" >> $logfile
 
 # Cleanup
 cd
-rm -rf /tmp/gazebo_build
+rm -rf $BUILD_ROOT
