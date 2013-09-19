39f8bcf98dfeac194e5e45b2c335e5d7eebd4107
Nate Koenig <natekoenig@gmail.com>
2013-09-19 09:43:34 -0700

    Updates

--- plugins/ModelPropShop.cc
+++ plugins/ModelPropShop.cc
@@ -14,6 +14,7 @@
  * limitations under the License.
  *
 */
+#include "gazebo/sensors/SensorsIface.hh"
 #include "ModelPropShop.hh"
 
 using namespace gazebo;
@@ -25,47 +26,52 @@ GZ_REGISTER_SYSTEM_PLUGIN(ModelPropShop)
 void ModelPropShop::Load(int /*_argc*/, char ** /*_argv*/)
 {
   printf("ModelPropShop::Load\n");
-  rendering::load();
 }
 
 /////////////////////////////////////////////
 void ModelPropShop::Init()
 {
-  printf("ModelPropShop::Init\n");
-  rendering::init();
-
-  sdf::ElementPtr cameraSDF(new sdf::Element);
-  sdf::initFile("camera.sdf", cameraSDF);
-
-  this->scene = rendering::create_scene("default", false, true);
-  this->camera = this->scene->CreateCamera("propshopcamera", true);
-  this->camera->SetCaptureData(true);
-  this->camera->Load(cameraSDF);
-  this->camera->Init();
-  this->camera->CreateRenderTexture("ModelPropShop_RttTex");
-
   this->connections.push_back(
       event::Events::ConnectWorldUpdateBegin(
         boost::bind(&ModelPropShop::Update, this)));
 
   this->renderCount = 0;
+  gazebo::sensors::stop();
+  gazebo::sensors::fini();
 }
 
 /////////////////////////////////////////////
 void ModelPropShop::Update()
 {
+  // Make sure to initialize the rendering engine in the same thread that will
+  // capture images.
+  if (!this->scene)
+  {
+    rendering::load();
+    rendering::init();
+
+    sdf::ElementPtr cameraSDF(new sdf::Element);
+    sdf::initFile("camera.sdf", cameraSDF);
+
+    this->scene = rendering::create_scene("default", false, true);
+    this->camera = this->scene->CreateCamera("propshopcamera", true);
+    this->camera->SetCaptureData(true);
+    this->camera->Load(cameraSDF);
+    this->camera->Init();
+    this->camera->CreateRenderTexture("ModelPropShop_RttTex");
+  }
+
   event::Events::preRender();
 
-  if (this->scene && this->scene->GetInitialized())
+  if (this->scene->GetInitialized())
   {
-    if (this->renderCount < 100)
+    /*if (this->renderCount < 100)
     {
       this->renderCount++;
       return;
-    }
+    }*/
 
-    printf("Update\n");
-    rendering::VisualPtr vis = this->scene->GetVisual("box");
+    rendering::VisualPtr vis = this->scene->GetVisual("pr2");
     if (vis)
     {
       math::Box bbox = vis->GetBoundingBox();
@@ -77,12 +83,11 @@ void ModelPropShop::Update()
       // Place the visual at the origin
       vis->SetWorldPose(math::Pose(0, 0, 0, 0, 0, 0));
 
-
       math::Pose pose;
 
       // Top view
-      pose.pos.Set(0, 0, 5.0);
-      pose.rot.SetFromEuler(0, GZ_DTOR(-90), 0);
+      pose.pos.Set(0, 0, 2.0);
+      pose.rot.SetFromEuler(0, GZ_DTOR(90), 0);
       this->camera->SetWorldPose(pose);
       this->camera->Update();
       this->camera->Render();
@@ -90,7 +95,7 @@ void ModelPropShop::Update()
       this->camera->SaveFrame("/tmp/top_view.png");
 
       // Front view
-      pose.pos.Set(5, 0, 0.5);
+      pose.pos.Set(1.5, 0, 0.5);
       pose.rot.SetFromEuler(0, 0, GZ_DTOR(-180));
       this->camera->Update();
       this->camera->Render();
@@ -98,7 +103,7 @@ void ModelPropShop::Update()
       this->camera->SaveFrame("/tmp/front_view.png");
 
       // Side view
-      pose.pos.Set(0, 5, 0.5);
+      pose.pos.Set(0, 1.5, 0.5);
       pose.rot.SetFromEuler(0, 0, GZ_DTOR(-90));
       this->camera->SetWorldPose(pose);
       this->camera->Update();
@@ -107,7 +112,7 @@ void ModelPropShop::Update()
       this->camera->SaveFrame("/tmp/side_view.png");
 
       // Back view
-      pose.pos.Set(-5, 0, 0.5);
+      pose.pos.Set(-1.5, 0, 0.5);
       pose.rot.SetFromEuler(0, 0, 0);
       this->camera->SetWorldPose(pose);
       this->camera->Update();
