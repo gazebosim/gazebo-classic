867edca111b63f05ad8ae548c4cd48270a2c09b0
Jeongseok Lee <jslee02@gmail.com>
2013-09-19 04:42:06 -0400

    Rename local variable name since it is same as class member variable.

--- gazebo/physics/dart/DARTJoint.cc
+++ gazebo/physics/dart/DARTJoint.cc
@@ -364,7 +364,7 @@ JointWrench DARTJoint::GetForceTorque(int _index)
 
 JointWrench DARTJoint::GetForceTorque(unsigned int /*_index*/)
 {
-  JointWrench wrench;
+  JointWrench jointWrench;
 
   //----------------------------------------------------------------------------
   // Parent and child link information
@@ -393,12 +393,12 @@ JointWrench DARTJoint::GetForceTorque(unsigned int /*_index*/)
 
   // kind of backwards here, body1 (parent) corresponds go f2, t2
   // and body2 (child) corresponds go f1, t1
-  wrench.body1Force.Set(F1(3), F1(4), F1(5));
-  wrench.body1Torque.Set(F1(0), F1(1), F1(2));
-  wrench.body2Force.Set(F2(3), F2(4), F2(5));
-  wrench.body2Torque.Set(F2(0), F2(1), F2(2));
+  jointWrench.body1Force.Set(F1(3), F1(4), F1(5));
+  jointWrench.body1Torque.Set(F1(0), F1(1), F1(2));
+  jointWrench.body2Force.Set(F2(3), F2(4), F2(5));
+  jointWrench.body2Torque.Set(F2(0), F2(1), F2(2));
 
-  return wrench;
+  return jointWrench;
 }
 
 void DARTJoint::SetForce(int _index, double _force)
