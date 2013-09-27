03e1bf77c824d23d59ef5f6613faebe6c4a7d0ab
Nate Koenig <natekoenig@gmail.com>
2013-09-27 06:59:52 -0700

    Split gripper test

--- test/integration/gripper.cc
+++ test/integration/gripper.cc
@@ -28,7 +28,58 @@ class GripperTest : public ServerFixture
 
 /////////////////////////////////////////////////
 // \brief Test to make sure the gripper forms a joint when grasping an object
-TEST_F(GripperTest, Grasp)
+TEST_F(GripperTest, Close)
+{
+  Load("worlds/gripper.world");
+  physics::WorldPtr world = physics::get_world("default");
+  ASSERT_TRUE(world);
+
+  physics::ModelPtr model = world->GetModel("simple_gripper");
+  ASSERT_TRUE(model);
+
+  physics::JointPtr leftJoint = model->GetJoint("palm_left_finger");
+  ASSERT_TRUE(leftJoint);
+
+  physics::JointPtr rightJoint = model->GetJoint("palm_right_finger");
+  ASSERT_TRUE(rightJoint);
+
+  physics::GripperPtr gripper = model->GetGripper(0);
+  ASSERT_TRUE(gripper);
+
+  // The gripper should not be attached to anything
+  EXPECT_FALSE(gripper->IsAttached());
+
+  // Close the gripper.
+  leftJoint->SetForce(0, -0.5);
+  rightJoint->SetForce(0, 0.5);
+
+  transport::PublisherPtr jointPub = this->node->Advertise<msgs::JointCmd>(
+        "~/simple_gripper/joint_cmd");
+
+  msgs::JointCmd msg;
+  msg.set_name("simple_gripper::palm_right_finger");
+  msg.set_force(0.6);
+  jointPub->Publish(msg);
+
+  msg.set_name("simple_gripper::palm_left_finger");
+  msg.set_force(-0.6);
+  jointPub->Publish(msg);
+
+  int i = 0;
+  while (!gripper->IsAttached() && i < 100)
+  {
+    common::Time::MSleep(500);
+    ++i;
+  }
+  EXPECT_LT(i, 100);
+
+  // The gripper should be attached.
+  EXPECT_TRUE(gripper->IsAttached());
+}
+
+/////////////////////////////////////////////////
+// \brief Test grasp can close and open
+TEST_F(GripperTest, CloseOpen)
 {
   Load("worlds/gripper.world");
   physics::WorldPtr world = physics::get_world("default");
