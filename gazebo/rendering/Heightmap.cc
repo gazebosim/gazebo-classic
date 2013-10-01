a32ce36278571e22e7adb75f128d690baeed1d4a
Carlos Agüero <caguero@osrfoundation.org>
2013-10-01 10:57:53 -0700

    Style tweak

--- gazebo/rendering/Heightmap.cc
+++ gazebo/rendering/Heightmap.cc
@@ -273,8 +273,10 @@ void Heightmap::Load()
 
   Ogre::Vector3 orig = Conversions::Convert(this->terrainOrigin);
   math::Vector3 origin(
-      orig.x -0.5 * this->terrainSize.x + 0.5 * this->terrainSize.x / sqrt(nTerrains),
-      orig.y -0.5 * this->terrainSize.x + 0.5 * this->terrainSize.x / sqrt(nTerrains),
+      orig.x -0.5 * this->terrainSize.x +
+      0.5 * this->terrainSize.x / sqrt(nTerrains),
+      orig.y -0.5 * this->terrainSize.x +
+      0.5 * this->terrainSize.x / sqrt(nTerrains),
       orig.z);
 
   this->terrainGroup->setOrigin(Conversions::Convert(origin));
