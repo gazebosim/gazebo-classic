e2484505243c547d0f76feac99b3f17efd92eed4
Carlos Agüero <caguero@osrfoundation.org>
2013-10-23 12:05:16 -0700

    Minor include tweak

--- gazebo/physics/HeightmapShape.cc
+++ gazebo/physics/HeightmapShape.cc
@@ -19,11 +19,9 @@
  * Date: 8 May 2003
  */
 
-#pragma GCC diagnostic ignored "-Wfloat-equal"
-
 #include <string.h>
 #include <math.h>
-#include <gdalwarper.h>
+#include <gdal/gdalwarper.h>
 
 #include "gazebo/common/Assert.hh"
 #include "gazebo/common/Console.hh"
