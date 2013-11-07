9b5e7e81f455801de599cb888b4732c306fd8a93
John Hsu <hsu@osrfoundation.org>
2013-11-07 13:33:10 -0800

    fix issue #943.  added Joint::GetInitialAnchorPose() for testing purposes.

--- gazebo/physics/Joint.hh
+++ gazebo/physics/Joint.hh
@@ -409,6 +409,11 @@ namespace gazebo
       /// \return viscous joint damping coefficient
       public: double GetDampingCoefficient() const;
 
+      /// \brief Get initial Anchor Pose specified by model
+      /// <joint><pose>...</pose></joint>
+      /// \return Joint::anchorPose, initial joint anchor pose.
+      public: math::Pose GetInitialAnchorPose();
+
       /// \brief Get the angle of an axis helper function.
       /// \param[in] _index Index of the axis.
       /// \return Angle of the axis.
