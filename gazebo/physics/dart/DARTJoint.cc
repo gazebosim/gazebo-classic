b49dbb4deea56868bfe608c4ba2fd05b7e885005
Jeongseok Lee <jslee02@gmail.com>
2013-12-11 02:40:38 -0500

    Joint spring force is added to DARTJoint

--- gazebo/physics/dart/DARTJoint.cc
+++ gazebo/physics/dart/DARTJoint.cc
@@ -281,9 +281,10 @@ void DARTJoint::SetStiffnessDamping(unsigned int _index,
     {
       if (!parentStatic && !childStatic)
       {
-        this->applyDamping = physics::Joint::ConnectJointUpdate(
-          boost::bind(&DARTJoint::ApplyStiffnessDamping, this));
-        this->dtJoint->setDampingCoefficient(_index, _damping);
+        this->dtJoint->setSpringStiffness(
+              static_cast<int>(_index), _stiffness, _reference);
+        this->dtJoint->setDampingCoefficient(
+              static_cast<int>(_index), _damping);
         this->stiffnessDampingInitialized = true;
       }
       else
@@ -295,55 +296,9 @@ void DARTJoint::SetStiffnessDamping(unsigned int _index,
     }
   }
   else
+  {
     gzerr << "SetStiffnessDamping _index too large.\n";
-}
-
-//////////////////////////////////////////////////
-void DARTJoint::SetStiffness(int _index, const double _stiffness)
-{
-  if (static_cast<unsigned int>(_index) < this->GetAngleCount())
-  {
-    this->SetStiffnessDamping(static_cast<unsigned int>(_index),
-      _stiffness,
-      this->dissipationCoefficient[_index]);
   }
-  else
-  {
-     gzerr << "DARTJoint::SetStiffness: index[" << _index
-           << "] is out of bounds (GetAngleCount() = "
-           << this->GetAngleCount() << ").\n";
-     return;
-  }
-}
-
-//////////////////////////////////////////////////
-void DARTJoint::SetStiffnessDamping(unsigned int _index,
-  double _stiffness, double _damping, double _reference)
-{
-  if (_index < this->GetAngleCount())
-  {
-    this->stiffnessCoefficient[_index] = _stiffness;
-    this->dissipationCoefficient[_index] = _damping;
-    this->springReferencePosition[_index] = _reference;
-
-    /// \TODO: set joint stiffness coefficient
-
-    /// setting joint damping
-    bool parentStatic = this->GetParent() ?
-      this->GetParent()->IsStatic() : false;
-    bool childStatic = this->GetChild() ? this->GetChild()->IsStatic() : false;
-
-    if (!parentStatic && !childStatic)
-    {
-      this->dtJoint->setDampingCoefficient(_index, _damping);
-    }
-
-    /// \TODO: add spring force element
-    gzdbg << "Joint [" << this->GetName()
-           << "] stiffness not implement in DART\n";
-  }
-  else
-    gzerr << "SetStiffnessDamping _index too large.\n";
 }
 
 //////////////////////////////////////////////////
@@ -654,7 +609,11 @@ unsigned int DARTJoint::GetAngleCount() const
 
 void DARTJoint::ApplyStiffnessDamping()
 {
-  // DART applying damping force inside of DART.
+  // DART applies stiffness and damping force implicitly itself by setting
+  // the stiffness coefficient and the damping coefficient using
+  // dart::dynamics::Joint::setSpringStiffness(index, stiffnessCoeff) and
+  // dart::dynamics::Joint::setDampingCoefficient(index, dampingCoeff).
+  // Therefore, we do nothing here.
 }
 
 DARTModelPtr DARTJoint::GetDARTModel() const
