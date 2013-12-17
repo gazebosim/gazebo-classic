04918c05e21822ae74f2e85bb8f84848860d7bca
Nate Koenig <natekoenig@gmail.com>
2013-12-17 10:40:10 -0800

    Revert changes

--- gazebo/gazebo_core.hh
+++ gazebo/gazebo_core.hh
@@ -20,6 +20,7 @@
 #include <gazebo/math/gzmath.hh>
 #include <gazebo/msgs/msgs.hh>
 #include <gazebo/transport/transport.hh>
+#include <gazebo/Server.hh>
 #include <gazebo/Master.hh>
 #include <gazebo/gazebo.hh>
 #endif
--- gazebo/transport/Publication.hh
+++ gazebo/transport/Publication.hh
@@ -119,6 +119,15 @@ namespace gazebo
                   boost::function<void(uint32_t)> _cb,
                   uint32_t _id);
 
+      /// \brief Remove a publisher.
+      /// \param[in] _pub Pointer to publisher object to remove.
+      public: void RemovePublisher(PublisherPtr _pub);
+
+      /// \brief Set the previous message for a publisher.
+      /// \param[in] _pubId ID of the publisher.
+      /// \param[in] _msg The previous message.
+      public: void SetPrevMsg(uint32_t _pubId, MessagePtr _msg);
+
       /// \brief Get a previous message for a publisher.
       /// \param[in] _pubId ID of the publisher.
       /// \return Pointer to the previous message. NULL if there is no
@@ -139,15 +148,6 @@ namespace gazebo
       /// \brief Add a publisher
       /// \param[in,out] _pub Pointer to publisher object to be added
       public: void AddPublisher(PublisherPtr _pub);
-
-      /// \brief Remove a publisher
-      /// \param[in] _pub Pointer to publisher object to remove
-      public: void RemovePublisher(PublisherPtr _pub);
-
-      /// \brief Set the previous message for a publisher.
-      /// \param[in] _pubId ID of the publisher.
-      /// \param[in] _msg The previous message.
-      public: void SetPrevMsg(uint32_t _pubId, MessagePtr _msg);
 
       /// \brief Remove nodes that have been marked for removal
       private: void RemoveNodes();
@@ -182,9 +182,6 @@ namespace gazebo
       /// \brief List of publishers.
       private: std::vector<PublisherPtr> publishers;
 
-      /// \brief Publishers and their last messages.
-      private: std::map<uint32_t, MessagePtr> prevMsgs;
-
       /// \brief True if the publication is advertised in the same process.
       private: bool locallyAdvertised;
 
@@ -196,6 +193,9 @@ namespace gazebo
 
       /// \brief Mutex to protect the list of nodes id for removed.
       private: mutable boost::mutex nodeRemoveMutex;
+
+      /// \brief Publishers and their last messages.
+      private: std::map<uint32_t, MessagePtr> prevMsgs;
     };
     /// \}
   }
