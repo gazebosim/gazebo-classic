ebd0a2333451f25dad6782d3c1a732dcc40ff3ab
Ian Chen <ichen@osrfoundation.org>
2013-12-27 15:13:08 -0800

    use 3x3 PCF

--- media/rtshaderlib/SGXLib_IntegratedPSSM.glsl
+++ media/rtshaderlib/SGXLib_IntegratedPSSM.glsl
@@ -43,47 +43,74 @@ void SGX_ModulateScalar(in float vIn0, in vec4 vIn1, out vec4 vOut)
 {
 	vOut = vIn0 * vIn1;
 }
-	
+
 //-----------------------------------------------------------------------------
-void SGX_ApplyShadowFactor_Diffuse(in vec4 ambient, 
-					  in vec4 lightSum, 
-					  in float fShadowFactor, 
+void SGX_ApplyShadowFactor_Diffuse(in vec4 ambient,
+					  in vec4 lightSum,
+					  in float fShadowFactor,
 					  out vec4 oLight)
 {
 	oLight.rgb = ambient.rgb + (lightSum.rgb - ambient.rgb) * (fShadowFactor);
 	oLight.a   = lightSum.a;
 }
-	
+
+//-----------------------------------------------------------------------------
+float texture2DCompare(sampler2D depths, float2 uv, float compare)
+{
+  float depth = tex2D(depths, uv).r;
+  return (step(compare, depth) == 0) ? 0.4 : 1.0;
+}
+
 //-----------------------------------------------------------------------------
 float _SGX_ShadowPCF4(sampler2D shadowMap, vec4 shadowMapPos, vec2 offset)
 {
-	shadowMapPos = shadowMapPos / shadowMapPos.w;
-	vec2 uv = shadowMapPos.xy;
-	vec3 o = vec3(offset, -offset.x) * 0.3;
+  // Interpolated 3x3 PCF
+  // Taken from http://www.ogre3d.org/forums/viewtopic.php?f=1&t=78834
+  shadowMapPos = shadowMapPos / shadowMapPos.w;
+  float2 uv = shadowMapPos.xy;
 
-  float z = shadowMapPos.z;
+  float2 texelSize = offset;
+  float2 size = 1 / offset;
+  float2 centroidUV = floor(uv * size + 0.5) / size;
+  float2 f = frac(uv * size + 0.5);
 
-	// Note: We using 2x2 PCF. Good enough and is a lot faster.
-  float c;
+  // 3 x 3 kernel
+  const int   X = 3;
 
-  // top left
-	c =	(z < texture2D(shadowMap, uv.xy - o.xy).x) ? 1.0 : 0.4;
+  float2 topLeft = centroidUV - texelSize * 1.5;
 
-  // bottom right
-	c += (z < texture2D(shadowMap, uv.xy + o.xy).x) ? 1.0 : 0.4;
+  // load all pixels needed for the computation
+  // this way a pixel won't be loaded twice
+  float kernel[9];
+  for(float i = 0; i < X; i++)
+  {
+    for(float j = 0; j < X; j++)
+    {
+       kernel[i * X + j] = texture2DCompare(shadowMap,
+          topLeft + float2(i, j) * texelSize, shadowMapPos.z);
+    }
+  }
 
-  // bottom left
-	c += (z < texture2D(shadowMap, uv.xy + o.zy).x) ? 1.0 : 0.4;
+  float kernel_interpolated[4];
 
-  // top right
-	c += (z < texture2D(shadowMap, uv.xy - o.zy).x) ? 1.0 : 0.4;
+  kernel_interpolated[0] = kernel[0] + kernel[1] + kernel[3] + kernel[4];
+  kernel_interpolated[0] /= 4.0;
+  kernel_interpolated[1] = kernel[1] + kernel[2] + kernel[4] + kernel[5];
+  kernel_interpolated[1] /= 4.0;
+  kernel_interpolated[2] = kernel[3] + kernel[4] + kernel[6] + kernel[7];
+  kernel_interpolated[2] /= 4.0;
+  kernel_interpolated[3] = kernel[4] + kernel[5] + kernel[7] + kernel[8];
+  kernel_interpolated[3] /= 4.0;
 
-	return c / 4.0;
+  float a = lerp(kernel_interpolated[0], kernel_interpolated[1], f.y);
+  float b = lerp(kernel_interpolated[2], kernel_interpolated[3], f.y);
+  float c = lerp(a, b, f.x);
+  return c;
 }
 
 //-----------------------------------------------------------------------------
 void SGX_ComputeShadowFactor_PSSM3(in float fDepth,
-							in vec4 vSplitPoints,	
+							in vec4 vSplitPoints,
 							in vec4 lightPosition0,
 							in vec4 lightPosition1,
 							in vec4 lightPosition2,
@@ -92,22 +119,22 @@ void SGX_ComputeShadowFactor_PSSM3(in float fDepth,
 							in sampler2D shadowMap2,
 							in vec4 invShadowMapSize0,
 							in vec4 invShadowMapSize1,
-							in vec4 invShadowMapSize2,																			
+							in vec4 invShadowMapSize2,
 							out float oShadowFactor)
 {
   if (fDepth  <= vSplitPoints.x)
-  {									
+  {
     oShadowFactor =
-      _SGX_ShadowPCF4(shadowMap0, lightPosition0, invShadowMapSize0.xy);				
+      _SGX_ShadowPCF4(shadowMap0, lightPosition0, invShadowMapSize0.xy);
   }
   else if (fDepth <= vSplitPoints.y)
-  {									
+  {
     oShadowFactor =
-      _SGX_ShadowPCF4(shadowMap1, lightPosition1, invShadowMapSize1.xy);		
+      _SGX_ShadowPCF4(shadowMap1, lightPosition1, invShadowMapSize1.xy);
   }
   else
-  {										
+  {
     oShadowFactor =
-      _SGX_ShadowPCF4(shadowMap2, lightPosition2, invShadowMapSize2.xy);				
+      _SGX_ShadowPCF4(shadowMap2, lightPosition2, invShadowMapSize2.xy);
   }
 }
