fde1f4be95ad41de6b02cd679dde15bc67d8bdcb
Ian Chen <ichen@osrfoundation.org>
2014-01-08 15:53:55 -0800

    add noise plugin integration test

--- plugins/RaySensorNoisePlugin.cc
+++ plugins/RaySensorNoisePlugin.cc
@@ -60,7 +60,7 @@ void RaySensorNoisePlugin::Load(sensors::SensorPtr _parent,
 double RaySensorNoisePlugin::OnApplyNoise(double _in)
 {
   // Apply alternating random noise.
-  double randNoise = math::Rand::GetDblNormal(0, this->fixedNoiseRate);
+  double randNoise = math::Rand::GetDblUniform(0, this->fixedNoiseRate);
   this->sign *= -1;
   return _in + this->sign*randNoise*_in;
 }
