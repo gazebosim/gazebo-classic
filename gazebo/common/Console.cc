e0f7975d2f868f8899fbb5fd81945bc323908686
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-01-16 18:29:50 +0100

    Check the correct opening of log file
    Display an error message if failed.

--- gazebo/common/Console.cc
+++ gazebo/common/Console.cc
@@ -57,6 +57,9 @@ void Console::Init(const std::string &_logFilename)
   }
 
   this->logStream = new std::ofstream(logPath.string().c_str(), std::ios::out);
+
+  if (! this->logStream->is_open())
+    gzerr << "Error opening log file: " << logPath << std::endl;
 }
 
 //////////////////////////////////////////////////
