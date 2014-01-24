138f1c2ec14eb0f03b0090e1f0002de6d03597af
John Hsu <hsu@osrfoundation.org>
2014-01-23 18:50:23 -0800

    update unconstrained damped linear motion test

--- test/integration/physics_unconstrained_forced_dynamics.cc
+++ test/integration/physics_unconstrained_forced_dynamics.cc
@@ -44,19 +44,105 @@ void UnconstrainedForced::DampedLinear(const std::string &_physicsEngine)
   ASSERT_TRUE(physics != NULL);
   EXPECT_EQ(physics->GetType(), _physicsEngine);
 
-  // simulate 1 step
-  world->Step(1);
-  double t = world->GetSimTime().Double();
-  // verify that time moves forward
-  EXPECT_GT(t, 0);
+  // set time step size to 0.001
+  double dt = 0.01;
+  physics->SetMaxStepSize(dt);
 
-  // simulate a few steps
-  int steps = 20;
-  world->Step(steps);
-  double dt = world->GetPhysicsEngine()->GetMaxStepSize();
-  EXPECT_GT(dt, 0);
-  t = world->GetSimTime().Double();
-  EXPECT_GT(t, 0.99*dt*static_cast<double>(steps+1));
+  // turn off gravity
+  physics->SetGravity(math::Vector3(0, 0, 0));
+
+  // spawn a box in midair (h = 2)
+  SpawnBox("rigid_body",
+    math::Vector3(1, 1, 1),  // size
+    math::Vector3(0, 0, 2),  // position
+    math::Vector3(0, 0, 0),  // orientation
+    false);  // not static
+
+  // get handle to box model
+  physics::ModelPtr model = world->GetModel("rigid_body");
+  ASSERT_TRUE(model.get() != NULL);
+
+  // get handle to canonical link
+  physics::LinkPtr link =  model->GetLink();
+
+  // set link mass
+  double mass = 1.0;
+  link->GetInertial()->SetMass(mass);
+
+  // set link moi for box
+  double ixx = 1.0 / 12.0 * mass * (2.0);
+  link->GetInertial()->SetInertiaMatrix(ixx, ixx, ixx, 0, 0, 0);
+
+  // declare damping coefficient
+  double damping = 0.1;
+
+  // declare initial velocity of box
+  math::Vector3 v0(1.0, 0, 0);
+
+  // set initial velocity of link
+  link->SetLinearVel(v0);
+
+  // get initial position of link
+  math::Vector3 p0 = link->GetWorldPose().pos;
+
+  // cache initial sim time
+  double t0 = world->GetSimTime().Double();
+
+  // get time step size
+  // double dt = world->GetPhysicsEngine()->GetMaxStepSize();
+
+  // run simulation for 1 second
+  double testDuration = 1.0;  // 1 second
+
+  // calculate number of steps needed
+  unsigned int steps = testDuration / dt;
+
+  // compute first order integrated velocity
+  double v1 = v0.x;
+
+  for (unsigned int step = 0; step < steps; ++step)
+  {
+    double t = world->GetSimTime().Double();
+
+    // compute analytical velocity
+    math::Vector3 va = v0 * exp(-damping / mass * (t - t0));
+
+    // compute analytical position by integrating velocity
+    math::Vector3 pa = p0 - mass * v0 / damping * (
+      exp(-damping / mass * t) -
+      exp(-damping / mass * t0));
+
+    // get simulation position and velocity
+    math::Vector3 p = link->GetWorldPose().pos;
+    math::Vector3 v = link->GetWorldLinearVel();
+
+    // compute damping force
+    math::Vector3 dampingForce = -damping * v;
+
+    // debug results
+    gzdbg << "engine: [" << _physicsEngine
+          << "] t: [" << t
+          << "] v: [" << v
+          << "] vdebug: [" << v1
+          << "] va: [" << va
+          << "] p: [" << p
+          << "] pa: [" << pa
+          << "] fd: [" << dampingForce
+          << "]\n";
+
+    // check results
+    EXPECT_GT(dt, 0);
+
+
+    // apply force to simulate damping force
+    link->SetForce(dampingForce);
+
+    // take a step
+    world->Step(1);
+
+    // compute first order integrated velocity
+    v1 = v1 + dt * dampingForce.x / mass;
+  }
 }
 
 TEST_P(UnconstrainedForced, DampedLinear)
