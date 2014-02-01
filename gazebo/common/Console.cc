c5e14390c9f376fe1388d1f59080fdd86ef525b1
Carlos Agüero <caguero@osrfoundation.org>
2014-02-01 10:50:24 -0800

    Set copyright date to 2014 and add a missing #include directive.

--- gazebo/common/Console.cc
+++ gazebo/common/Console.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Open Source Robotics Foundation
+ * Copyright 2012-2014 Open Source Robotics Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 #include <string.h>
 #include <boost/filesystem.hpp>
 #include <sstream>
+#include <string>
 
 #include "gazebo/common/Exception.hh"
 #include "gazebo/common/Time.hh"
