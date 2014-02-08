3a3ca2b4487b1993f42b2fa0d6ba0e3438ef0fc3
Jeongseok Lee <jslee02@gmail.com>
2014-02-08 00:35:25 -0500

    Remove unnecessary lines in DARTModel.hh

--- gazebo/physics/dart/DARTModel.hh
+++ gazebo/physics/dart/DARTModel.hh
@@ -54,9 +54,6 @@ namespace gazebo
       // Documentation inherited.
       public: virtual void Fini();
 
-      // Documentation inherited.
-      // public: void Reset();
-
       /// \brief
       public: void BackupState();
 
