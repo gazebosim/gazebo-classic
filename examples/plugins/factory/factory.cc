ed94a073fe86eb6a458ca3b459b39f9e54eec1f2
Steve Peters <scpeters@osrfoundation.org>
2014-02-10 01:25:46 -0800

    more whitespace

--- examples/plugins/factory/factory.cc
+++ examples/plugins/factory/factory.cc
@@ -33,21 +33,21 @@ class Factory : public WorldPlugin
     sdf::SDF sphereSDF;
     sphereSDF.SetFromString(
        "<sdf version ='1.4'>\
-         <model name ='sphere'>\
-           <pose>1 0 0 0 0 0</pose>\
-           <link name ='link'>\
-             <pose>0 0 .5 0 0 0</pose>\
-             <collision name ='collision'>\
-               <geometry>\
-                 <sphere><radius>0.5</radius></sphere>\
-               </geometry>\
-             </collision>\
-             <visual name ='visual'>\
-               <geometry>\
-                 <sphere><radius>0.5</radius></sphere>\
-               </geometry>\
-             </visual>\
-           </link>\
+          <model name ='sphere'>\
+            <pose>1 0 0 0 0 0</pose>\
+            <link name ='link'>\
+              <pose>0 0 .5 0 0 0</pose>\
+              <collision name ='collision'>\
+                <geometry>\
+                  <sphere><radius>0.5</radius></sphere>\
+                </geometry>\
+              </collision>\
+              <visual name ='visual'>\
+                <geometry>\
+                  <sphere><radius>0.5</radius></sphere>\
+                </geometry>\
+              </visual>\
+            </link>\
           </model>\
         </sdf>");
     // Demonstrate using a custom model name.
