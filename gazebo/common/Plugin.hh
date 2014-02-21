8799c183cb9d570c9ef9bb8d2ee706713dbbbe10
Steven Peters <scpeters@osrfoundation.org>
2014-02-21 03:15:38 +0000

    Plugin.hh edited online with Bitbucket: add comment about #1026

--- gazebo/common/Plugin.hh
+++ gazebo/common/Plugin.hh
@@ -84,6 +84,8 @@ namespace gazebo
     public: virtual ~PluginT()
             {
 #ifdef HAVE_DL
+// The following ifdef is a workaround for #1026.
+// \todo Figure out the right thing to do.
 #ifndef __APPLE__
               dlclose(this->dlHandle);
 #endif  // ifndef __APPLE
