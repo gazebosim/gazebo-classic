575278801f39e6d8e8a7d517a99f678741e54c4a
Nate Koenig <natekoenig@gmail.com>
2014-03-04 17:26:38 -0800

    Added tests for submeshes

--- test/integration/concave_mesh.cc
+++ test/integration/concave_mesh.cc
@@ -26,7 +26,65 @@ class ConcaveMeshTest : public ServerFixture,
                         public testing::WithParamInterface<const char*>
 {
   public: void RayTest(const std::string &_physicsEngine);
+  public: void SubmeshNoCollisionTest(const std::string &_physicsEngine);
+  public: void SubmeshCollisionTest(const std::string &_physicsEngine);
 };
+
+/////////////////////////////////////////////////
+void ConcaveMeshTest::SubmeshNoCollisionTest(const std::string &_physicsEngine)
+{
+  Load("worlds/concave_submesh_no_collision_test.world", true, _physicsEngine);
+  physics::WorldPtr world = physics::get_world("default");
+  EXPECT_TRUE(world != NULL);
+
+  world->Step(100);
+
+  sensors::RaySensorPtr raySensor =
+    boost::dynamic_pointer_cast<sensors::RaySensor>(
+    sensors::get_sensor("default::hokuyo::link::laser"));
+  EXPECT_TRUE(raySensor != NULL);
+
+  // No ray should interect a collision.
+  for (int i = 0; i < raySensor->GetRangeCount(); ++i)
+    EXPECT_NEAR(raySensor->GetRange(i), raySensor->GetRangeMax(), 1e-2);
+}
+
+/////////////////////////////////////////////////
+void ConcaveMeshTest::SubmeshCollisionTest(const std::string &_physicsEngine)
+{
+  Load("worlds/concave_submesh_collision_test.world", true, _physicsEngine);
+  physics::WorldPtr world = physics::get_world("default");
+  EXPECT_TRUE(world != NULL);
+
+  world->Step(100);
+
+  sensors::RaySensorPtr raySensor =
+    boost::dynamic_pointer_cast<sensors::RaySensor>(
+    sensors::get_sensor("default::hokuyo::link::laser"));
+  EXPECT_TRUE(raySensor != NULL);
+
+  EXPECT_NEAR(raySensor->GetRange(0), 10, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(1), 10, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(2), 10, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(3), 10, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(4), 10, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(5), 10, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(6), 10, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(7), 10, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(8), 1.92439, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(9), 1.86443, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(10), 1.86443, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(11), 1.92439, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(12), 10, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(13), 10, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(14), 10, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(15), 10, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(16), 10, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(17), 10, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(18), 10, 1e-2);
+  EXPECT_NEAR(raySensor->GetRange(19), 10, 1e-2);
+
+}
 
 /////////////////////////////////////////////////
 void ConcaveMeshTest::RayTest(const std::string &_physicsEngine)
@@ -62,6 +120,34 @@ void ConcaveMeshTest::RayTest(const std::string &_physicsEngine)
   EXPECT_NEAR(raySensor->GetRange(17), 10, 1e-2);
   EXPECT_NEAR(raySensor->GetRange(18), 10, 1e-2);
   EXPECT_NEAR(raySensor->GetRange(19), 10, 1e-2);
+}
+
+/////////////////////////////////////////////////
+TEST_P(ConcaveMeshTest, SubmeshCollisionTest)
+{
+  if (std::string(GetParam()) == "simbody" ||
+      std::string(GetParam()) == "dart")
+  {
+    gzdbg << "ConcaveMeshes not supported in " << GetParam() << "\n";
+  }
+  else
+  {
+    SubmeshCollisionTest(GetParam());
+  }
+}
+
+/////////////////////////////////////////////////
+TEST_P(ConcaveMeshTest, SubmeshNoCollisionTest)
+{
+  if (std::string(GetParam()) == "simbody" ||
+      std::string(GetParam()) == "dart")
+  {
+    gzdbg << "ConcaveMeshes not supported in " << GetParam() << "\n";
+  }
+  else
+  {
+    SubmeshNoCollisionTest(GetParam());
+  }
 }
 
 /////////////////////////////////////////////////
