010bd383c9d635f70dd4815196531c7a4388a09f
Nate Koenig <natekoenig@gmail.com>
2014-03-11 08:38:38 -0700

    Commented our macros

--- deps/opende/OPCODE/Opcode.h
+++ deps/opende/OPCODE/Opcode.h
@@ -1,17 +1,17 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
- *	OPCODE - Optimized Collision Detection
- *	Copyright (C) 2001 Pierre Terdiman
- *	Homepage: http://www.codercorner.com/Opcode.htm
+ *  OPCODE - Optimized Collision Detection
+ *  Copyright (C) 2001 Pierre Terdiman
+ *  Homepage: http://www.codercorner.com/Opcode.htm
  */
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /**
- *	Main file for Opcode.dll.
- *	\file		Opcode.h
- *	\author		Pierre Terdiman
- *	\date		March, 20, 2001
+ *  Main file for Opcode.dll.
+ *  \file  	Opcode.h
+ *  \author  	Pierre Terdiman
+ *  \date  	March, 20, 2001
  */
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -51,59 +51,70 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Compilation messages
 #ifdef _MSC_VER
-	#if defined(OPCODE_EXPORTS)
-		// #pragma message("Compiling OPCODE")
-	#elif !defined(OPCODE_EXPORTS)
-		// #pragma message("Using OPCODE")
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		// Automatic linking
-		#ifndef BAN_OPCODE_AUTOLINK
-			#ifdef _DEBUG
-				//#pragma comment(lib, "Opcode_D.lib")
-			#else
-				//#pragma comment(lib, "Opcode.lib")
-			#endif
-		#endif
-	#endif
+  #if defined(OPCODE_EXPORTS)
+    // #pragma message("Compiling OPCODE")
+  #elif !defined(OPCODE_EXPORTS)
+    // #pragma message("Using OPCODE")
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Automatic linking
+    #ifndef BAN_OPCODE_AUTOLINK
+    	#ifdef _DEBUG
+    		//#pragma comment(lib, "Opcode_D.lib")
+    	#else
+    		//#pragma comment(lib, "Opcode.lib")
+    	#endif
+    #endif
+  #endif
 #endif
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////
+// Preprocessor
+// #ifndef ICE_NO_DLL
+//   #ifdef OPCODE_EXPORTS
+//     #define OPCODE_API// __declspec(dllexport)
+//   #else
+//     #define OPCODE_API// __declspec(dllimport)
+//   #endif
+// #else
+//   #define OPCODE_API
+// #endif
+
 // Preprocessor__
 #define OPCODE_API GAZEBO_VISIBLE
 
-	#include "OPC_Settings.h"
-	#include "OPC_IceHook.h"
+  #include "OPC_Settings.h"
+  #include "OPC_IceHook.h"
 
-	namespace Opcode
-	{
-		// Bulk-of-the-work
-		#include "OPC_Common.h"
-		#include "OPC_MeshInterface.h"
-		// Builders
-		#include "OPC_TreeBuilders.h"
-		// Trees
-		#include "OPC_AABBTree.h"
-		#include "OPC_OptimizedTree.h"
-		// Models
-		#include "OPC_BaseModel.h"
-		#include "OPC_Model.h"
-		#include "OPC_HybridModel.h"
-		// Colliders
-		#include "OPC_Collider.h"
-		#include "OPC_VolumeCollider.h"
-		#include "OPC_TreeCollider.h"
-		#include "OPC_RayCollider.h"
-		#include "OPC_SphereCollider.h"
-		#include "OPC_OBBCollider.h"
-		#include "OPC_AABBCollider.h"
-		#include "OPC_LSSCollider.h"
-		#include "OPC_PlanesCollider.h"
-		// Usages
-		#include "OPC_Picking.h"
+  namespace Opcode
+  {
+    // Bulk-of-the-work
+    #include "OPC_Common.h"
+    #include "OPC_MeshInterface.h"
+    // Builders
+    #include "OPC_TreeBuilders.h"
+    // Trees
+    #include "OPC_AABBTree.h"
+    #include "OPC_OptimizedTree.h"
+    // Models
+    #include "OPC_BaseModel.h"
+    #include "OPC_Model.h"
+    #include "OPC_HybridModel.h"
+    // Colliders
+    #include "OPC_Collider.h"
+    #include "OPC_VolumeCollider.h"
+    #include "OPC_TreeCollider.h"
+    #include "OPC_RayCollider.h"
+    #include "OPC_SphereCollider.h"
+    #include "OPC_OBBCollider.h"
+    #include "OPC_AABBCollider.h"
+    #include "OPC_LSSCollider.h"
+    #include "OPC_PlanesCollider.h"
+    // Usages
+    #include "OPC_Picking.h"
 
 
-		FUNCTION OPCODE_API bool InitOpcode();
-		FUNCTION OPCODE_API bool CloseOpcode();
-	}
+    FUNCTION OPCODE_API bool InitOpcode();
+    FUNCTION OPCODE_API bool CloseOpcode();
+  }
 
 #endif // __OPCODE_H__
