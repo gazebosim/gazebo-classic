0fecc303c6852300263a7cd40d4d44cd13dbef3b
Nathan Koenig <natekoenig@gmail.com>
2014-03-13 20:13:41 +0000

    Added comments

--- tools/website/backup.rb
+++ tools/website/backup.rb
@@ -1,8 +1,23 @@
 #!/usr/bin/env ruby
+
+# Copyright (C) 2012-2014 Open Source Robotics Foundation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Description:
 # This script removes old snapshots of the gazebosim.org server, and creates
 # a new snapshot if the most recent snapshot is older than 1 day. Snapshots
 # are removed if they are older than one week.
+#
+# A cronjob runs on gazebosim.org that assumes this file is located in /home/ubuntu/bin/
 
 require 'rubygems'
 require 'net/http'
