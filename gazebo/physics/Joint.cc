ced95c884e708f0b7024e50b410800bf30c8e129
Nathan Koenig <natekoenig@gmail.com>
2014-03-13 17:41:56 +0000

    Pass by reference

--- gazebo/physics/Joint.cc
+++ gazebo/physics/Joint.cc
@@ -609,7 +609,7 @@ void Joint::ApplyStiffnessDamping()
 }
 
 //////////////////////////////////////////////////
-double Joint::GetInertiaRatio(const math::Vector3 _axis) const
+double Joint::GetInertiaRatio(const math::Vector3 &_axis) const
 {
   if (this->parentLink && this->childLink)
   {
