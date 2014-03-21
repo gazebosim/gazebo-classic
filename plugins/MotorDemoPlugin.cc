e48188b9743423d3c1bda92b756cc3440eba317d
Carlos Agüero <caguero@osrfoundation.org>
2014-03-21 16:29:14 -0700

    [plugins/MotorDemoPlugin.cc] Minor style fix and license date.

--- plugins/MotorDemoPlugin.cc
+++ plugins/MotorDemoPlugin.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2013 Open Source Robotics Foundation
+ * Copyright (C) 2014 Open Source Robotics Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,8 +34,7 @@ MotorDemoPlugin::MotorDemoPlugin() : maxTorque(0.0), torqueSpeedSlope(-1.0)
 }
 
 /////////////////////////////////////////////////
-void MotorDemoPlugin::Load(physics::ModelPtr _model,
-                     sdf::ElementPtr _sdf)
+void MotorDemoPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
 {
   GZ_ASSERT(_model, "MotorDemoPlugin _model pointer is NULL");
   this->model = _model;
