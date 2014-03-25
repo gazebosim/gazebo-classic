3022182eaf941a373f7e11aa5aaf4d58651cdd48
John Hsu <hsu@osrfoundation.org>
2014-03-25 14:09:29 -0700

    recover ODEPhysics::OnPhysicsMsg lost in merges

--- gazebo/physics/ode/ODEPhysics.cc
+++ gazebo/physics/ode/ODEPhysics.cc
@@ -271,49 +271,53 @@ void ODEPhysics::OnRequest(ConstRequestPtr &_msg)
 void ODEPhysics::OnPhysicsMsg(ConstPhysicsPtr &_msg)
 {
   if (_msg->has_solver_type())
-  {
     this->SetStepType(_msg->solver_type());
-  }
 
   if (_msg->has_min_step_size())
-  {
     this->SetParam(MIN_STEP_SIZE, _msg->min_step_size());
-  }
 
   if (_msg->has_precon_iters())
-  {
     this->SetSORPGSPreconIters(_msg->precon_iters());
-  }
 
   if (_msg->has_iters())
-  {
     this->SetSORPGSIters(_msg->iters());
-  }
 
   if (_msg->has_sor())
-  {
     this->SetSORPGSW(_msg->sor());
-  }
 
   if (_msg->has_cfm())
-  {
     this->SetWorldCFM(_msg->cfm());
-  }
 
   if (_msg->has_erp())
-  {
     this->SetWorldERP(_msg->erp());
-  }
+
+  if (_msg->has_enable_physics())
+    this->world->EnablePhysicsEngine(_msg->enable_physics());
 
   if (_msg->has_contact_max_correcting_vel())
-  {
     this->SetContactMaxCorrectingVel(_msg->contact_max_correcting_vel());
-  }
 
   if (_msg->has_contact_surface_layer())
-  {
     this->SetContactSurfaceLayer(_msg->contact_surface_layer());
+
+  if (_msg->has_gravity())
+    this->SetGravity(msgs::Convert(_msg->gravity()));
+
+  if (_msg->has_real_time_factor())
+    this->SetTargetRealTimeFactor(_msg->real_time_factor());
+
+  if (_msg->has_real_time_update_rate())
+  {
+    this->SetRealTimeUpdateRate(_msg->real_time_update_rate());
   }
+
+  if (_msg->has_max_step_size())
+  {
+    this->SetMaxStepSize(_msg->max_step_size());
+  }
+
+  /// Make sure all models get at least on update cycle.
+  this->world->EnableAllModels();
 
   // Parent class handles many generic parameters
   PhysicsEngine::OnPhysicsMsg(_msg);
