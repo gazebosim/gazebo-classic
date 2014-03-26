f015d51f75914b284688bd3b6c5ed4b9972caf31
Ian Chen <ichen@osrfoundation.org>
2014-03-25 20:09:41 -0700

    transform bbox

--- gazebo/rendering/Camera.cc
+++ gazebo/rendering/Camera.cc
@@ -1496,7 +1496,7 @@ bool Camera::IsVisible(VisualPtr _visual)
     Ogre::AxisAlignedBox box;
     box.setMinimum(bbox.min.x, bbox.min.y, bbox.min.z);
     box.setMaximum(bbox.max.x, bbox.max.y, bbox.max.z);
-
+    box.transformAffine(_visual->GetSceneNode()->_getFullTransform());
     return this->camera->isVisible(box);
   }
 
