0eefe3a47649476c514c7cefe5d629c70c2d4167
John Hsu <hsu@osrfoundation.org>
2014-03-27 16:58:11 -0700

    const another catch

--- gazebo/physics/ode/ODEScrewJoint.cc
+++ gazebo/physics/ode/ODEScrewJoint.cc
@@ -202,7 +202,7 @@ bool ODEScrewJoint::SetParam(const std::string &_key,
     {
       this->threadPitch = boost::any_cast<double>(_value);
     }
-    catch(boost::bad_any_cast &e)
+    catch(const boost::bad_any_cast &e)
     {
       gzerr << "boost any_cast error:" << e.what() << "\n";
       return false;
