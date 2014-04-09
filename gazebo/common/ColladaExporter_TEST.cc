2540218c45523de0f8e0b7de070553fc1ede8ba5
chapulina <burajiru.no.chapulina@gmail.com>
2014-04-09 17:42:00 -0300

    added xml test, cordless_drill test

--- gazebo/common/ColladaExporter_TEST.cc
+++ gazebo/common/ColladaExporter_TEST.cc
@@ -15,6 +15,7 @@
  *
 */
 #include <gtest/gtest.h>
+#include <tinyxml.h>
 
 #include "test_config.h"
 #include "gazebo/common/Mesh.hh"
@@ -34,10 +35,70 @@ TEST_F(ColladaExporter, ExportBox)
       std::string(PROJECT_SOURCE_PATH) + "/test/data/box.dae");
 
   common::ColladaExporter exporter;
-  exporter.Export(meshOriginal, std::string(PROJECT_SOURCE_PATH) + "/test/data/exported.dae");
+  exporter.Export(meshOriginal, std::string(PROJECT_SOURCE_PATH) + "/test/data/box_exported.dae");
 
+  // Check .dae file
+  TiXmlDocument xmlDoc;
+  EXPECT_TRUE(xmlDoc.LoadFile(std::string(PROJECT_SOURCE_PATH) + "/test/data/box_exported.dae"));
+
+  const char *countDae = xmlDoc.FirstChildElement("COLLADA")
+                               ->FirstChildElement("library_geometries")
+                               ->FirstChildElement("geometry")
+                               ->FirstChildElement("mesh")
+                               ->FirstChildElement("source")
+                               ->FirstChildElement("float_array")
+                               ->Attribute("count");
+  unsigned int countMeshInt = meshOriginal->GetSubMesh(0)->GetVertexCount()*3;
+  char countMesh[100];
+  snprintf(countMesh, sizeof(countMesh), "%u", countMeshInt);
+
+  EXPECT_STREQ(countDae, countMesh);
+
+  // Reload mesh and compare
   const common::Mesh *meshReloaded = loader.Load(
-      std::string(PROJECT_SOURCE_PATH) + "/test/data/exported.dae");
+      std::string(PROJECT_SOURCE_PATH) + "/test/data/box_exported.dae");
+
+  EXPECT_EQ(meshOriginal->GetName(), meshReloaded->GetName());
+  EXPECT_EQ(meshOriginal->GetMax(), meshReloaded->GetMax());
+  EXPECT_EQ(meshOriginal->GetMin(), meshReloaded->GetMin());
+  EXPECT_EQ(meshOriginal->GetSubMeshCount(), meshReloaded->GetSubMeshCount());
+  EXPECT_EQ(meshOriginal->GetMaterialCount(), meshReloaded->GetMaterialCount());
+  EXPECT_EQ(meshOriginal->GetVertexCount(), meshReloaded->GetVertexCount());
+  EXPECT_EQ(meshOriginal->GetNormalCount(), meshReloaded->GetNormalCount());
+  EXPECT_EQ(meshOriginal->GetTexCoordCount(), meshReloaded->GetTexCoordCount());
+  EXPECT_EQ(meshOriginal->GetIndexCount(), meshReloaded->GetIndexCount());
+}
+
+/////////////////////////////////////////////////
+TEST_F(ColladaExporter, ExportCordlessDrill)
+{
+  common::ColladaLoader loader;
+  const common::Mesh *meshOriginal = loader.Load(
+      std::string(PROJECT_SOURCE_PATH) + "/test/data/cordless_drill.dae");
+
+  common::ColladaExporter exporter;
+  exporter.Export(meshOriginal, std::string(PROJECT_SOURCE_PATH) + "/test/data/cordless_drill_exported.dae");
+
+  // Check .dae file
+  TiXmlDocument xmlDoc;
+  EXPECT_TRUE(xmlDoc.LoadFile(std::string(PROJECT_SOURCE_PATH) + "/test/data/cordless_drill_exported.dae"));
+
+  const char *countDae = xmlDoc.FirstChildElement("COLLADA")
+                               ->FirstChildElement("library_geometries")
+                               ->FirstChildElement("geometry")
+                               ->FirstChildElement("mesh")
+                               ->FirstChildElement("source")
+                               ->FirstChildElement("float_array")
+                               ->Attribute("count");
+  unsigned int countMeshInt = meshOriginal->GetSubMesh(0)->GetVertexCount()*3;
+  char countMesh[100];
+  snprintf(countMesh, sizeof(countMesh), "%u", countMeshInt);
+
+  EXPECT_STREQ(countDae, countMesh);
+
+  // Reload mesh and compare
+  const common::Mesh *meshReloaded = loader.Load(
+      std::string(PROJECT_SOURCE_PATH) + "/test/data/cordless_drill_exported.dae");
 
   EXPECT_EQ(meshOriginal->GetName(), meshReloaded->GetName());
   EXPECT_EQ(meshOriginal->GetMax(), meshReloaded->GetMax());
