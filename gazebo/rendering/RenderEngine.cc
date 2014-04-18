3bc1d67dcb62de6c4777ed9dfd443bb9033ce5b5
Carlos Agüero <caguero@osrfoundation.org>
2014-04-18 16:47:45 -0700

    Adding an #ifdef in RenderEngine.cc

--- gazebo/rendering/RenderEngine.cc
+++ gazebo/rendering/RenderEngine.cc
@@ -407,7 +407,10 @@ void RenderEngine::LoadPlugins()
     plugins.push_back(path+"/"+prefix+"Plugin_ParticleFX");
     plugins.push_back(path+"/"+prefix+"Plugin_BSPSceneManager");
     plugins.push_back(path+"/"+prefix+"Plugin_OctreeSceneManager");
+
+#ifdef HAVE_OCULUS
     plugins.push_back(path+"/Plugin_CgProgramManager");
+#endif
 
     for (piter = plugins.begin(); piter != plugins.end(); ++piter)
     {
