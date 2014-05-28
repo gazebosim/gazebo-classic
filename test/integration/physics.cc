c7b54eceb8be11c24292370b67e432b997802d91
Steve Peters <scpeters@osrfoundation.org>
2014-05-28 09:30:39 -0700

    Add failing test for #1193

--- test/integration/physics.cc
+++ test/integration/physics.cc
@@ -71,6 +71,15 @@ void PhysicsTest::EmptyWorld(const std::string &_physicsEngine)
   EXPECT_GT(dt, 0);
   t = world->GetSimTime().Double();
   EXPECT_GT(t, 0.99*dt*static_cast<double>(steps+1));
+
+  // test World::GetRealTime()
+  world->SetPaused(true);
+  double startRealTime = world->GetRealTime().Double();
+  for(unsigned int i = 0; i < 2000; ++i)
+  {
+    world->Step(1);
+    EXPECT_GT(world->GetRealTime().Double(), startRealTime);
+  }
 }
 
 TEST_P(PhysicsTest, EmptyWorld)
