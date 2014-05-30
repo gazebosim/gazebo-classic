a7ac1d683620115bf3b84bdbefedef7e362f1251
Nate Koenig <natekoenig@gmail.com>
2014-05-30 09:10:51 -0700

    Added plugins to test/examples

--- test/examples/example_plugins.cc
+++ test/examples/example_plugins.cc
@@ -79,6 +79,8 @@ TEST_P(ExamplePlugins, Build)
 }
 
 INSTANTIATE_TEST_CASE_P(ExamplePlugins, ExamplePlugins, ::testing::Values(
+  "animate_joints",
+  "animate_pose",
   "hello_world"
   , "world_edit"
   , "model_push"
