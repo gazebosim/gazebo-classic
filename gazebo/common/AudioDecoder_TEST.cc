7259786e0b22866f4f4204f133feccbf14a17581
Nate Koenig <natekoenig@gmail.com>
2014-06-30 10:42:03 -0700

    Fixed test

--- gazebo/common/AudioDecoder_TEST.cc
+++ gazebo/common/AudioDecoder_TEST.cc
@@ -148,9 +148,10 @@ TEST(AudioDecoder, CheerFile)
     EXPECT_EQ(audio.GetSampleRate(), 44100);
 
     audio.Decode(&dataBuffer, &dataBufferSize);
-    gzerr << "Disable OGG buffer size check per #1189"
-          << std::endl;
-    // EXPECT_EQ(dataBufferSize, 4989184u);
+    // In Ubuntu trusty the buffer size double for ogg decoding.
+    // This check is suitable for both older and newer versions of Ubuntu.
+    EXPECT_TRUE(dataBufferSize == 4989184u ||
+                dataBufferSize == 4989184u * 2u);
   }
 
   // MP3
