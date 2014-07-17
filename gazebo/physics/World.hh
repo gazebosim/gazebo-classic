ba2d1efa5ac9071b0d8cbd945a4909914d07392a
Carlos Agüero <caguero@osrfoundation.org>
2014-07-16 19:34:41 -0700

    Adding some doxygen.

--- gazebo/physics/World.hh
+++ gazebo/physics/World.hh
@@ -460,42 +460,90 @@ namespace gazebo
       /// \param[in] _msg Pointer to the light message.
       private: void OnLightMsg(ConstLightPtr &_msg);
 
-      /// \brief
+      /// \brief Populate a vector of poses with '_modelCount' elements,
+      /// randomly distributed within a cuboid.
+      /// \param[in] _modelCount Number of poses.
+      /// \param[in] _cuboid SDF element containing the SDF properties of the
+      /// cuboid.
+      /// \param[out] _poses Vector containing the poses that will be used to
+      /// populate models.
       private: bool PopulateCuboidRandom(int _modelCount,
                                          const sdf::ElementPtr _cuboid,
                                          std::vector<math::Vector3> &_poses);
 
-      /// \brief
+      /// \brief Populate a vector of poses with '_modelCount' elements,
+      /// uniformly distributed within a cuboid. We use k-means to split the
+      /// cuboid in similar subregions.
+      /// \param[in] _modelCount Number of poses.
+      /// \param[in] _cuboid SDF element containing the SDF properties of the
+      /// cuboid.
+      /// \param[out] _poses Vector containing the poses that will be used to
+      /// populate models.
       private: bool PopulateCuboidUniform(int _modelCount,
                                           const sdf::ElementPtr _cuboid,
                                           std::vector<math::Vector3> &_poses);
 
-      /// \brief
+      /// \brief Populate a vector of poses with '_modelCount' elements,
+      /// evenly placed in a 2D grid pattern.
+      /// \param[in] _cuboid SDF element containing the SDF properties of the
+      /// cuboid.
+      /// \param[out] _poses Vector containing the poses that will be used to
+      /// populate models.
       private: bool PopulateCuboidGrid(const sdf::ElementPtr _cuboid,
                                        std::vector<math::Vector3> &_poses);
 
+      /// \brief Populate a vector of poses with '_modelCount' elements,
+      /// evenly placed in a row along the global x-axis.
+      /// \param[in] _modelCount Number of poses.
+      /// \param[in] _cuboid SDF element containing the SDF properties of the cuboid
+      /// \param[out] _poses Vector containing the poses that will be used to
+      /// populate models.
       private: bool PopulateCuboidLinearX(int _modelCount,
                                           const sdf::ElementPtr _cuboid,
                                           std::vector<math::Vector3> &_poses);
 
+      /// \brief Populate a vector of poses with '_modelCount' elements,
+      /// evenly placed in a row along the global y-axis.
+      /// \param[in] _modelCount Number of poses.
+      /// \param[in] _cuboid SDF element containing the SDF properties of the cuboid
+      /// \param[out] _poses Vector containing the poses that will be used to
+      /// populate models.
       private: bool PopulateCuboidLinearY(int _modelCount,
                                           const sdf::ElementPtr _cuboid,
                                           std::vector<math::Vector3> &_poses);
 
+      /// \brief Populate a vector of poses with '_modelCount' elements,
+      /// evenly placed in a row along the global z-axis.
+      /// \param[in] _modelCount Number of poses.
+      /// \param[in] _cuboid SDF element containing the SDF properties of the cuboid
+      /// \param[out] _poses Vector containing the poses that will be used to
+      /// populate models.
       private: bool PopulateCuboidLinearZ(int _modelCount,
                                           const sdf::ElementPtr _cuboid,
                                           std::vector<math::Vector3> &_poses);
 
-       /// \brief
+      /// \brief Populate a vector of poses with '_modelCount' elements,
+      /// randomly distributed within a cylinder.
+      /// \param[in] _modelCount Number of poses.
+      /// \param[in] _cylinder SDF element containing the SDF properties of the
+      /// cylinder.
+      /// \param[out] _poses Vector containing the poses that will be used to
+      /// populate models.
       private: bool PopulateCylinderRandom(int _modelCount,
                                            const sdf::ElementPtr _cylinder,
                                            std::vector<math::Vector3> &_poses);
 
-      /// \brief
+      /// \brief Populate a vector of poses with '_modelCount' elements,
+      /// uniformly distributed within a cylinder. We use k-means to split the
+      /// cylinder in similar subregions.
+      /// \param[in] _modelCount Number of poses.
+      /// \param[in] _cuboid SDF element containing the SDF properties of the
+      /// cuboid.
+      /// \param[out] _poses Vector containing the poses that will be used to
+      /// populate models.
       private: bool PopulateCylinderUniform(int _modelCount,
                                             const sdf::ElementPtr _cylinder,
                                             std::vector<math::Vector3> &_poses);
-
 
       /// \brief Insert into the world a population of models.
       /// \param[in] _pop SDF population element.
