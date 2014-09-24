869a620f0d704e2e214afad033622d45cab06f8d
John Hsu <hsu@osrfoundation.org>
2014-09-24 15:49:49 -0700

    add controller responser

---
+++ examples/plugins/model_control_responser/responser.cc
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2012-2014 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <gazebo/transport/transport.hh>
+#include <gazebo/msgs/msgs.hh>
+#include <gazebo/gazebo.hh>
+
+#include <iostream>
+
+/////////////////////////////////////////////////
+// Function is called everytime a message is received.
+void cb(ConstControlRequestPtr &_msg)
+{
+  // Dump the message contents to stdout.
+  std::cout << _msg->DebugString();
+}
+
+/////////////////////////////////////////////////
+int main(int _argc, char **_argv)
+{
+  // Load gazebo
+  gazebo::setupClient(_argc, _argv);
+
+  // Create our node for communication
+  gazebo::transport::NodePtr node(new gazebo::transport::Node());
+  node->Init();
+
+  // Listen to Gazebo world_stats topic
+  gazebo::transport::SubscriberPtr sub =
+    node->Subscribe("~/ur10/control_request", cb);
+
+  // Busy wait loop...replace with your own code as needed.
+  while (true)
+    gazebo::common::Time::MSleep(10);
+
+  // Make sure to shut everything down.
+  gazebo::shutdown();
+}
