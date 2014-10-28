1e7fe51543e66a995b5bc276b2e48b0e9be668d0
Nate Koenig <natekoenig@gmail.com>
2014-10-28 07:22:48 -0700

    Modified migration and changelog. Improved GLWidget.cc

--- gazebo/gui/GLWidget.cc
+++ gazebo/gui/GLWidget.cc
@@ -311,13 +311,9 @@ void GLWidget::keyPressEvent(QKeyEvent *_event)
   }
 
   // Process Key Events
-
-  bool handled = KeyEventHandler::Instance()->HandlePress(this->keyEvent);
-
-  if (!handled)
+  if (!KeyEventHandler::Instance()->HandlePress(this->keyEvent))
   {
     ModelManipulator::Instance()->OnKeyPressEvent(this->keyEvent);
-
     this->userCamera->HandleKeyPressEvent(this->keyText);
   }
 }
