17f65b70f5c1bb1ffb7e135c3d918ae58fbc0c8a
Steve Peters <scpeters@osrfoundation.org>
2014-11-06 07:37:04 -0800

    Fix build and ensure backward compatibility

--- gazebo/physics/ode/ODEPhysics.cc
+++ gazebo/physics/ode/ODEPhysics.cc
@@ -181,8 +181,11 @@ void ODEPhysics::Load(sdf::ElementPtr _sdf)
   sdf::ElementPtr solverElem = odeElem->GetElement("solver");
 
   this->stepType = solverElem->Get<std::string>("type");
-  dWorldSetQuickStepInertiaRatioReduction(this->worldId,
-    solverElem->Get<bool>("use_dynamic_moi_rescaling");
+  if (solverElem->HasElement("use_dynamic_moi_rescaling"))
+  {
+    dWorldSetQuickStepInertiaRatioReduction(this->worldId,
+      solverElem->Get<bool>("use_dynamic_moi_rescaling"));
+  }
 
   dWorldSetDamping(this->worldId, 0.0001, 0.0001);
 
