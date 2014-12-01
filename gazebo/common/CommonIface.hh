7d588f36d621e453622f929446a6179a84067602
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-12-01 18:50:37 +0100

    Fix problem of defining GZ_COMMON_VISIBLE globally

--- gazebo/common/CommonIface.hh
+++ gazebo/common/CommonIface.hh
@@ -68,7 +68,6 @@ namespace gazebo
     /// function are std::string and any STL container.
     /// \return The string representation (40 character) of the SHA1 hash.
     template<typename T>
-    GZ_COMMON_VISIBLE
     std::string get_sha1(const T &_buffer);
 
     /// \}
