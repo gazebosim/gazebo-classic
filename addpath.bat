ed8039e1635ed1c29d4d78c6edc306ab35037bcd
Brian Gerkey <gerkey@osrfoundation.org>
2014-12-04 11:38:33 -0800

    added a hacky script to adjust PATH to include the DLLs that we care about

---
+++ addpath.bat
@@ -0,0 +1,2 @@
+@REM Meant to be run from inside build/
+@set PATH=%cd%\..\..\boost_1_56_0\lib64-msvc-12.0;%cd%\..\..\FreeImage-vc12-x64-release-debug\x64\Release\DLL;%cd%\deps\opende;%cd%\..\..\OgreSDK_vc11_x64_v1-9-0unstable\bin\Release;%cd%\..\..\sdformat\install\build\Release\lib;%cd%\..\..\tbb43_20141023oss\bin\intel64\vc12;%cd%\deps\libccd;%cd%\deps\opende\OPCODE;%cd%\deps\opende\ou;%PATH%
