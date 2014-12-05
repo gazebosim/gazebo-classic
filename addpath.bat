fefa6dbcc6037ae0de7de08e40a4ad2fb020c976
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-12-05 20:58:47 +0100

    Use absolute paths on addpath

--- addpath.bat
+++ addpath.bat
@@ -2,4 +2,13 @@
 @set build_type=Release
 @if not "%1"=="" set build_type=%1
 @echo Configuring for build type %build_type%
-@set PATH=%cd%\..\..\boost_1_56_0\lib64-msvc-12.0;%cd%\..\..\FreeImage-vc12-x64-release-debug\x64\%build_type%\DLL;%cd%\deps\opende;%cd%\..\..\ogre_src_v1-8-1-vc12-x64-release-debug\build\install\%build_type%\bin\%build_type%;%cd%\..\..\sdformat\build\install\%build_type%\lib;%cd%\..\..\tbb43_20141023oss\bin\intel64\vc12;%cd%\deps\libccd;%cd%\deps\opende\OPCODE;%cd%\deps\opende\ou;%cd%\..\..\zlib-1.2.8-vc12-x64-release-debug\contrib\vstudio\vc11\x64\ZlibDll%build_type%;%PATH%
+
+@REM Need absolute paths in order to run anything inside build/
+@echo - script path is %~dp0
+@set root_path=%~dp0
+@pushd %~dp0\..
+@set deps_path=%CD%
+@popd
+@echo - dependencies path is %deps_path%
+
+@set PATH=%deps_path%\boost_1_56_0\lib64-msvc-12.0;%deps_path%\FreeImage-vc12-x64-release-debug\x64\%build_type%\DLL;%root_path%\deps\opende;%deps_path%\ogre_src_v1-8-1-vc12-x64-release-debug\build\install\%build_type%\bin\%build_type%;%deps_path%\sdformat\build\install\%build_type%\lib;%deps_path%\tbb43_20141023oss\bin\intel64\vc12;%root_path%\deps\libccd;%root_path%\deps\opende\OPCODE;%root_path%\deps\opende\ou;%deps_path%\zlib-1.2.8-vc12-x64-release-debug\contrib\vstudio\vc11\x64\ZlibDll%build_type%;%PATH%
