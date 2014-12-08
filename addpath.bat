d07c6ccde40ef21c2e845248cfa532cf01cbc137
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-12-08 18:24:39 +0100

    Added HOME automatically

--- addpath.bat
+++ addpath.bat
@@ -11,4 +11,6 @@
 @popd
 @echo - dependencies path is %deps_path%
 
+@set HOME=%HOMEPATH%
+
 @set PATH=%deps_path%\boost_1_56_0\lib64-msvc-12.0;%deps_path%\FreeImage-vc12-x64-release-debug\x64\%build_type%\DLL;%root_path%\deps\opende;%deps_path%\ogre_src_v1-8-1-vc12-x64-release-debug\build\install\%build_type%\bin\%build_type%;%deps_path%\sdformat\build\install\%build_type%\lib;%deps_path%\tbb43_20141023oss\bin\intel64\vc12;%root_path%\deps\libccd;%root_path%\deps\opende\OPCODE;%root_path%\deps\opende\ou;%deps_path%\zlib-1.2.8-vc12-x64-release-debug\contrib\vstudio\vc11\x64\ZlibDll%build_type%;%deps_path%\bzip2-1.0.6-vc12-x64-release-debug\%build_type%;C:\Qt\4.8.6\x64\msvc2013\bin;%PATH%
