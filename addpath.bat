8cb309d2413e0ede7fa3f94ba39f01e1e751e4f1
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-12-09 21:49:50 +0100

    Set all GAZEBO_ environment variables

--- addpath.bat
+++ addpath.bat
@@ -4,13 +4,19 @@
 @echo Configuring for build type %build_type%
 
 @REM Need absolute paths in order to run anything inside build/
-@echo - script path is %~dp0
-@set root_path=%~dp0\build
+@set gz_root_path=%~dp0
+@set gz_build_path=%gz_root_path%\build
 @pushd %~dp0\..
 @set deps_path=%CD%
 @popd
+@echo - script path is %gz_root_path%
 @echo - dependencies path is %deps_path%
 
 @set HOME=%HOMEPATH%
 
-@set PATH=%deps_path%\boost_1_56_0\lib64-msvc-12.0;%deps_path%\FreeImage-vc12-x64-release-debug\x64\%build_type%\DLL;%root_path%\deps\opende;%deps_path%\ogre_src_v1-8-1-vc12-x64-release-debug\build\install\%build_type%\bin\%build_type%;%deps_path%\sdformat\build\install\%build_type%\lib;%deps_path%\tbb43_20141023oss\bin\intel64\vc12;%root_path%\deps\libccd;%root_path%\deps\opende\OPCODE;%root_path%\deps\opende\ou;%deps_path%\zlib-1.2.8-vc12-x64-release-debug\contrib\vstudio\vc11\x64\ZlibDll%build_type%;%deps_path%\bzip2-1.0.6-vc12-x64-release-debug\%build_type%;C:\Qt\4.8.6\x64\msvc2013\bin;%PATH%
+@set GAZEBO_MODEL_PATH=%gz_root_path%\models
+@set GAZEBO_PLUGINS_PATH=%gz_build_path%\plugins
+@set GAZEBO_RESOURCE_PATH=%gz_root_path%
+@set OGRE_RESORCE_PATH=%deps_path%\ogre_src_v1-8-1-vc12-x64-release-debug\build\install\Debug\bin\Debug
+
+@set PATH=%deps_path%\boost_1_56_0\lib64-msvc-12.0;%deps_path%\FreeImage-vc12-x64-release-debug\x64\%build_type%\DLL;%gz_build_path%\deps\opende;%deps_path%\ogre_src_v1-8-1-vc12-x64-release-debug\build\install\%build_type%\bin\%build_type%;%deps_path%\sdformat\build\install\%build_type%\lib;%deps_path%\tbb43_20141023oss\bin\intel64\vc12;%gz_build_path%\deps\libccd;%gz_build_path%\deps\opende\OPCODE;%gz_build_path%\deps\opende\ou;%deps_path%\zlib-1.2.8-vc12-x64-release-debug\contrib\vstudio\vc11\x64\ZlibDll%build_type%;%deps_path%\bzip2-1.0.6-vc12-x64-release-debug\%build_type%;C:\Qt\4.8.6\x64\msvc2013\bin;%PATH%
