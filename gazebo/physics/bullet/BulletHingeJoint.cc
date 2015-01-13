c31b3db86552116ac76d84053d28cf01214b0d3c
Steve Peters <scpeters@osrfoundation.org>
2015-01-13 14:51:20 -0800

    Fix SetParam for several bullet joints (and thus fix the test)

--- gazebo/physics/bullet/BulletHingeJoint.cc
+++ gazebo/physics/bullet/BulletHingeJoint.cc
@@ -394,6 +394,10 @@ bool BulletHingeJoint::SetParam(const std::string &_key,
         return false;
       }
     }
+    else
+    {
+      return BulletJoint::SetParam(_key, _index, _value);
+    }
   }
   catch(const boost::bad_any_cast &e)
   {
@@ -402,7 +406,7 @@ bool BulletHingeJoint::SetParam(const std::string &_key,
           << std::endl;
     return false;
   }
-  return BulletJoint::SetParam(_key, _index, _value);
+  return true;
 }
 
 //////////////////////////////////////////////////
