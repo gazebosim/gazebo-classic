1a1beae58f33091c5e16bc85731341488db66543
John Hsu <hsu@osrfoundation.org>
2015-02-10 16:04:54 -0800

    fix segfault for simbody screw joint when setting limits due to uninitialized limitForce.

--- gazebo/physics/simbody/SimbodyScrewJoint.cc
+++ gazebo/physics/simbody/SimbodyScrewJoint.cc
@@ -330,6 +330,14 @@ bool SimbodyScrewJoint::SetHighStop(
   {
     if (this->physicsInitialized)
     {
+      // check if limitForce is initialized
+      if (this->limitForce[_index].isEmptyHandle())
+      {
+        gzerr << "child link is NULL, force element not initialized, "
+              << "SetHighStop failed. Please file a report on issue tracker.\n";
+        return false;
+      }
+
       if (_index == 0)
       {
         // angular limit is specified
@@ -399,6 +407,14 @@ bool SimbodyScrewJoint::SetLowStop(
   {
     if (this->physicsInitialized)
     {
+      // check if limitForce is initialized
+      if (this->limitForce[_index].isEmptyHandle())
+      {
+        gzerr << "child link is NULL, force element not initialized, "
+              << "SetHighStop failed. Please file a report on issue tracker.\n";
+        return false;
+      }
+
       if (_index == 0)
       {
         // angular limit is specified
