0d56a11d01c2439a3e412f2849b15d7a94140046
Steve Peters <scpeters@osrfoundation.org>
2015-03-03 16:11:48 -0800

    Update doxygen for AngularAccel and AngularMomentum

--- gazebo/physics/Link.hh
+++ gazebo/physics/Link.hh
@@ -266,16 +266,18 @@ namespace gazebo
       public: math::Vector3 GetRelativeAngularAccel() const;
 
       /// \brief Get the angular momentum of the body CoG in the world frame,
+      /// which is computed as (I * w), where
+      /// I: inertia matrix in world frame
+      /// w: angular velocity in world frame
+      /// \return Angular momentum of the body.
+      public: math::Vector3 GetWorldAngularMomentum() const;
+
+      /// \brief Get the angular acceleration of the body in the world frame,
       /// which is computed as (I^-1 * (T - w x L)), where
       /// I: inertia matrix in world frame
       /// T: sum of external torques in world frame
       /// L: angular momentum of CoG in world frame
       /// w: angular velocity in world frame
-      /// \return Angular momentum of the body.
-      public: math::Vector3 GetWorldAngularMomentum() const;
-
-      /// \brief Get the angular acceleration of the body in the world
-      /// frame.
       /// \return Angular acceleration of the body in the world frame.
       public: math::Vector3 GetWorldAngularAccel() const;
 
