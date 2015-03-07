0f95661a83e6e58b73585f7270a1576e8d417f5c
Steve Peters <scpeters@osrfoundation.org>
2015-03-06 16:14:17 -0800

    Fix whitespace

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -374,7 +374,6 @@ find_package(SDFormat 2.3.1)
 if (NOT SDFormat_FOUND)
   find_package(SDFormat 3)
 endif()
-
 if (NOT SDFormat_FOUND)
   message (STATUS "Looking for SDFormat - not found")
   BUILD_ERROR ("Missing: SDF version >=2.3.1. Required for reading and writing SDF files.")
