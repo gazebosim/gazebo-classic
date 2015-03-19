20d4d978a37fe48918d798a9625466fd1679a45c
Steve Peters <scpeters@osrfoundation.org>
2015-03-19 01:28:29 -0700

    Add tool for converting junit xml files to csv to simplify data analysis

---
+++ tools/junit_to_csv.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'rexml/document'
+
+xml = File.read(ARGV[0])
+
+doc, arrayOfHashes, hashOfArrays = REXML::Document.new(xml), [], {}
+doc.elements.each('testsuites/testsuite/testcase') do |t|
+  arrayOfHashes << t.attributes
+end
+sortedKeys = arrayOfHashes.first.keys.sort
+sortedKeys.delete("value_param")
+puts sortedKeys.join(',')
+
+arrayOfHashes.each do |h|
+  row = [];
+  sortedKeys.each do |k|
+    row << h[k]
+  end
+  puts row.join(',')
+end
+
