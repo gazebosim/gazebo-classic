6634cb8313a4ce987d945df1667fbe679c6cb977
Nate Koenig <natekoenig@gmail.com>
2015-03-20 13:08:15 -0700

    Update

--- cmake/GazeboUtils.cmake
+++ cmake/GazeboUtils.cmake
@@ -135,6 +135,26 @@ macro (gz_setup_windows)
     # Using static linking in Windows by default
     set(BUILD_SHARED_LIBS FALSE)
     add_definitions(-DBUILDING_STATIC_LIBS)
+    # Need for M_PI constant
+    add_definitions(-D_USE_MATH_DEFINES) 
+    # Don't pull in the Windows min/max macros
+    add_definitions(-DNOMINMAX) 
+    # Use dynamic linking for boost
+    add_definitions(-DBOOST_ALL_DYN_LINK)
+    # And force linking to MSVC dynamic runtime
+    if ("${CMAKE_BUILD_TYPE_UPPERCASE}" STREQUAL "DEBUG")
+      add_definitions("/MDd")
+    else()
+      add_definitions("/MD")
+    endif()
+    # And we want exceptions
+    add_definitions("/EHsc")
+    if (MSVC AND CMAKE_SIZEOF_VOID_P EQUAL 8)
+      # Not need if proper cmake gnerator (-G "...Win64") is passed to cmake
+      # Enable as a second measeure to workaround over bug
+      # http://www.cmake.org/Bug/print_bug_page.php?bug_id=11240 
+      set(CMAKE_SHARED_LINKER_FLAGS "/machine:x64")
+    endif()
 endmacro()
 
 #################################################
