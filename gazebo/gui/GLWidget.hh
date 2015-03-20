c3ef180aab2c6a4b5b805ce42c5cec3f7dfc0ff3
Jackie Kay jacquelinekay1 <Jackie Kay jacquelinekay1@gmail.com>
2015-03-20 10:11:39 -0700

    Doxygen

--- gazebo/gui/GLWidget.hh
+++ gazebo/gui/GLWidget.hh
@@ -66,6 +66,9 @@ namespace gazebo
       public: void Clear();
 
       signals: void clicked();
+
+      /// \brief QT signal to notify when we received a selection msg.
+      /// \param[in] _name Name of the selected entity.
       signals: void selectionMsgReceived(const QString &_name);
 
       protected: virtual void moveEvent(QMoveEvent *_e);
@@ -180,6 +183,7 @@ namespace gazebo
       private slots: void OnModelEditor(bool _checked);
 
       /// \brief Qt callback when a selection msg is received.
+      /// \param[in] The name of the selected entity.
       private slots: void OnSelectionMsgEvent(const QString &_name);
 
       private: int windowId;
