2b8ef6109b5c6a24ea1ca0b53755e8a214a92293
Nate Koenig <natekoenig@gmail.com>
2015-03-25 06:39:28 -0700

    Updates

--- gazebo/gazebo.cc
+++ gazebo/gazebo.cc
@@ -14,6 +14,13 @@
  * limitations under the License.
  *
  */
+
+#ifdef _WIN32
+  // Ensure that Winsock2.h is included before Windows.h, which can get
+  // pulled in by anybody (e.g., Boost).
+  #include <Winsock2.h>
+#endif
+
 #include <vector>
 #include <boost/thread/mutex.hpp>
 #include <sdf/sdf.hh>
