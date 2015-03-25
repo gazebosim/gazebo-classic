13afa98200eb330aaa1cb39ec07da4894b645eee
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-03-25 18:58:08 +0100

    Fix code style

--- examples/plugins/model_move/model_move.cc
+++ examples/plugins/model_move/model_move.cc
@@ -27,7 +27,9 @@
 
 namespace gazebo
 {
-typedef const boost::shared_ptr<const gazebo::msgs::PoseAnimation> PoseAnimationPtr;
+
+  typedef const boost::shared_ptr<const gazebo::msgs::PoseAnimation> PoseAnimationPtr;
+ 
   class ModelMove : public ModelPlugin
   {
     gazebo::common::PoseAnimationPtr anim;
@@ -37,70 +39,70 @@ typedef const boost::shared_ptr<const gazebo::msgs::PoseAnimation> PoseAnimation
     math::Vector3 start_point;
     math::Vector3 *path;
 
-  private: void move(math::Vector3 *start, math::Vector3 *end, math::Vector3 *translation) {
-
-    int duration = floor(start->Distance((*end).x, (*end).y, (*end).z));
-    math::Vector3 diff = *end - *start;
-    float x_step = diff.x / duration;
-    float y_step = diff.y / duration;
-    float z_step = diff.z / duration;
-    int curr_frame = anim->GetKeyFrameCount();
-    gazebo::common::PoseKeyFrame *key;
-    
-    for (int i=1; i <= duration; i++) {
-      key = anim->CreateKeyFrame(i+curr_frame);
-      key->SetTranslation(math::Vector3((*translation).x + x_step*i, 
-					(*translation).y + y_step*i, 
-					  (*translation).z + z_step*i)); 
-      key->SetRotation(math::Quaternion(0, 0, 0));
+    private: void move(math::Vector3 *start, math::Vector3 *end, math::Vector3 *translation)
+    {
+      int duration = floor(start->Distance((*end).x, (*end).y, (*end).z));
+      math::Vector3 diff = *end - *start;
+      float x_step = diff.x / duration;
+      float y_step = diff.y / duration;
+      float z_step = diff.z / duration;
+      int curr_frame = anim->GetKeyFrameCount();
+      gazebo::common::PoseKeyFrame *key;
+     
+      for (int i=1; i <= duration; i++) {
+        key = anim->CreateKeyFrame(i+curr_frame);
+        key->SetTranslation(math::Vector3((*translation).x + x_step*i,
+            (*translation).y + y_step*i,
+              (*translation).z + z_step*i));
+        key->SetRotation(math::Quaternion(0, 0, 0));
+      }
+     
+      translation->Set((*translation).x + x_step*duration,
+           (*translation).y + y_step*duration,
+           (*translation).z + z_step*duration);
     }
-    
-    translation->Set((*translation).x + x_step*duration, 
-		     (*translation).y + y_step*duration, 
-		     (*translation).z + z_step*duration);
-  }
 
-  public: void initiateMove() 
+    public: void initiateMove()
     {
       float path_length = start_point.Distance(path->x, path->y, path->z);
-      for (int i=0; i < num_points-1; i++) {
-	path_length += path[i].Distance(path[i+1].x, path[i+1].y, path[i+1].z);
-      }	
-      
+
+      for (int i=0; i < num_points-1; i++)
+        path_length += path[i].Distance(path[i+1].x, path[i+1].y, path[i+1].z);
+     
       // create the animation
       this->anim = gazebo::common::PoseAnimationPtr(new gazebo::common::PoseAnimation("test", path_length+1, false));
-      
+     
       gazebo::common::PoseKeyFrame *key;
-      
+     
       // set starting location of the box
       key = anim->CreateKeyFrame(0);
       key->SetTranslation(math::Vector3(0, 0, 0));
       key->SetRotation(math::Quaternion(0, 0, 0));
-      
+     
       math::Vector3 translation = math::Vector3(0, 0, 0);
-      
-      move(&start_point, path, &translation); 
-      for (int i=0; i < num_points-1; i++) {
-	move(path+i, path+i+1, &translation);
-      }	
-      
+     
+      move(&start_point, path, &translation);
+      for (int i=0; i < num_points-1; i++)
+        move(path+i, path+i+1, &translation);
+     
       // set the animation
       this->model->SetAnimation(anim);
     }
 
-  public: void getPathMsg(PoseAnimationPtr &msg)
+    public: void getPathMsg(PoseAnimationPtr &msg)
     {
       std::cout << "Received path message." << std::endl;
 
       this->num_points = msg->pose_size();
       this->path = new math::Vector3[num_points];
-      for (int i = 0; i < num_points; i++) {
-	path[i] = gazebo::msgs::Convert(msg->pose(i)).pos;
-      }
+
+      for (int i = 0; i < num_points; i++)
+        path[i] = gazebo::msgs::Convert(msg->pose(i)).pos;
+      
       initiateMove();
     }
-    
-  public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+   
+    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
     {
       std::cout << "In plugin code.\n";
       // Store the pointer to the model
@@ -108,35 +110,34 @@ typedef const boost::shared_ptr<const gazebo::msgs::PoseAnimation> PoseAnimation
 
       this->node = transport::NodePtr(new transport::Node());
       this->node->Init(this->model->GetWorld()->GetName());
-      
+     
       // Either get parameters from sdf
-      if (_sdf->HasElement("path") && _sdf->HasElement("n_points")) 
-	{
-	  sdf::Vector3 sdf_pose = _sdf->GetParent()->GetElement("pose")->Get<sdf::Pose>().pos;
-	  this->start_point = math::Vector3(sdf_pose.x, sdf_pose.y, sdf_pose.z);
-	  this->num_points = std::stoi(_sdf->GetElement("n_points")->Get<std::string>());
-	  this->path = new math::Vector3[num_points];
-	  std::stringstream stream(_sdf->GetElement("path")->Get<std::string>());
-	  for (int i=0; i<num_points; i++) {
-	    float f1, f2, f3;
-	    stream >> f1 >> f2 >> f3;
-	    path[i] = math::Vector3(f1, f2, f3);
-	  }
-	  initiateMove();
-	}
-      // Or get parameters from gazebo topics
-      else 
-	{
-	  std::cout << "Waiting\n";
-	  pathSubscriber = node->Subscribe("/gazebo/default/pose_animation", &ModelMove::getPathMsg, this);
-	}
+      if (_sdf->HasElement("path") && _sdf->HasElement("n_points"))
+      {
+        sdf::Vector3 sdf_pose = _sdf->GetParent()->GetElement("pose")->Get<sdf::Pose>().pos;
+        this->start_point = math::Vector3(sdf_pose.x, sdf_pose.y, sdf_pose.z);
+        this->num_points = std::stoi(_sdf->GetElement("n_points")->Get<std::string>());
+        this->path = new math::Vector3[num_points];
+        std::stringstream stream(_sdf->GetElement("path")->Get<std::string>());
+        for (int i=0; i<num_points; i++) {
+          float f1, f2, f3;
+          stream >> f1 >> f2 >> f3;
+          path[i] = math::Vector3(f1, f2, f3);
+        }
+        initiateMove();
+      }
+      else // Or get parameters from gazebo topics
+      {
+        std::cout << "Waiting\n";
+        pathSubscriber = node->Subscribe("/gazebo/default/pose_animation", &ModelMove::getPathMsg, this);
+      }
     }
-    
+   
     // Pointer to the model
-  private: physics::ModelPtr model;
+    private: physics::ModelPtr model;
 
     // Pointer to the update event connection
-  private: event::ConnectionPtr updateConnection;
+    private: event::ConnectionPtr updateConnection;
   };
 
   // Register this plugin with the simulator
