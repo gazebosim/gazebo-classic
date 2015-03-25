9085e6a9786c175f3ee4ed54a104e071fe0f55a0
Steve Peters <scpeters@osrfoundation.org>
2015-03-25 14:55:50 -0700

    Test PID constructor, PID::Init

---
+++ gazebo/common/PID_TEST.cc
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2015 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <gtest/gtest.h>
+
+#include "gazebo/common/PID.hh"
+#include "test/util.hh"
+
+using namespace gazebo;
+
+class PID : public gazebo::testing::AutoLogFixture { };
+
+TEST_F(PID, ConstructorInit)
+{
+  // default constructor
+  {
+    common::PID pid;
+    EXPECT_DOUBLE_EQ(0.0, pid.GetPGain());
+    EXPECT_DOUBLE_EQ(0.0, pid.GetIGain());
+    EXPECT_DOUBLE_EQ(0.0, pid.GetDGain());
+    EXPECT_DOUBLE_EQ(0.0, pid.GetIMax());
+    EXPECT_DOUBLE_EQ(0.0, pid.GetIMin());
+    EXPECT_DOUBLE_EQ(0.0, pid.GetCmdMax());
+    EXPECT_DOUBLE_EQ(0.0, pid.GetCmdMin());
+  }
+
+  const double p = 1.0;
+  const double i = 2.0;
+  const double d = 3.0;
+  const double iMax =  4.0;
+  const double iMin = -4.0;
+  const double cmdMax =  5.0;
+  const double cmdMin = -5.0;
+
+  // Constructor with some parameters
+  // Followed by Init with all parameters
+  {
+    common::PID pid(p, i, d);
+    EXPECT_DOUBLE_EQ(p, pid.GetPGain());
+    EXPECT_DOUBLE_EQ(i, pid.GetIGain());
+    EXPECT_DOUBLE_EQ(d, pid.GetDGain());
+    EXPECT_DOUBLE_EQ(0.0, pid.GetIMax());
+    EXPECT_DOUBLE_EQ(0.0, pid.GetIMin());
+    EXPECT_DOUBLE_EQ(0.0, pid.GetCmdMax());
+    EXPECT_DOUBLE_EQ(0.0, pid.GetCmdMin());
+
+    pid.Init(p, i, d, iMax, iMin, cmdMax, cmdMin);
+    EXPECT_DOUBLE_EQ(p, pid.GetPGain());
+    EXPECT_DOUBLE_EQ(i, pid.GetIGain());
+    EXPECT_DOUBLE_EQ(d, pid.GetDGain());
+    EXPECT_DOUBLE_EQ(iMax, pid.GetIMax());
+    EXPECT_DOUBLE_EQ(iMin, pid.GetIMin());
+    EXPECT_DOUBLE_EQ(cmdMax, pid.GetCmdMax());
+    EXPECT_DOUBLE_EQ(cmdMin, pid.GetCmdMin());
+  }
+}
+
+
+/////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
