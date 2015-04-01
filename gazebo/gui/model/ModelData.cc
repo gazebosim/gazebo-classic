88cb7b05e6d7871a0c6513150302da8fa91ae3ec
Nate Koenig <natekoenig@gmail.com>
2015-04-01 11:16:45 -0700

    Fix build

--- gazebo/gui/model/ModelData.cc
+++ gazebo/gui/model/ModelData.cc
@@ -696,7 +696,7 @@ void LinkData::OnAddVisual(const std::string &_name)
 
     visVisual.reset(new rendering::Visual(visualName.str(),
         this->linkVisual));
-    sdf::ElementPtr visualElem =  modelTemplateSDF->root
+    sdf::ElementPtr visualElem =  modelTemplateSDF->Root()
         ->GetElement("model")->GetElement("link")->GetElement("visual");
     visVisual->Load(visualElem);
   }
@@ -741,7 +741,7 @@ void LinkData::OnAddCollision(const std::string &_name)
 
     collisionVis.reset(new rendering::Visual(collisionName.str(),
         this->linkVisual));
-    sdf::ElementPtr collisionElem =  modelTemplateSDF->root
+    sdf::ElementPtr collisionElem =  modelTemplateSDF->Root()
         ->GetElement("model")->GetElement("link")->GetElement("visual");
     collisionVis->Load(collisionElem);
     collisionVis->SetMaterial("Gazebo/Orange");
