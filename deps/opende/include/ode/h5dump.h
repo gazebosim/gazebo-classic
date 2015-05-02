06b089c223380b9aad0fdc9ad2e836f3b26a5fab
rosebudflyaway <rosebudflyaway@gmail.com>
2015-05-02 13:43:11 -0700

    tentative to fix Jenkins build

--- deps/opende/include/ode/h5dump.h
+++ deps/opende/include/ode/h5dump.h
@@ -1,4 +1,4 @@
-/* 
+/*
 Copyright (c) 2013 Claude Lacoursiere
 
 This software is provided 'as-is', without any express or implied
@@ -20,9 +20,9 @@ freely, subject to the following restrictions:
    3. This notice may not be removed or altered from any source
    distribution.
 
-*/ 
+*/
 
-#ifdef MBSFCLIB_HDF
+#ifdef HDF5_INSTRUMENT
 
 
 #ifndef H5_IO_C
@@ -33,7 +33,7 @@ struct dxWorld;
 #ifdef __cplusplus
 extern "C" {
 #endif
-  void h5dump_world(const char * file, dxWorld * w, dReal stepsize); 
+  void h5dump_world(const char * file, dxWorld * w, dReal stepsize);
   void h5_write_errors(const char *, dReal * errors, unsigned int n);
 #ifdef __cplusplus
 }
@@ -41,4 +41,4 @@ extern "C" {
 
 #endif
 
-#endif // MBSFCLIB_HDF
+#endif // HDF5_INSTRUMENT
--- deps/opende/src/step.cpp
+++ deps/opende/src/step.cpp
@@ -26,13 +26,16 @@
 #include <ode/timer.h>
 #include <ode/error.h>
 #include <ode/matrix.h>
-#include <ode/h5dump.h>
 #include "config.h"
 #include "objects.h"
 #include "joints/joint.h"
 #include "lcp.h"
 #include "util.h"
 #include "joints/hinge.h"
+
+#ifdef HDF5_INSTRUMENT
+#include <ode/h5dump.h>
+#endif
 
 //****************************************************************************
 // misc defines
