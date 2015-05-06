c474810e72944187536614e670efe9f4afa92dd9
Steve Peters <scpeters@osrfoundation.org>
2015-05-06 10:18:47 -0700

    Use path to world file in run_diagnostic.bash

--- tools/run_diagnostic.bash
+++ tools/run_diagnostic.bash
@@ -1,10 +1,11 @@
 WORLD=$1
 PRESET=$2
-PREFIX=${WORLD}_${PRESET}
+WORLD_NAME=`basename $1 .world`
+PREFIX=${WORLD_NAME}_${PRESET}
 DIAG=$HOME/.gazebo/diagnostics
 STDOUT=${DIAG}/stdout.txt
 /usr/bin/time -a -p -o ${DIAG}/stdout.txt \
-  gzserver --verbose $HOME/ws/gazebo_sdformat/src/gazebo/test/worlds/${WORLD}.world -o ${PRESET} \
+  gzserver --verbose ${WORLD} -o ${PRESET} \
   --iters 100000 > ${STDOUT} 2>&1
 cat ${STDOUT}
 SUFFIX=`ls -t ${DIAG} | grep ^201 | head -1`
