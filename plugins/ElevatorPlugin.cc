77161f470d1f9ab5fceaaba06bc9f4e87618f2ad
Nate Koenig <natekoenig@gmail.com>
2015-05-15 15:46:46 -0700

    delete

--- plugins/ElevatorPlugin.cc
+++ plugins/ElevatorPlugin.cc
@@ -184,6 +184,8 @@ void ElevatorPlugin::Update(const common::UpdateInfo &_info)
 void ElevatorPlugin::Reset()
 {
   std::lock_guard<std::mutex> lock(this->dataPtr->stateMutex);
+  for (auto s: this->states)
+    delete s;
   this->dataPtr->states.clear();
   this->dataPtr->doorController->Reset();
   this->dataPtr->liftController->Reset();
