50eb570029b3d5d1f33b28dcddf553d0ce7090f2
Steve Peters <scpeters@osrfoundation.org>
2015-05-22 15:47:13 -0700

    whitespace, line length

--- plugins/TimerGUIPlugin.cc
+++ plugins/TimerGUIPlugin.cc
@@ -258,7 +258,7 @@ void TimerGUIPlugin::Load(sdf::ElementPtr _elem)
   {
     int xPos, yPos;
     if (this->parent())
-       xPos = static_cast<QWidget*>(this->parent())->width() - this->width() - 10;
+      xPos = static_cast<QWidget*>(this->parent())->width() - this->width() -10;
     else
       xPos = 600;
 
