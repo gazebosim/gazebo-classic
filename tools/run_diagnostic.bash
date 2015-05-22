9a8c2a69d160ba69d3d711fcb208988195a6e906
Steve Peters <scpeters@osrfoundation.org>
2015-05-22 12:12:36 -0700

    Simulate 10 s instead of 100 in run_diagnostic

--- tools/run_diagnostic.bash
+++ tools/run_diagnostic.bash
@@ -6,7 +6,7 @@ DIAG=$HOME/.gazebo/diagnostics
 STDOUT=${DIAG}/stdout.txt
 /usr/bin/time -a -p -o ${DIAG}/stdout.txt \
   gzserver --verbose ${WORLD} -o ${PRESET} \
-  --iters 100000 > ${STDOUT} 2>&1
+  --iters 10000 > ${STDOUT} 2>&1
 cat ${STDOUT}
 SUFFIX=`ls -t ${DIAG} | grep ^201 | head -1`
 mv ${STDOUT} ${DIAG}/${SUFFIX}/
