326c9501fcdf16f8250b2f8e4144c5e6b93211d1
asymingt <asymingt@ideapad-yoga13>
2015-06-13 16:44:17 -0700

    Updated physics message and added fluid pressure sensor template code

---
+++ gazebo/sensors/FluidPressureSensor.cc
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2012-2015 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifdef _WIN32
+  // Ensure that Winsock2.h is included before Windows.h, which can get
+  // pulled in by anybody (e.g., Boost).
+  #include <Winsock2.h>
+#endif
+
+#include "gazebo/sensors/SensorFactory.hh"
+
+#include "gazebo/common/common.hh"
+#include "gazebo/math/gzmath.hh"
+#include "gazebo/physics/physics.hh"
+#include "gazebo/sensors/Noise.hh"
+#include "gazebo/transport/transport.hh"
+
+#include "gazebo/sensors/FluidPressureSensor.hh"
+
+using namespace gazebo;
+using namespace sensors;
+
+GZ_REGISTER_STATIC_SENSOR("fluid_pressure", FluidPressureSensor)
+
+/////////////////////////////////////////////////
+FluidPressureSensor::FluidPressureSensor()
+: Sensor(sensors::OTHER)
+{
+}
+
+/////////////////////////////////////////////////
+FluidPressureSensor::~FluidPressureSensor()
+{
+}
+
+/////////////////////////////////////////////////
+void FluidPressureSensor::Load(const std::string &_worldName, sdf::ElementPtr _sdf)
+{
+  Sensor::Load(_worldName, _sdf);
+}
+
+/////////////////////////////////////////////////
+void FluidPressureSensor::Load(const std::string &_worldName)
+{
+  Sensor::Load(_worldName);
+
+  // Save the link to which this sensor is attached
+  physics::EntityPtr parentEntity = this->world->GetEntity(this->parentName);
+  this->parentLink = boost::dynamic_pointer_cast<physics::Link>(parentEntity);
+  this->lastFluidPressureMsg.set_link_name(this->parentName);
+
+  // Create a message publisher to send out measurements
+  this->topicName = "~/" + this->parentName + '/' + this->GetName();
+  if (this->sdf->HasElement("topic"))
+    this->topicName += '/' + this->sdf->Get<std::string>("topic");
+  boost::replace_all(this->topicName, "::", "/");
+  this->fluidPressurePub = this->node->Advertise<msgs::FluidPressure>(this->topicName, 50);
+
+  // Parse sdf noise parameters
+  this->noises[FluidPressureNoisePascals] = NoiseFactory::NewNoiseModel(
+    this->sdf->GetElement("horizontal")->GetElement("noise"));
+}
+
+/////////////////////////////////////////////////
+void FluidPressureSensor::Fini()
+{
+  Sensor::Fini();
+  this->parentLink.reset();
+}
+
+//////////////////////////////////////////////////
+void FluidPressureSensor::Init()
+{
+  Sensor::Init();
+}
+
+//////////////////////////////////////////////////
+bool FluidPressureSensor::UpdateImpl(bool /*_force*/)
+{
+  // Get latest pose information
+  if (this->parentLink)
+  {
+    // Measure position and apply noise
+    math::Pose fluidPressurePose = this->pose + this->parentLink->GetWorldPose();
+
+    // Reference: https://en.wikipedia.org/wiki/Atmospheric_pressure
+    double p0 = this->world->GetPhysicsEngine()->GetPressure();              // Reference pressure
+    double t0 = this->world->GetPhysicsEngine()->GetTemperature();           // Reference temperature
+    double g  = this->world->GetPhysicsEngine()->GetGravity().GetLength();   // Gravity
+    double L  = 0.0065;                                                      // Temperature lapse rate
+    double R  = 8.31447;                                                     // Universal gas constant
+    double M  = 0.0289644;                                                   // Molar mass of dry air
+    double h  = fluidPressurePose.pos.z;                                     // Altitude          
+
+    // Calculate the pressure based on the altitude
+    double p = p0 * exp(1.0 - L * h / t0, g * M / R / L);
+
+    // Apply noise to the pressure
+    p = this->noises[FluidPressureNoisePascals].Apply(p);
+
+    // Set the sensor value
+    this->lastFluidPressureMsg.set_fluid_pressure(p);
+  }
+
+  // Set the measurement time
+  this->lastMeasurementTime = this->world->GetSimTime();
+  msgs::Set(this->lastFluidPressureMsg.mutable_time(), this->lastMeasurementTime);
+
+  // If we are publishing the sensor measurements, do so now
+  if (this->fluidPressurePub)
+    this->fluidPressurePub->Publish(this->lastFluidPressureMsg);
+
+  return true;
+}
+
+//////////////////////////////////////////////////
+double FluidPressureSensor::GetFluidPressure() const
+{
+  return this->lastFluidPressureMsg.fluid_pressure();
+}
