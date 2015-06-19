0890c7251156018ee7f2a3b19a38de5a669053e1
chapulina <burajiru.no.chapulina@gmail.com>
2015-06-18 17:19:08 -0700

    attempt to highligh selected link origin

--- gazebo/rendering/Visual.cc
+++ gazebo/rendering/Visual.cc
@@ -1495,6 +1495,21 @@ void Visual::SetHighlighted(bool _highlighted)
       this->dataPtr->boundingBox->Init(bbox);
     }
     this->dataPtr->boundingBox->SetVisible(true);
+
+    if (this->GetType() == VT_LINK)
+    {
+      VisualPtr linkOriginVis;
+      for (auto child : this->dataPtr->children)
+      {
+        if (child->GetName().find("LINK_ORIGIN_VISUAL__") != std::string::npos)
+          linkOriginVis = child;
+      }
+      if (linkOriginVis)
+      {
+        std::cout << linkOriginVis->GetName() << std::endl;
+        linkOriginVis->SetEmissive(common::Color::Red);
+      }
+    }
   }
   else if (this->dataPtr->boundingBox)
   {
