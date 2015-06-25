9612a84f2eafe858d0a3cb53d890441be8b21573
Silvio Traversaro <silvio.traversaro@iit.it>
2015-06-25 17:55:19 +0200

    drop support for sdformat 3.0.5

--- test/integration/joint_get_force_torque.cc
+++ test/integration/joint_get_force_torque.cc
@@ -126,12 +126,6 @@ class JointGetForceTorqueTest : public ServerFixture,
               joint->set_parent("world");
               joint->set_child("dummy_link");
               joint->set_type(msgs::ConvertJointType("fixed"));
-              // remove the following four lines when we drop
-              // support for sdformat < 3.0.6
-              auto axis = joint->mutable_axis1();
-              msgs::Set(axis->mutable_xyz(), _opt.jointAxis);
-              axis->set_limit_lower(0.0);
-              axis->set_limit_upper(0.0);
             }
 
             msg.add_joint();
@@ -149,9 +143,7 @@ class JointGetForceTorqueTest : public ServerFixture,
             }
             joint->set_child("link");
 
-            // remove this comment when we can drop support
-            // for old sdformat (< 3.0.6)
-            // if( _opt.jointType != "fixed" )
+            if( _opt.jointType != "fixed" )
             {
               auto axis = joint->mutable_axis1();
               msgs::Set(axis->mutable_xyz(), _opt.jointAxis);
