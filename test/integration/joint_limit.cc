5cada0401b4f308dd182766b237764a6c31483c2
John Hsu <hsu@osrfoundation.org>
2015-07-09 17:23:03 -0700

    tweaking tests for joint limits

--- test/integration/joint_limit.cc
+++ test/integration/joint_limit.cc
@@ -50,32 +50,62 @@ void JointLimitTest::HingeJointLimit(const std::string &_physicsEngine)
   ASSERT_TRUE(physics != NULL);
   EXPECT_EQ(physics->GetType(), _physicsEngine);
 
-  physics->SetGravity(math::Vector3(0, 0, 0));
-
-  // simulate 1 step
-  world->Step(1);
-  double t = world->GetSimTime().Double();
-
-  // get time step size
+  // quick basic sim check
+  // sanity checks: get step size:
   double dt = world->GetPhysicsEngine()->GetMaxStepSize();
   EXPECT_GT(dt, 0);
   gzlog << "dt : " << dt << "\n";
-
-  // verify that time moves forward
+  // sanity checks: simulate 1 step and see that sim time moved forward
+  world->Step(1);
+  double t = world->GetSimTime().Double();
   EXPECT_DOUBLE_EQ(t, dt);
   gzlog << "t after one step : " << t << "\n";
 
   // get joint and get force torque
   physics::ModelPtr model_1 = world->GetModel("model_1");
+  ASSERT_TRUE(model_1 != NULL);
   physics::LinkPtr link_1 = model_1->GetLink("link_1");
   physics::LinkPtr link_2 = model_1->GetLink("link_2");
+  physics::LinkPtr link_3 = model_1->GetLink("link_3");
+  physics::LinkPtr link_4 = model_1->GetLink("link_4");
   physics::JointPtr joint_01 = model_1->GetJoint("joint_01");
   physics::JointPtr joint_12 = model_1->GetJoint("joint_12");
+  physics::JointPtr joint_23 = model_1->GetJoint("joint_23");
+  physics::JointPtr joint_34 = model_1->GetJoint("joint_34");
+  ASSERT_TRUE(link_1 != NULL);
+  ASSERT_TRUE(link_2 != NULL);
+  ASSERT_TRUE(link_3 != NULL);
+  ASSERT_TRUE(link_4 != NULL);
+  ASSERT_TRUE(joint_01 != NULL);
+  ASSERT_TRUE(joint_12 != NULL);
+  ASSERT_TRUE(joint_23 != NULL);
+  ASSERT_TRUE(joint_34 != NULL);
 
-  gzlog << "Test: drive joint to limit with constant force.\n";
+  gzdbg << "Test: drive joint_01 to positive limit with constant force.\n";
+  gzdbg << "-----------------------------------------------------------\n";
+  getchar();
+  joint_01->SetParam("stop_erp", 0, 0);
+  joint_01->SetParam("stop_cfm", 0, 10);
+  for (unsigned int i = 0; i < 50000; ++i)
+  {
+    joint_01->SetForce(0, 100);
+    world->Step(1);
+    if (joint_01->GetAngle(0) >= joint_01->GetUpperLimit(0))
+    {
+      gzdbg << "t: [" << world->GetSimTime().Double()
+            << "] pos: [" << joint_01->GetAngle(0)
+            << "] >= lim: [" << joint_01->GetUpperLimit(0)
+            << "] err: [" << joint_01->GetAngle(0)-joint_01->GetUpperLimit(0)
+            << "] >=?: [" << (joint_01->GetAngle(0) >= joint_01->GetUpperLimit(0))
+            << "]\n";
+      getchar();
+    }
+  }
+  getchar();
+  gzdbg << "\n";
+
   for (unsigned int i = 0; i < 10; ++i)
   {
-    world->Step(1);
     // test joint_01 wrench
     physics::JointWrench wrench_01 = joint_01->GetForceTorque(0u);
     EXPECT_DOUBLE_EQ(wrench_01.body1Force.x,    0.0);
