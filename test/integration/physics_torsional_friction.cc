a72023575f2d661a7fc0bb0294c0044cb42dfa9c
chapulina <burajiru.no.chapulina@gmail.com>
2015-07-10 17:48:35 -0700

    cleaned up a bit and added beginning of coefficient test

--- test/integration/physics_torsional_friction.cc
+++ test/integration/physics_torsional_friction.cc
@@ -29,23 +29,23 @@ const double g_friction_tolerance = 1e-3;
 class PhysicsTorsionalFrictionTest : public ServerFixture,
     public testing::WithParamInterface<const char*>
 {
-  protected: PhysicsTorsionalFrictionTest() : ServerFixture(), spawnCount(0)
+  protected: PhysicsTorsionalFrictionTest() : ServerFixture()
   {
   }
 
   /// \brief Data structure to hold model pointer and friction parameter
-  ///        for each test model in friction demo world.
-  class TorsionalFrictionDemoSphere
+  /// for each test model in friction demo world.
+  class SphereData
   {
     /// \brief Constructor.
     /// \param[in] _world World pointer.
     /// \param[in] _name Model name.
-    public: TorsionalFrictionDemoSphere(physics::WorldPtr _world,
+    public: SphereData(physics::WorldPtr _world,
         const std::string &_name)
-        : modelName(_name), world(_world), torsionalFriction(0.0)
+        : torsionalFriction(0.0)
     {
       // Get the model pointer
-      this->model = this->world->GetModel(this->modelName);
+      this->model = _world->GetModel(_name);
 
       // Get the friction coefficient
       physics::LinkPtr link = this->model->GetLink();
@@ -59,13 +59,7 @@ class PhysicsTorsionalFrictionTest : public ServerFixture,
     }
 
     /// \brief Destructor
-    public: ~TorsionalFrictionDemoSphere() {}
-
-    /// \brief Model name
-    public: std::string modelName;
-
-    /// \brief Pointer to the world
-    public: physics::WorldPtr world;
+    public: ~SphereData() = default;
 
     /// \brief Pointer to the model
     public: physics::ModelPtr model;
@@ -74,141 +68,23 @@ class PhysicsTorsionalFrictionTest : public ServerFixture,
     public: double torsionalFriction;
   };
 
-  /// \brief Class to hold parameters for spawning spheres.
-  public: class SpawnTorsionalFrictionSphereOptions
-  {
-    /// \brief Constructor.
-    public: SpawnTorsionalFrictionSphereOptions() : mass(1.0),
-        torsionalFriction(1.0)
-    {
-    }
-
-    /// \brief Destructor.
-    public: ~SpawnTorsionalFrictionSphereOptions()
-    {
-    }
-
-    /// \brief Radius of sphere to spawn.
-    public: double radius;
-
-    /// \brief Mass of sphere to spawn (inertia computed automatically).
-    public: double mass;
-
-    /// \brief Model pose.
-    public: math::Pose modelPose;
-
-    /// \brief Link pose.
-    public: math::Pose linkPose;
-
-    /// \brief Inertial pose.
-    public: math::Pose inertialPose;
-
-    /// \brief Collision pose.
-    public: math::Pose collisionPose;
-
-    /// \brief Torsional friction.
-    public: double torsionalFriction;
-  };
-
-  /// \brief Spawn a sphere with friction coefficients
-  /// \param[in] _opt Options for friction sphere.
-  public: physics::ModelPtr SpawnSphere(
-      const SpawnTorsionalFrictionSphereOptions &_opt)
-  {
-    msgs::Factory msg;
-    std::ostringstream modelStr;
-    std::ostringstream modelName;
-    modelName << "sphere_model" << this->spawnCount++;
-
-    double r = _opt.radius;
-    double m = _opt.mass;
-    double ixx = (2 * m * r * r) / 5;
-    double iyy = (2 * m * r * r) / 5;
-    double izz = (2 * m * r * r) / 5;
-
-    modelStr
-      << "<sdf version='" << SDF_VERSION << "'>"
-      << "<model name ='" << modelName.str() << "'>"
-      << "  <pose>" << _opt.modelPose << "</pose>"
-      << "  <link name='link'>"
-      << "    <pose>" << _opt.linkPose << "</pose>"
-      << "    <inertial>"
-      << "      <pose>" << _opt.inertialPose << "</pose>"
-      << "      <mass>" << _opt.mass << "</mass>"
-      << "      <inertia>"
-      << "        <ixx>" << ixx << "</ixx>"
-      << "        <iyy>" << iyy << "</iyy>"
-      << "        <izz>" << izz << "</izz>"
-      << "        <ixy>" << 0.0 << "</ixy>"
-      << "        <ixz>" << 0.0 << "</ixz>"
-      << "        <iyz>" << 0.0 << "</iyz>"
-      << "      </inertia>"
-      << "    </inertial>"
-      << "    <collision name='collision'>"
-      << "      <pose>" << _opt.collisionPose << "</pose>"
-      << "      <geometry>"
-      << "        <sphere><radius>" << _opt.radius << "</radius></sphere>"
-      << "      </geometry>"
-      << "      <surface>"
-      << "        <friction>"
-      << "          <ode>"
-      << "            <mu3>" << _opt.torsionalFriction << "</mu3>"
-      << "          </ode>"
-      << "        </friction>"
-      << "      </surface>"
-      << "    </collision>"
-      << "    <visual name='visual'>"
-      << "      <pose>" << _opt.collisionPose << "</pose>"
-      << "      <geometry>"
-      << "        <sphere><radius>" << _opt.radius << "</radius></sphere>"
-      << "      </geometry>"
-      << "    </visual>"
-      << "  </link>"
-      << "</model>";
-
-    physics::WorldPtr world = physics::get_world("default");
-    world->InsertModelString(modelStr.str());
-
-    physics::ModelPtr model;
-    common::Time wait(100, 0);
-
-    common::Time wallStart = common::Time::GetWallTime();
-    unsigned int waitCount = 0;
-    while (wait > (common::Time::GetWallTime() - wallStart) &&
-        !this->HasEntity(modelName.str()))
-    {
-      common::Time::MSleep(10);
-      if (++waitCount % 100 == 0)
-      {
-        gzwarn << "Waiting " << waitCount / 100 << " seconds for "
-               << "sphere to spawn." << std::endl;
-      }
-    }
-    if (this->HasEntity(modelName.str()) && waitCount >= 100)
-      gzwarn << "sphere has spawned." << std::endl;
-
-    if (world != NULL)
-      model = world->GetModel(modelName.str());
-
-    return model;
-  }
-
-  /// \brief Use the torsional_friction world.
+  /// \brief Use the torsional_friction_demo world to test spheres with
+  /// different coefficients of friction.
   /// \param[in] _physicsEngine Physics engine to use.
-  public: void VelocityTest(const std::string &_physicsEngine);
+  public: void CoefficientTest(const std::string &_physicsEngine);
 
-  /// \brief Count of spawned models, used to ensure unique model names.
-  private: unsigned int spawnCount;
+  /// \brief Use the torsional_friction_demo world to test spheres with
+  /// different contact depths.
+  /// \param[in] _physicsEngine Physics engine to use.
+  public: void DepthTest(const std::string &_physicsEngine);
 };
 
 /////////////////////////////////////////////////
-// TorsionalFrictionDemo test:
-// Uses the torsional_friction world, which has a bunch of spheres on the ground
-// with a gravity vector to simulate a 45-degree inclined plane. Each
-// sphere has a different coefficient of friction. These friction coefficients
-// are chosen to be close to the value that would prevent sliding according
-// to the Coulomb model.
-void PhysicsTorsionalFrictionTest::VelocityTest(
+// Coefficient test:
+// Uses the torsional_friction_demo world, which has several models rotating.
+// Some of these models are spheres rotating about the Z axis, each one with a
+// different coefficient of torsional friction.
+void PhysicsTorsionalFrictionTest::CoefficientTest(
     const std::string &_physicsEngine)
 {
   if (_physicsEngine != "ode")
@@ -232,23 +108,18 @@ void PhysicsTorsionalFrictionTest::VelocityTest(
   EXPECT_DOUBLE_EQ(g.z, -9.8);
 
   // Load the spheres
-  std::vector<PhysicsTorsionalFrictionTest::TorsionalFrictionDemoSphere>
+  std::vector<PhysicsTorsionalFrictionTest::SphereData>
       spheres;
   spheres.push_back(
-      PhysicsTorsionalFrictionTest::TorsionalFrictionDemoSphere(world,
-      "sphere_1"));
+      PhysicsTorsionalFrictionTest::SphereData(world, "sphere_1"));
   spheres.push_back(
-      PhysicsTorsionalFrictionTest::TorsionalFrictionDemoSphere(world,
-      "sphere_2"));
+      PhysicsTorsionalFrictionTest::SphereData(world, "sphere_2"));
   spheres.push_back(
-      PhysicsTorsionalFrictionTest::TorsionalFrictionDemoSphere(world,
-      "sphere_3"));
+      PhysicsTorsionalFrictionTest::SphereData(world, "sphere_3"));
   spheres.push_back(
-      PhysicsTorsionalFrictionTest::TorsionalFrictionDemoSphere(world,
-      "sphere_4"));
+      PhysicsTorsionalFrictionTest::SphereData(world, "sphere_4"));
   spheres.push_back(
-      PhysicsTorsionalFrictionTest::TorsionalFrictionDemoSphere(world,
-      "sphere_5"));
+      PhysicsTorsionalFrictionTest::SphereData(world, "sphere_5"));
 
   // Verify sphere data structure
   for (auto sphere : spheres)
@@ -257,7 +128,7 @@ void PhysicsTorsionalFrictionTest::VelocityTest(
   }
 
   common::Time t = world->GetSimTime();
-  while (t.sec < 10)
+  while (t.sec < 1)
   {
     world->Step(500);
     t = world->GetSimTime();
@@ -286,9 +157,94 @@ void PhysicsTorsionalFrictionTest::VelocityTest(
 }
 
 /////////////////////////////////////////////////
-TEST_P(PhysicsTorsionalFrictionTest, TorsionalFrictionDemo)
+// Depth test:
+// Uses the torsional_friction_demo world, which has several spheres. It checks
+// if the contact depth is correct for those spheres.
+void PhysicsTorsionalFrictionTest::DepthTest(
+    const std::string &_physicsEngine)
 {
-  VelocityTest(GetParam());
+  if (_physicsEngine != "ode")
+  {
+    gzerr << "Torsional friction only works with ODE (#ISSUE)"
+          << std::endl;
+    return;
+  }
+
+  Load("worlds/torsional_friction_demo.world", true, _physicsEngine);
+  physics::WorldPtr world = physics::get_world("default");
+  ASSERT_TRUE(world != NULL);
+
+  // check the gravity vector
+  physics::PhysicsEnginePtr physics = world->GetPhysicsEngine();
+  ASSERT_TRUE(physics != NULL);
+  EXPECT_EQ(physics->GetType(), _physicsEngine);
+  math::Vector3 g = physics->GetGravity();
+  EXPECT_DOUBLE_EQ(g.x, 0);
+  EXPECT_DOUBLE_EQ(g.y, 0);
+  EXPECT_DOUBLE_EQ(g.z, -9.8);
+
+  // Get contact manager
+  physics::ContactManager *mgr = physics->GetContactManager();
+  ASSERT_TRUE(mgr != NULL);
+
+  // Load the spheres
+  std::vector<PhysicsTorsionalFrictionTest::SphereData>
+      spheres;
+  spheres.push_back(
+      PhysicsTorsionalFrictionTest::SphereData(world, "sphere_1"));
+  spheres.push_back(
+      PhysicsTorsionalFrictionTest::SphereData(world, "sphere_2"));
+  spheres.push_back(
+      PhysicsTorsionalFrictionTest::SphereData(world, "sphere_3"));
+  spheres.push_back(
+      PhysicsTorsionalFrictionTest::SphereData(world, "sphere_4"));
+  spheres.push_back(
+      PhysicsTorsionalFrictionTest::SphereData(world, "sphere_5"));
+
+  // Verify sphere data structure
+  for (auto sphere : spheres)
+  {
+    ASSERT_TRUE(sphere.model != NULL);
+  }
+
+  // Get contacts
+  std::vector<physics::Contact *> contacts;
+
+  int maxTries = 100;
+  while (contacts.size() == 0 && maxTries > 0)
+  {
+    world->Step(1);
+    contacts = mgr->GetContacts();
+
+    std::cout << "Contacts: " << contacts.size()  << std::endl;
+    maxTries--;
+  }
+
+  // Check depth
+  for (auto sphere : spheres)
+  {
+    bool contactFound = false;
+    for (auto contact : contacts)
+    {
+      if (contact->collision1->GetLink()->GetModel() != sphere.model)
+        continue;
+      contactFound = true;
+
+      std::cout << "Depth: " << contact->depths[0] << std::endl;
+    }
+  }
+}
+
+/////////////////////////////////////////////////
+TEST_P(PhysicsTorsionalFrictionTest, CoefficientTest)
+{
+  CoefficientTest(GetParam());
+}
+
+/////////////////////////////////////////////////
+TEST_P(PhysicsTorsionalFrictionTest, DepthTest)
+{
+  DepthTest(GetParam());
 }
 
 INSTANTIATE_TEST_CASE_P(PhysicsEngines, PhysicsTorsionalFrictionTest,
