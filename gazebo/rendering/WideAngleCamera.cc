a173752815d457349d440852702eb7f74563d9af
Mykola Dolhyi <0xb000@gmail.com>
2015-07-13 12:34:07 +0300

    Add environment rendering to cubemap texture

--- gazebo/rendering/WideAngleCamera.cc
+++ gazebo/rendering/WideAngleCamera.cc
@@ -4,19 +4,39 @@
 
 #include "WideAngleCamera.hh"
 
+// #include "gazebo/rendering/skyx/include/SkyX.h"
 #include "gazebo/rendering/ogre_gazebo.h"
+
+#include "gazebo/rendering/RTShaderSystem.hh"
+#include "gazebo/rendering/Conversions.hh"
+#include "gazebo/rendering/Scene.hh"
 
 
 using namespace gazebo;
 using namespace rendering;
 
-WideAngleCamera::WideAngleCamera(const std::string &_namePrefix, ScenePtr _scene, bool _autoRender):
-  Camera(_namePrefix,_scene,_autoRender)
+WideAngleCamera::WideAngleCamera(const std::string &_namePrefix, ScenePtr _scene, bool _autoRender, int textureSize):
+  Camera(_namePrefix,_scene,_autoRender),
+  envTextureSize(textureSize)
 {
+  envCubeMapTexture = NULL;
+  for(int i=0;i<6;i++)
+  {
+    envCameras[i] = NULL;
+    envRenderTargets[i] = NULL;
+  }
 }
 
 WideAngleCamera::~WideAngleCamera()
 {
+  //TODO
+}
+
+void WideAngleCamera::Init()
+{
+  Camera::Init();
+
+  this->CreateEnvCameras();
 }
 
 void WideAngleCamera::SetRenderTarget(Ogre::RenderTarget *_target)
@@ -31,6 +51,111 @@ void WideAngleCamera::SetRenderTarget(Ogre::RenderTarget *_target)
       Ogre::CompositorManager::getSingleton().addCompositor(this->viewport,
           "WideCameraLensMap/PathThrough");
 
+    if(this->envCubeMapTexture)
+    {
+      Ogre::MaterialPtr compMat = 
+          Ogre::MaterialManager::getSingleton().getByName("Gazebo/WideLensMap");
+
+      compMat->getTechnique(0)->getPass(0)->createTextureUnitState(
+          envCubeMapTexture->getName(),0);
+
+      gzdbg << "Compositor cubemap texture bound OK " << envCubeMapTexture->getName() << "\n";
+    }
+    else
+      gzerr << "Compositor texture MISSING";
+
+
     this->wamapInstance->setEnabled(true);
   }
+}
+
+void WideAngleCamera::CreateEnvRenderTexture(const std::string &_textureName)
+{
+  int fsaa = 4;
+
+  // if(this->envCubeMapTexture)
+  // {
+  //   delete this->envCubeMapTexture;   // XXX
+  //   this->envCubeMapTexture = NULL;
+  // }
+
+  this->envCubeMapTexture = Ogre::TextureManager::getSingleton().createManual(
+      _textureName,
+      "General",
+      Ogre::TEX_TYPE_CUBE_MAP,
+      this->envTextureSize,
+      this->envTextureSize,
+      0,
+      Ogre::PF_A8R8G8B8,
+      Ogre::TU_RENDERTARGET,
+      0,
+      false,
+      fsaa).getPointer();
+
+  for(int i=0;i<6;i++)
+  {
+    Ogre::RenderTarget *rtt;
+    rtt = this->envCubeMapTexture->getBuffer(i)->getRenderTarget();
+
+    Ogre::Viewport *vp = rtt->addViewport(this->envCameras[i]);
+    vp->setClearEveryFrame(true);
+    vp->setShadowsEnabled(true);
+    vp->setOverlaysEnabled(false);
+
+    RTShaderSystem::AttachViewport(vp, this->GetScene());
+
+    vp->setBackgroundColour(
+      Conversions::Convert(this->scene->GetBackgroundColor()));
+    vp->setVisibilityMask(GZ_VISIBILITY_ALL & ~(GZ_VISIBILITY_GUI | GZ_VISIBILITY_SELECTABLE));
+
+    //FIXME: problem with Skyx include
+    // if (this->GetScene()->GetSkyX())
+    //   rtt->addListener(this->GetScene()->GetSkyX());
+
+    this->envRenderTargets[i] = rtt;
+  }
+}
+
+void WideAngleCamera::CreateEnvCameras()
+{
+  for(int i=0;i<6;i++)
+  {
+    std::stringstream name_str;
+
+    name_str << this->scopedUniqueName << "_env_" << i;
+
+    envCameras[i] = this->GetScene()->GetManager()->createCamera(name_str.str());
+
+    gzdbg << "creating camera "<< name_str.str() << "\n";
+
+    envCameras[i]->setFixedYawAxis(false);
+    envCameras[i]->setFOVy(Ogre::Degree(90));
+    envCameras[i]->setAspectRatio(1);
+
+    envCameras[i]->setNearClipDistance(0.01);
+    envCameras[i]->setFarClipDistance(1000);
+  }
+}
+
+void WideAngleCamera::RenderImpl()
+{
+  math::Quaternion orient = this->GetWorldRotation();
+  math::Vector3 pos = this->GetWorldPosition();
+
+  for(int i=0;i<6;i++)
+  {
+    this->envCameras[i]->setPosition(this->camera->getRealPosition());
+    this->envCameras[i]->setOrientation(this->camera->getRealOrientation());
+  }
+
+  this->envCameras[0]->rotate(this->camera->getDerivedUp(),Ogre::Degree(-90));
+  this->envCameras[1]->rotate(this->camera->getDerivedUp(),Ogre::Degree(90));
+  this->envCameras[2]->pitch(Ogre::Degree(90));
+  this->envCameras[3]->pitch(Ogre::Degree(-90));
+  this->envCameras[5]->rotate(this->camera->getDerivedUp(),Ogre::Degree(180));
+
+  for(int i=0;i<6;i++)
+    this->envRenderTargets[i]->update();
+
+  this->renderTarget->update();
 }
--- gazebo/rendering/WideAngleCamera.hh
+++ gazebo/rendering/WideAngleCamera.hh
@@ -14,14 +14,31 @@ namespace gazebo
   {
     class GAZEBO_VISIBLE WideAngleCamera : public Camera
     {
-      public: WideAngleCamera(const std::string &_namePrefix, ScenePtr _scene, bool _autoRender = true);
+      public: WideAngleCamera(const std::string &_namePrefix, ScenePtr _scene,
+                              bool _autoRender = true, int textureSize = 256);
 
       public: ~WideAngleCamera();
       //public: virtual void Load() override;
 
       public: virtual void SetRenderTarget(Ogre::RenderTarget *_target) override;
 
-      public: Ogre::CompositorInstance *wamapInstance;
+      public: void CreateEnvRenderTexture(const std::string &_textureName);
+
+      private: void CreateEnvCameras();
+
+      protected: virtual void RenderImpl();
+
+      public: virtual void Init();
+
+      protected: Ogre::CompositorInstance *wamapInstance;
+
+      private: Ogre::Camera *envCameras[6];
+
+      private: Ogre::RenderTarget *envRenderTargets[6];
+
+      private: Ogre::Texture *envCubeMapTexture;
+
+      private: int envTextureSize;
     };
   }
 }
