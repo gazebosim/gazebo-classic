593f77bcbc55766e49d99426e08955094aa56682
chapulina <burajiru.no.chapulina@gmail.com>
2015-07-17 20:26:37 -0700

    removed all mouse functions from boxmaker

--- gazebo/gui/BoxMaker.cc
+++ gazebo/gui/BoxMaker.cc
@@ -41,11 +41,11 @@ using namespace gui;
 
 unsigned int BoxMaker::counter = 0;
 
+/////////////////////////////////////////////////
 BoxMaker::BoxMaker()
 : EntityMaker()
 {
   this->state = 0;
-  this->leftMousePressed = false;
   this->visualMsg = new msgs::Visual();
   this->visualMsg->mutable_geometry()->set_type(msgs::Geometry::BOX);
   this->visualMsg->mutable_material()->mutable_script()->add_uri(
@@ -56,12 +56,14 @@ BoxMaker::BoxMaker()
             math::Quaternion());
 }
 
+/////////////////////////////////////////////////
 BoxMaker::~BoxMaker()
 {
   this->camera.reset();
   delete this->visualMsg;
 }
 
+/////////////////////////////////////////////////
 void BoxMaker::Start(const rendering::UserCameraPtr _camera)
 {
   this->camera = _camera;
@@ -73,6 +75,7 @@ void BoxMaker::Start(const rendering::UserCameraPtr _camera)
   this->state = 1;
 }
 
+/////////////////////////////////////////////////
 void BoxMaker::Stop()
 {
   msgs::Request *msg = msgs::CreateRequest("entity_delete",
@@ -85,100 +88,10 @@ void BoxMaker::Stop()
   gui::Events::moveMode(true);
 }
 
+/////////////////////////////////////////////////
 bool BoxMaker::IsActive() const
 {
   return this->state > 0;
-}
-
-void BoxMaker::OnMousePush(const common::MouseEvent &_event)
-{
-  if (this->state == 0)
-    return;
-
-  this->mousePushPos = _event.PressPos();
-}
-
-/////////////////////////////////////////////////
-void BoxMaker::OnMouseRelease(const common::MouseEvent &_event)
-{
-  if (this->state == 0)
-    return;
-
-  this->state++;
-
-  this->mouseReleasePos = _event.Pos();
-
-  if (this->state == 3)
-  {
-    this->CreateTheEntity();
-    this->Stop();
-  }
-}
-
-/////////////////////////////////////////////////
-void BoxMaker::OnMouseMove(const common::MouseEvent &_event)
-{
-  if (this->state != 2)
-    return;
-
-  math::Vector3 p = msgs::Convert(this->visualMsg->pose().position());
-  math::Vector3 scale = msgs::Convert(this->visualMsg->geometry().box().size());
-
-  scale.z = (this->mouseReleasePos.y - _event.Pos().Y())*0.01;
-  if (!_event.Shift())
-    scale.z = rint(scale.z);
-  p.z = scale.z/2.0;
-
-  msgs::Set(this->visualMsg->mutable_pose()->mutable_position(), p);
-  msgs::Set(this->visualMsg->mutable_geometry()->mutable_box()->mutable_size(),
-      scale);
-
-  this->visPub->Publish(*this->visualMsg);
-}
-
-/////////////////////////////////////////////////
-void BoxMaker::OnMouseDrag(const common::MouseEvent &_event)
-{
-  if (this->state != 1)
-    return;
-
-  math::Vector3 norm(0, 0, 1);
-  math::Vector3 p1, p2;
-
-  if (!this->camera->GetWorldPointOnPlane(this->mousePushPos.x,
-                                          this->mousePushPos.y,
-                                          math::Plane(norm), p1))
-  {
-    gzerr << "Invalid mouse point\n";
-    return;
-  }
-
-  p1 = this->GetSnappedPoint(p1);
-
-  if (!this->camera->GetWorldPointOnPlane(
-        _event.Pos().X(), _event.Pos().Y() , math::Plane(norm), p2))
-  {
-    gzerr << "Invalid mouse point\n";
-    return;
-  }
-
-  p2 = this->GetSnappedPoint(p2);
-
-  msgs::Set(this->visualMsg->mutable_pose()->mutable_position(), p1);
-
-  math::Vector3 scale = p1-p2;
-  math::Vector3 p = msgs::Convert(this->visualMsg->pose().position());
-
-  scale.z = 0.01;
-  p.x = p1.x - scale.x/2.0;
-  p.y = p1.y - scale.y/2.0;
-
-
-  msgs::Set(this->visualMsg->mutable_pose()->mutable_position(), p);
-  msgs::Set(this->visualMsg->mutable_geometry()->mutable_box()->mutable_size(),
-      scale.GetAbs());
-
-  this->visPub->Publish(*this->visualMsg);
 }
 
 /////////////////////////////////////////////////
--- gazebo/gui/BoxMaker.hh
+++ gazebo/gui/BoxMaker.hh
@@ -55,27 +55,14 @@ namespace gazebo
       /// Documentation inherited
       public: virtual bool IsActive() const;
 
-      /// Documentation inherited
-      public: virtual void OnMousePush(const common::MouseEvent &_event);
-
-      /// Documentation inherited
-      public: virtual void OnMouseRelease(const common::MouseEvent &_event);
-
-      /// Documentation inherited
-      public: virtual void OnMouseDrag(const common::MouseEvent &_event);
-
-      /// Documentation inherited
-      public: virtual void OnMouseMove(const common::MouseEvent &_event);
-
       /// \brief Get the SDF information for the box.
       /// \return The SDF as a string.
       public: std::string GetSDFString();
 
       /// Documentation inherited
       private: virtual void CreateTheEntity();
+
       private: int state;
-      private: bool leftMousePressed;
-      private: math::Vector2i mousePushPos, mouseReleasePos;
       private: msgs::Visual *visualMsg;
 
       private: static unsigned int counter;
