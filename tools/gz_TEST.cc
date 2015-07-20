3fc18fc668af95066dd57dc91d79fd0fc6a12449
Steve Peters <scpeters@osrfoundation.org>
2015-07-20 13:40:38 -0700

    Fix gzTest.SDF

--- tools/gz_TEST.cc
+++ tools/gz_TEST.cc
@@ -620,7 +620,7 @@ TEST_F(gzTest, SDF)
   descSums["1.2"] = "f524458ace57d6aabbbc2303da208f65af37ef53";
   descSums["1.3"] = "74a3aa8d31f97328175f43d03410be55631fa0e1";
   descSums["1.4"] = "057f26137669d9d7eeb5a8c6f51e4f4077d9ddcf";
-  descSums["1.5"] = "34d06081d9bee1deff08c669ba756bdbdd393a4f";
+  descSums["1.5"] = "522285759f420eba3b774e610822c357a0a683e2";
 
   // Test each descSum
   for (std::map<std::string, std::string>::iterator iter = descSums.begin();
@@ -639,7 +639,7 @@ TEST_F(gzTest, SDF)
   docSums["1.2"] = "27f9d91080ce8aa18eac27c9d899fde2d4b78785";
   docSums["1.3"] = "ad80986d42eae97baf277118f52d7e8b951d8ea1";
   docSums["1.4"] = "153ddd6ba6797c37c7fcddb2be5362c9969d97a1";
-  docSums["1.5"] = "9c6c8a30ace4be9972e53efa3748e9efb0e17ef0";
+  docSums["1.5"] = "1ccc4861895a2eb331de76a2aa92da5a98b45273";
 
   // Test each docSum
   for (std::map<std::string, std::string>::iterator iter = docSums.begin();
