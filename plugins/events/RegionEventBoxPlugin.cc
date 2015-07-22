c68f9363a3aac940f20516f0b919437ef3e4370b
Ian Chen <ichen@osrfoundation.org>
2015-07-22 12:16:41 -0700

    add box region event

---
+++ plugins/events/RegionEventBoxPlugin.cc
@@ -0,0 +1,195 @@
+/*
+ * Copyright (C) 2015 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include "RegionEventBoxPlugin.hh"
+
+using namespace gazebo;
+
+GZ_REGISTER_MODEL_PLUGIN(RegionEventBoxPlugin)
+
+//////////////////////////////////////////////////
+RegionEventBoxPlugin::RegionEventBoxPlugin()
+  : ModelPlugin(), eventPub(0)
+{
+  this->receiveMutex = new boost::mutex();
+  this->hasStaleSizeAndPose = true;
+}
+
+//////////////////////////////////////////////////
+void RegionEventBoxPlugin::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+{
+  std::cout << "RegionEventBoxPlugin::Load(): model='" << _parent->GetName()
+      << "'" << std::endl << std::flush;
+
+  this->model = _parent;
+  this->modelName = _parent->GetName();
+  this->world = _parent->GetWorld();
+
+  this->node = transport::NodePtr(new transport::Node());
+  this->node->Init();
+
+  this->modelSub = this->node->Subscribe("~/model/info",
+      &RegionEventBoxPlugin::OnModelMsg, this);
+
+  sdf::ElementPtr linkEl = _sdf->GetElement("link");
+  sdf::ElementPtr visualEl = linkEl->GetElement("visual");
+  sdf::ElementPtr geometryEl = visualEl->GetElement("geometry");
+  sdf::ElementPtr boxEl = geometryEl->GetElement("box");
+  this->boxSize = boxEl->Get<math::Vector3>("size");
+  this->boxScale = math::Vector3::One;
+  this->boxPose = this->model->GetWorldPose();
+  this->UpdateRegion(this->boxSize * this->boxScale, this->boxPose);
+
+
+  if (_sdf->HasElement("event"))
+  {
+    sdf::ElementPtr event = _sdf->GetElement("event");
+    std::string eventType = event->Get<std::string>("type");
+
+    if (eventType == "inclusion")
+    {
+      this->eventPub =
+          this->node->Advertise<gazebo::msgs::SimEvent>("/gazebo/sim_events");
+      this->eventSource = gazebo::EventSourcePtr(
+          new EventSource(eventPub, eventType, this->world));
+      this->eventSource->Load(event);
+    }
+  }
+
+  this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+      boost::bind(&RegionEventBoxPlugin::OnUpdate, this, _1));
+}
+
+//////////////////////////////////////////////////
+void RegionEventBoxPlugin::OnModelMsg(ConstModelPtr & _msg)
+{
+  boost::mutex::scoped_lock lock(*this->receiveMutex);
+
+  if (_msg->has_name() && _msg->name() == this->modelName && _msg->has_scale())
+  {
+    this->boxScale = msgs::Convert(_msg->scale());
+    this->hasStaleSizeAndPose = true;
+  }
+
+}
+
+//////////////////////////////////////////////////
+void RegionEventBoxPlugin::OnUpdate(const common::UpdateInfo & _info)
+{
+  if (this->boxPose != this->model->GetWorldPose())
+  {
+    this->boxPose = this->model->GetWorldPose();
+    this->hasStaleSizeAndPose = true;
+  }
+
+  for (unsigned int i = 0; i < this->world->GetModelCount(); ++i)
+  {
+    physics::ModelPtr m = this->world->GetModel(i);
+    std::string name = m->GetName();
+
+    if (name == "ground_plane")
+      continue;
+
+    if (name == this->modelName)
+    {
+      if (this->hasStaleSizeAndPose)
+      {
+        if (!this->UpdateRegion(this->boxSize * this->boxScale, this->boxPose))
+        {
+          std::cerr << "RegionEventPlugin::OnUpdate(): "
+              << "failed to update size and pose for model '" << name << "'"
+              << std::endl << std::flush;
+          return;
+        }
+        this->hasStaleSizeAndPose = false;
+      }
+      continue;
+    }
+
+    auto it = this->insiders.find(m->GetName());
+
+    if (this->box.Contains(m->GetWorldPose().pos))
+    {
+      if (it == this->insiders.end())
+      {
+        this->insiders[m->GetName()] = _info.simTime;
+        if (this->eventPub)
+          this->SendEnteringRegionEvent(m);
+      }
+    }
+    else
+    {
+      if (it != this->insiders.end())
+      {
+        if (this->eventPub)
+          this->SendExitingRegionEvent(m);
+
+        this->insiders.erase(m->GetName());
+      }
+    }
+  }
+}
+
+
+//////////////////////////////////////////////////
+bool RegionEventBoxPlugin::UpdateRegion(const math::Vector3 &_size,
+    const math::Pose &_pose)
+{
+  std::cout << "RegionEventBoxPlugin::UpdateSizeAndPose(): model='"
+      << this->modelName << "'" << std::endl << std::flush;
+
+  math::Vector3 vmin(_pose.pos.x - (_size.x / 2), _pose.pos.y - (_size.y / 2),
+      0.0 /*this->pose.pos.z*/);
+  math::Vector3 vmax(_pose.pos.x + (_size.x / 2), _pose.pos.y + (_size.y / 2),
+      _pose.pos.z + _size.z);
+
+  this->box = math::Box(vmin, vmax);
+
+  return true;
+}
+
+//////////////////////////////////////////////////
+void RegionEventBoxPlugin::SendEnteringRegionEvent(physics::ModelPtr _model)
+{
+  std::cout << "RegionEventBoxPlugin::SendEnteringRegionEvent(): model='"
+      << _model->GetName() << "' region='" << this->modelName << "'"
+      << std::endl << std::flush;
+
+  std::string json = "{";
+  json += "\"state\":\"inside\",";
+  json += "\"region\":\"" + this->modelName + "\", ";
+  json += "\"model\":\"" + _model->GetName() + "\"";
+  json += "}";
+
+  eventSource->Emit(json);
+}
+
+//////////////////////////////////////////////////
+void RegionEventBoxPlugin::SendExitingRegionEvent(physics::ModelPtr _model)
+{
+  std::cout << "RegionEventBoxPlugin::SendExitingRegionEvent(): model='"
+      << _model->GetName() << "' region=\"" << this->modelName << "'"
+      << std::endl << std::flush;
+
+  std::string json = "{";
+  json += "\"state\":\"outside\",";
+  json += "\"region\":\"" + this->modelName + "\", ";
+  json += "\"model\":\"" + _model->GetName() + "\"";
+  json += "}";
+
+  eventSource->Emit(json);
+}
---
+++ plugins/events/RegionEventBoxPlugin.hh
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) 2015 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef _GAZEBO_REGION_EVENT_BOX_PLUGIN_HH_
+#define _GAZEBO_REGION_EVENT_BOX_PLUGIN_HH_
+
+#include <map>
+#include <string>
+
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/Plugin.hh>
+
+#include "EventSource.hh"
+
+namespace gazebo
+{
+
+  class RegionEventBoxPlugin : public ModelPlugin
+  {
+    /// \brief Constructor
+    public: RegionEventBoxPlugin();
+
+    // \brief Documentation Inherited.
+    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
+
+    /// \brief Callback when a model message is received.
+    /// \param[in] _msg model msg
+    public: void OnModelMsg(ConstModelPtr & _msg);
+
+    /// \brief Callback when timestamped poses message is received.
+    /// \param[in] _msg poses stamped msg
+    public:void OnPoseMsg(ConstPosesStampedPtr &_msg);
+
+    /// \brief Updates the box event plugin at every physics iteration
+    /// \param[in] _info Update info
+    public: void OnUpdate(const common::UpdateInfo &_info);
+
+    /// \brief Update box region dimensions and pose.
+    /// \param[in] _scale New scale
+    /// \param[in] _pose New pose
+    private: bool UpdateRegion(const math::Vector3 &_scale,
+        const math::Pose& _pose);
+
+    /// \brief Send event when model enters box region
+    /// \param[in] _model Model that entered the box region.
+    private: void SendEnteringRegionEvent(physics::ModelPtr _model);
+
+    /// \brief Send event when model exits region
+    /// \param[in] _model Model that exit the box region.
+    private: void SendExitingRegionEvent(physics::ModelPtr _model);
+
+    /// \brief Pointer to the World.
+    private: physics::WorldPtr world;
+
+    /// \brief Pointer to the model.
+    private: physics::ModelPtr model;
+
+    /// \brief WorldUpdateBegin connection Event.
+    private: event::ConnectionPtr updateConnection;
+
+    /// \brief Name of model.
+    private: std::string modelName;
+
+    /// \brief Transport node pointer.
+    public: transport::NodePtr node;
+
+    /// \brief Mutex to protect incoming messages
+    public: boost::mutex *receiveMutex;
+
+    /// \brief Box region initial size
+    private: math::Vector3 boxSize;
+
+    /// \brief Box region scale
+    private: math::Vector3 boxScale;
+
+    /// \brief Box region pose.
+    private: math::Pose boxPose;
+
+    /// \brief Box region
+    private: math::Box box;
+
+    /// \brief Subscriber to model/info topic.
+    private: transport::SubscriberPtr modelSub;
+
+    /// \brief Flag set when box region size or pose has changed.
+    private: bool hasStaleSizeAndPose;
+
+    /// \brief A map that stores model names and the sim time when they entered
+    /// the box region.
+    private: std::map<std::string, common::Time> insiders;
+
+    /// \brief Publisher to the sim event topic
+    private: transport::PublisherPtr eventPub;
+
+    /// \brief Pointer to event source object that emits sim events
+    private: gazebo::EventSourcePtr eventSource;
+
+  };
+}
+
+#endif
