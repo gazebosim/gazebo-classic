56a89a1c379bca87af19fd7f14c1938abac6e7a8
Mykola Dolhyi <0xb000@gmail.com>
2015-07-28 00:07:57 +0300

    Remove obsolete shader components

--- media/materials/programs/wide_lens_map_fp.glsl
+++ media/materials/programs/wide_lens_map_fp.glsl
@@ -1,14 +1,13 @@
 
 uniform samplerCube envMap;
 
-uniform int projectionType;
 uniform float HFOV;
 
 uniform float f;
 uniform float c1;
 uniform float c2;
 uniform float c3;
-uniform vec2 fun;
+uniform vec3 fun;
 
 varying vec2 frag_pos;
 
@@ -21,55 +20,25 @@ vec3 map(float th)
 	return vec3(-sin(th)*frag_pos.x/r,sin(th)*frag_pos.y/r,cos(th));
 }
 
-vec3 mercator()
-{
-	float alpha = frag_pos.y*pi/2.0;
-	float phi	= (-frag_pos.x)*pi + pi/2.0;
+// vec3 mercator()
+// {
+// 	float alpha = frag_pos.y*pi/2.0;
+// 	float phi	= (-frag_pos.x)*pi + pi/2.0;
 
-	vec3 tc = vec3(
-		cos(phi)*cos(alpha),
-		sin(alpha),
-		sin(phi)*cos(alpha)
-	);
+// 	vec3 tc = vec3(
+// 		cos(phi)*cos(alpha),
+// 		sin(alpha),
+// 		sin(phi)*cos(alpha)
+// 	);
 
-	return tc;
-}
-
-vec3 gnomonical()
-{
-	return map(atan(r,f));
-}
-
-vec3 stereographic()
-{
-	return map(2.0*atan(r,2.0*f));
-}
-
-vec3 equidistant()
-{
-	return map(r/f);
-}
-
-vec3 equisolid()
-{
-	return map(2.0*asin(r/(2.0*f)));
-}
-
-vec3 orthographic()
-{
-	return map(asin(r/f));
-}
-
-vec3 fallback()
-{
-	return mercator();
-}
+// 	return tc;
+// }
 
 vec3 custom()
 {
 	float param = r/(c1*f);
 
-	float phi_1 = fun.x*asin(param)+fun.y*atan(param);
+	float phi_1 = fun.x*asin(param)+fun.y*atan(param)+fun.z*param;
 
 	return map((phi_1-c3)/c2);
 }
@@ -77,15 +46,7 @@ vec3 custom()
 void main()
 {
 	vec3 tc;
-
-	if(projectionType ==-1) tc = custom();			else
-	if(projectionType == 0) tc = mercator();		else
-	if(projectionType == 1) tc = gnomonical();		else
-	if(projectionType == 2) tc = stereographic();	else
-	if(projectionType == 3) tc = equidistant();		else
-	if(projectionType == 4) tc = equisolid();		else
-	if(projectionType == 5) tc = orthographic();	else
-		tc = fallback();
+	tc = custom();
 
 	gl_FragColor = vec4(textureCube(envMap,tc).rgb,1);
 }
