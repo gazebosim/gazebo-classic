75a729d1ad9bdf670855139cea23e8283d131a5a
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-07-31 04:14:11 +0200

    Include BulletLink header

--- gazebo/physics/bullet/BulletBoxShape.hh
+++ gazebo/physics/bullet/BulletBoxShape.hh
@@ -25,6 +25,7 @@
 #include "gazebo/physics/bullet/BulletPhysics.hh"
 #include "gazebo/physics/bullet/BulletTypes.hh"
 #include "gazebo/physics/bullet/BulletCollision.hh"
+#include "gazebo/physics/bullet/BulletLink.hh"
 #include "gazebo/physics/World.hh"
 #include "gazebo/physics/BoxShape.hh"
 #include "gazebo/util/system.hh"
