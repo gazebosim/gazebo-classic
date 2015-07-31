ade71b288cfd0aa4d9d5a1d93da77c224b1c3b84
chapulina <burajiru.no.chapulina@gmail.com>
2015-07-30 20:24:54 -0700

    prevent crash when clicking ground plane inside editor

--- gazebo/gui/model/ModelCreator.cc
+++ gazebo/gui/model/ModelCreator.cc
@@ -1713,11 +1713,12 @@ bool ModelCreator::OnMouseRelease(const common::MouseEvent &_event)
       g_alignAct->setEnabled(false);
       g_copyAct->setEnabled(!this->selectedLinks.empty());
 
-      if (!vis->IsPlane())
-        return true;
+      //if (!vis->IsPlane())
+        //return true;
     }
   }
-  return false;
+  // We don't want GLWidget handling these events unless we know what's going on
+  return true;
 }
 
 /////////////////////////////////////////////////
