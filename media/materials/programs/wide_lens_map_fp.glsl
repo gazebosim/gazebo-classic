c13ba43c63c0f64c78d275a36a1b20eafd6837e5
Mykola Dolhyi <0xb000@gmail.com>
2015-08-18 07:51:21 +0300

    Clean up shader

--- media/materials/programs/wide_lens_map_fp.glsl
+++ media/materials/programs/wide_lens_map_fp.glsl
@@ -19,20 +19,6 @@ vec3 map(float th)
 	return vec3(-sin(th)*frag_pos.x/r,sin(th)*frag_pos.y/r,cos(th));
 }
 
-// vec3 mercator()
-// {
-// 	float alpha = frag_pos.y*pi/2.0;
-// 	float phi	= (-frag_pos.x)*pi + pi/2.0;
-
-// 	vec3 tc = vec3(
-// 		cos(phi)*cos(alpha),
-// 		sin(alpha),
-// 		sin(phi)*cos(alpha)
-// 	);
-
-// 	return tc;
-// }
-
 void main()
 {
 	float param = r/(c1*f);
@@ -40,15 +26,9 @@ void main()
 
 	theta = (theta-c3)*c2;
 
-	// gl_FragColor = vec4(vec3(c1,c2,c3),1.0);
-	// return;
-
-	{
-		vec3 tc;
-		tc = map(theta);
-		gl_FragColor = vec4(textureCube(envMap,tc).rgb,1);
-	}
+	vec3 tc = map(theta);
+	gl_FragColor = vec4(textureCube(envMap,tc).rgb,1);
 
 	if(theta > cutOffAngle)
-		gl_FragColor.rgb = gl_FragColor.rgb*0.2;
+		gl_FragColor.rgb = gl_FragColor.rgb*0.0;
 }
