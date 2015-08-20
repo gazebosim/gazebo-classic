57124ed83140bbc4138a08d2a10a15b0cda36c2e
Steve Peters <scpeters@osrfoundation.org>
2015-08-20 15:13:18 -0700

    Create simple example of friction cone model

--- examples/plugins/world_edit/world_edit.cc
+++ examples/plugins/world_edit/world_edit.cc
@@ -31,26 +31,8 @@ namespace gazebo
   {
     public: void Load(physics::WorldPtr _parent, sdf::ElementPtr _sdf)
     {
-      // Create a new transport node
-      transport::NodePtr node(new transport::Node());
-
-      // Initialize the node with the world name
-      node->Init(_parent->GetName());
-
-      // Create a publisher on the ~/physics topic
-      transport::PublisherPtr physicsPub =
-        node->Advertise<msgs::Physics>("~/physics");
-
-      msgs::Physics physicsMsg;
-      physicsMsg.set_type(msgs::Physics::ODE);
-
-      // Set the step time
-      physicsMsg.set_max_step_size(0.01);
-
-      // Change gravity
-      msgs::Set(physicsMsg.mutable_gravity(),
-          ignition::math::Vector3d(0.01, 0, 0.1));
-      physicsPub->Publish(physicsMsg);
+      auto physics = _parent->GetPhysicsEngine();
+      physics->SetParam("friction_model", std::string("cone_model"));
     }
   };
 
