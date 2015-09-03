ea3b7277ef6a6a95e7c0b2b4e5272b31afa78675
Steve Peters <scpeters@osrfoundation.org>
2015-09-03 00:38:27 -0700

    Add SetCoG test, which only DART currently passes

--- test/integration/physics_msgs_inertia.cc
+++ test/integration/physics_msgs_inertia.cc
@@ -32,6 +32,11 @@ class InertiaMsgsTest : public ServerFixture,
   /// \param[in] _physicsEngine Type of physics engine to use.
   public: void InertialAccessors(const std::string &_physicsEngine);
 
+  /// \brief Set center of mass of link over ~/model/modify
+  /// and verify that it causes a seesaw to unbalance.
+  /// \param[in] _physicsEngine Type of physics engine to use.
+  public: void SetCoG(const std::string &_physicsEngine);
+
   /// \brief Set mass of link over ~/model/modify
   /// and verify that it causes a seesaw to unbalance.
   /// \param[in] _physicsEngine Type of physics engine to use.
@@ -116,6 +121,71 @@ void InertiaMsgsTest::InertialAccessors(const std::string &_physicsEngine)
 TEST_P(InertiaMsgsTest, InertialAccessors)
 {
   InertialAccessors(GetParam());
+}
+
+/////////////////////////////////////////////////
+void InertiaMsgsTest::SetCoG(const std::string &_physicsEngine)
+{
+  Load("worlds/seesaw.world", true, _physicsEngine);
+  physics::WorldPtr world = physics::get_world("default");
+  ASSERT_TRUE(world != NULL);
+
+  // check the gravity vector
+  physics::PhysicsEnginePtr physics = world->GetPhysicsEngine();
+  ASSERT_TRUE(physics != NULL);
+  EXPECT_EQ(physics->GetType(), _physicsEngine);
+  math::Vector3 g = physics->GetGravity();
+
+  const std::string modelName("plank");
+  auto model = world->GetModel(modelName);
+  ASSERT_TRUE(model != NULL);
+  auto link = model->GetLink();
+  ASSERT_TRUE(link != NULL);
+  auto inertial = link->GetInertial();
+  ASSERT_TRUE(inertial != NULL);
+  const double mass = inertial->GetMass();
+  const math::Vector3 cog = inertial->GetCoG();
+  const math::Vector3 Ixxyyzz = inertial->GetPrincipalMoments();
+  const math::Vector3 Ixyxzyz = inertial->GetProductsofInertia();
+  EXPECT_DOUBLE_EQ(mass, 120);
+  EXPECT_EQ(cog, math::Vector3::Zero);
+  EXPECT_EQ(Ixxyyzz, math::Vector3(2.564, 360.064, 362.5));
+  EXPECT_EQ(Ixyxzyz, math::Vector3::Zero);
+
+  // new center of mass
+  msgs::Model msg;
+  msg.set_name(modelName);
+  msg.add_link();
+  auto msgLink = msg.mutable_link(0);
+  msgLink->set_name("link");
+  msgLink->set_id(link->GetId());
+  auto msgInertial = msgLink->mutable_inertial();
+  const ignition::math::Vector3d newCoG(2.5, 0, 0);
+  msgs::Set(msgInertial->mutable_pose(), ignition::math::Pose3d(
+      newCoG, ignition::math::Quaterniond()));
+
+  // Set inertial properties by publishing to "~/model/modify"
+  transport::PublisherPtr modelPub =
+    this->node->Advertise<msgs::Model>("~/model/modify");
+  modelPub->WaitForConnection();
+  modelPub->Publish(msg, true);
+
+  while (newCoG != inertial->GetCoG().Ign())
+  {
+    world->Step(1);
+    common::Time::MSleep(1);
+    modelPub->Publish(msg, true);
+  }
+  EXPECT_EQ(inertial->GetCoG().Ign(), newCoG);
+
+  world->Step(1000);
+  EXPECT_GT(model->GetWorldPose().rot.GetAsEuler().y, 0.25);
+}
+
+/////////////////////////////////////////////////
+TEST_P(InertiaMsgsTest, SetCoG)
+{
+  SetCoG(GetParam());
 }
 
 /////////////////////////////////////////////////
