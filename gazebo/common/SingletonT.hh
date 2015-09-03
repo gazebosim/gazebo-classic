a54c6355629539ee2919c8e27b7778eafa8c9e24
Nate Koenig <natekoenig@gmail.com>
2015-09-03 10:58:24 -0700

    Updates

--- gazebo/common/SingletonT.hh
+++ gazebo/common/SingletonT.hh
@@ -30,7 +30,11 @@
 /// \class SingletonT SingletonT.hh common/common.hh
 /// \brief Singleton template class
 template <class T>
+#ifdef _WIN32
 class SingletonT
+#else
+class GZ_COMMON_VISIBLE SingletonT
+#endif
 {
   /// \brief Get an instance of the singleton
   public: static T *Instance()
