6beec8a1490364f7e41658c4f21f321135cadde4
Mykola Dolhyi <0xb000@gmail.com>
2015-09-08 22:07:24 +0300

    rollback fov check: sdf element is copied - it has no parent

--- gazebo/rendering/Camera.cc
+++ gazebo/rendering/Camera.cc
@@ -172,8 +172,7 @@ void Camera::Load()
   {
     sdf::ElementPtr elem = this->sdf->GetElement("horizontal_fov");
     double angle = elem->Get<double>();
-    auto type = this->sdf->GetParent()->Get<std::string>("type");
-    if ((angle < 0.01 || angle > M_PI) && type != "wideanglecamera")
+    if (angle < 0.01 || angle > M_PI*2)
     {
       gzthrow("Camera horizontal field of view invalid.");
     }
