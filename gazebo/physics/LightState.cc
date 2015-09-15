d659cdb8072d36a524e3de0876ecdef7b3d96614
chapulina <burajiru.no.chapulina@gmail.com>
2015-09-15 00:06:16 -0700

    added ightstate class

---
+++ gazebo/physics/LightState.cc
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2015 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// #include "gazebo/common/Exception.hh"
+// #include "gazebo/physics/Light.hh"
+// #include "gazebo/physics/Link.hh"
+// #include "gazebo/physics/World.hh"
+#include "gazebo/physics/LightState.hh"
+
+using namespace gazebo;
+using namespace physics;
+
+/////////////////////////////////////////////////
+LightState::LightState(const sdf::ElementPtr _sdf)
+  : State()
+{
+  this->Load(_sdf);
+}
+
+/////////////////////////////////////////////////
+void LightState::Load(const sdf::ElementPtr _elem)
+{
+  // Set the name
+  this->name = _elem->Get<std::string>("name");
+
+  // Set the light pose
+  if (_elem->HasElement("pose"))
+    this->pose = _elem->Get<ignition::math::Pose3d>("pose");
+  else
+    this->pose.Set(0, 0, 0, 0, 0, 0);
+}
+
+/////////////////////////////////////////////////
+const ignition::math::Pose3d LightState::Pose() const
+{
+  return this->pose;
+}
+
+/////////////////////////////////////////////////
+bool LightState::IsZero() const
+{
+  bool result = true;
+
+  return result && this->pose == ignition::math::Pose3d::Zero;
+}
+
+/////////////////////////////////////////////////
+LightState &LightState::operator=(const LightState &_state)
+{
+  State::operator=(_state);
+
+  // Copy the pose
+  this->pose = _state.pose;
+
+  return *this;
+}
+
+/////////////////////////////////////////////////
+LightState LightState::operator-(const LightState &_state) const
+{
+  LightState result;
+
+  result.name = this->name;
+  result.pose.Pos() = this->pose.Pos() - _state.pose.Pos();
+  result.pose.Rot() = _state.pose.Rot().Inverse() * this->pose.Rot();
+
+  return result;
+}
+
+/////////////////////////////////////////////////
+LightState LightState::operator+(const LightState &_state) const
+{
+  LightState result;
+
+  result.name = this->name;
+  result.pose.Pos() = this->pose.Pos() + _state.pose.Pos();
+  result.pose.Rot() = _state.pose.Rot() * this->pose.Rot();
+
+  return result;
+}
+
+/////////////////////////////////////////////////
+void LightState::FillSDF(sdf::ElementPtr _sdf)
+{
+  _sdf->ClearElements();
+
+  _sdf->GetAttribute("name")->Set(this->name);
+  _sdf->GetElement("pose")->Set(this->pose);
+}
+
