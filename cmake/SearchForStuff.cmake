ce0730f7ac06c3d12a6a746b2fa5e9af2cbfc9b0
Mykola Dolhyi <0xb000@gmail.com>
2015-10-07 23:22:41 +0300

    increase required sdformat version

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -480,7 +480,7 @@ endif ()
 
 ########################################
 # Find SDFormat
-set (SDFormat_MIN_VERSION 3.3.0)
+set (SDFormat_MIN_VERSION 3.5.0)
 find_package(SDFormat ${SDFormat_MIN_VERSION})
 
 if (NOT SDFormat_FOUND)
