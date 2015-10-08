f8dbba20b9e15c33eeb64985e50323db2a68de1c
Carlos Agüero <caguero@osrfoundation.org>
2015-10-08 00:48:02 +0000

    SearchForStuff.cmake edited online with Bitbucket

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -618,7 +618,7 @@ endif()
 # Find the Ignition_Transport library
 find_package(ignition-transport0 QUIET REQUIRED)
 if (NOT ignition-transport0_FOUND)
-  BUILD_WARNING ("Missing: Ignition Transport (libignition-transport-dev)")
+  BUILD_WARNING ("Missing: Ignition Transport (libignition-transport0-dev)")
 else()
   set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${IGNITION-TRANSPORT_CXX_FLAGS}")
   include_directories(${IGNITION-TRANSPORT_INCLUDE_DIRS})
