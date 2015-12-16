f974871e371f31c2c1d88749512ce333a300e2cb
Carlos Agüero <caguero@osrfoundation.org>
2015-12-16 10:40:02 -0800

    Typo.

--- gazebo/gui/terrain/TerrainEditorPrivate.hh
+++ gazebo/gui/terrain/TerrainEditorPrivate.hh
@@ -14,8 +14,8 @@
  * limitations under the License.
  *
 */
-#ifndef _GAZEBO_GIU_TERRAINEDITOR_PRIVATE_HH_
-#define _GAZEBO_GIU_TERRAINEDITOR_PRIVATE_HH_
+#ifndef _GAZEBO_GUI_TERRAINEDITOR_PRIVATE_HH_
+#define _GAZEBO_GUI_TERRAINEDITOR_PRIVATE_HH_
 
 namespace gazebo
 {
