38994e92d83ae80c81fcc5da2f8e24b84052dab9
Steve Peters <scpeters@osrfoundation.org>
2015-12-16 16:02:37 -0800

    Fix gzTest.SDF

--- tools/gz_TEST.cc
+++ tools/gz_TEST.cc
@@ -658,21 +658,21 @@ TEST_F(gzTest, SDF)
   path /= "worlds/box_plane_low_friction_test.world";
 
   {
-    // Check empty.world
+    // Check box_plane_low_friction_test.world
     std::string output =
       custom_exec_str(std::string("gz sdf -k ") + path.string());
     EXPECT_EQ(output, "Check complete\n");
   }
 
   {
-    // Print empty.world
+    // Print box_plane_low_friction_test.world
     // Regenerate using:
-    // gz sdf -p test/worlds/empty_different_name.world
-    // | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/"/\\"/g'
+    // gz sdf -p test/worlds/box_plane_low_friction_test.world
+    // | shasum
     std::string output =
       custom_exec_str(std::string("gz sdf -p ") + path.string());
     std::string shasum = gazebo::common::get_sha1<std::string>(output);
-    EXPECT_EQ(shasum, "ea127f9858a5e07c40ef6d949ef6113236adddfa");
+    EXPECT_EQ(shasum, "81960fabd921e1832aeb15d8b1ac39754c0eed81");
   }
 
   path = PROJECT_BINARY_PATH;
