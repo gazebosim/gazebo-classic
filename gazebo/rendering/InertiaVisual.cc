1142ed9e53dc568b2bc381e1b735c8c636e24f93
Steve Peters <scpeters@osrfoundation.org>
2015-12-23 18:01:01 -0800

    Use ignition::math::MassMatrix3d in InertiaVisual to account for off-diagonal inertia terms

--- gazebo/rendering/InertiaVisual.cc
+++ gazebo/rendering/InertiaVisual.cc
@@ -15,6 +15,8 @@
  *
 */
 
+#include <ignition/math/MassMatrix3.hh>
+
 #include "gazebo/math/Vector3.hh"
 #include "gazebo/math/Quaternion.hh"
 #include "gazebo/math/Pose.hh"
@@ -74,40 +76,31 @@ void InertiaVisual::Load(ConstLinkPtr &_msg)
 {
   Visual::Load();
 
-  math::Vector3 xyz(_msg->inertial().pose().position().x(),
-                    _msg->inertial().pose().position().y(),
-                    _msg->inertial().pose().position().z());
-  math::Quaternion q(_msg->inertial().pose().orientation().w(),
-                     _msg->inertial().pose().orientation().x(),
-                     _msg->inertial().pose().orientation().y(),
-                     _msg->inertial().pose().orientation().z());
+  auto xyz = msgs::ConvertIgn(_msg->inertial().pose().position());
+  auto q = msgs::ConvertIgn(_msg->inertial().pose().orientation());
 
-  // Use principal moments of inertia to scale Inertia visual
-  // \todo: rotate to match principal axes when product terms are nonzero
-  // This can be done with Eigen, or with code from the following paper:
-  // A Method for Fast Diagonalization of a 2x2 or 3x3 Real Symmetric Matrix
-  // http://arxiv.org/abs/1306.6291v3
-  double mass = _msg->inertial().mass();
-  double Ixx = _msg->inertial().ixx();
-  double Iyy = _msg->inertial().iyy();
-  double Izz = _msg->inertial().izz();
-  math::Vector3 boxScale;
-  if (mass < 0 || Ixx < 0 || Iyy < 0 || Izz < 0 ||
-      Ixx + Iyy < Izz || Iyy + Izz < Ixx || Izz + Ixx < Iyy)
+  // Use ignition::math::MassMatrix3 to compute
+  // equivalent box size and rotation
+  ignition::math::MassMatrix3d m(_msg->inertial().mass(),
+      ignition::math::Vector3d(_msg->inertial().ixx(),
+                               _msg->inertial().iyy(),
+                               _msg->inertial().izz()),
+      ignition::math::Vector3d(_msg->inertial().ixy(),
+                               _msg->inertial().ixz(),
+                               _msg->inertial().iyz()));
+  ignition::math::Vector3d boxScale;
+  ignition::math::Quaterniond boxRot;
+  if (!m.EquivalentBox(boxScale, boxRot))
   {
-    // Unrealistic inertia, load with default scale
+    // Invalid inertia, load with default scale
     gzlog << "The link " << _msg->name() << " has unrealistic inertia, "
           << "unable to visualize box of equivalent inertia." << std::endl;
     this->Load(math::Pose(xyz, q));
   }
   else
   {
-    // Compute dimensions of box with uniform density and equivalent inertia.
-    boxScale.x = sqrt(6*(Izz + Iyy - Ixx) / mass);
-    boxScale.y = sqrt(6*(Izz + Ixx - Iyy) / mass);
-    boxScale.z = sqrt(6*(Ixx + Iyy - Izz) / mass);
-
-    this->Load(math::Pose(xyz, q), boxScale);
+    // Apply additional rotation by boxRot
+    this->Load(math::Pose(xyz, q * boxRot), boxScale);
   }
 }
 
