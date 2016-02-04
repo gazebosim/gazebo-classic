97cf196cede6ceaa43e148502c9eeb7ddb0e9ba5
Nate Koenig <natekoenig@gmail.com>
2016-02-04 15:21:09 -0800

    small changes

--- gazebo/util/IntrospectionManagerPrivate.hh
+++ gazebo/util/IntrospectionManagerPrivate.hh
@@ -59,7 +59,7 @@ namespace gazebo
       std::string type;
 
       /// \brief ToDo.
-      std::function <bool(gazebo::msgs::Any &_msg)> cb;
+      std::function <bool (gazebo::msgs::Any &_msg)> cb;
     };
 
     /// \brief Private data for the IntrospectionManager class.
