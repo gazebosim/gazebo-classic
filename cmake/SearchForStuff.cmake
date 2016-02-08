0556776ff3ed2a81c20f0cd39501704213b94f0e
Ian Chen <ichen@osrfoundation.org>
2016-02-08 08:13:31 -0800

    update find qwt in cmake

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -691,6 +691,7 @@ find_path(QWT_INCLUDE_DIR NAMES qwt.h PATHS
   /usr/local/include
   "$ENV{LIB_DIR}/include"
   "$ENV{INCLUDE}"
+  /usr/local/lib/qwt.framework/Headers
   PATH_SUFFIXES qwt-qt4 qwt qwt5
   )
 
@@ -699,6 +700,7 @@ find_library(QWT_LIBRARY NAMES qwt qwt6 qwt5 PATHS
   /usr/local/lib
   "$ENV{LIB_DIR}/lib"
   "$ENV{LIB}/lib"
+  /usr/local/lib/qwt.framework
   )
 
 if (QWT_INCLUDE_DIR AND QWT_LIBRARY)
@@ -706,11 +708,8 @@ if (QWT_INCLUDE_DIR AND QWT_LIBRARY)
 endif (QWT_INCLUDE_DIR AND QWT_LIBRARY)
 
 if (HAVE_QWT)
-  if (NOT QWT_FIND_QUIETLY)
-    message(STATUS "Found Qwt: ${QWT_LIBRARY}")
-  endif (NOT QWT_FIND_QUIETLY)
-else ()
-  if (QWT_FIND_REQUIRED)
-    BUILD_ERROR ("Missing: libqwt-dev. Required for plotting.")
-  endif (QWT_FIND_REQUIRED)
+  message (STATUS "Looking for qwt - found")
+else()
+  message (STATUS "Looking for qwt - not found")
+  BUILD_ERROR ("Missing: libqwt-dev. Required for plotting.")
 endif ()
