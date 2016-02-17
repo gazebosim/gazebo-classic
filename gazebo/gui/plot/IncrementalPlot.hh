5a6ca0202bcee3803346847b7c5d7fe05b5c8ea5
Nathan Koenig <natekoenig@gmail.com>
2016-02-17 18:23:37 +0000

    IncrementalPlot.hh edited online with Bitbucket

--- gazebo/gui/plot/IncrementalPlot.hh
+++ gazebo/gui/plot/IncrementalPlot.hh
@@ -15,8 +15,8 @@
  *
 */
 
-#ifndef _GAZEBO_GUI_PLOT_INCREMENTAL_PLOT_HH_
-#define _GAZEBO_GUI_PLOT_INCREMENTAL_PLOT_HH_
+#ifndef _GAZEBO_GUI_PLOT_INCREMENTALPLOT_HH_
+#define _GAZEBO_GUI_PLOT_INCREMENTALPLOT_HH_
 
 #include <memory>
 #include <string>
