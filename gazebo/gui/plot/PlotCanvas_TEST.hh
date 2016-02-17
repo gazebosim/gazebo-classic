87fe8d20a41a507e4b58b23d14c5da9ddf719b60
Nathan Koenig <natekoenig@gmail.com>
2016-02-17 18:50:19 +0000

    PlotCanvas_TEST.hh edited online with Bitbucket

--- gazebo/gui/plot/PlotCanvas_TEST.hh
+++ gazebo/gui/plot/PlotCanvas_TEST.hh
@@ -15,8 +15,8 @@
  *
 */
 
-#ifndef _GAZEBO_GUI_PLOT_PLOT_CANVAS_TEST_HH_
-#define _GAZEBO_GUI_PLOT_PLOT_CANVAS_TEST_HH_
+#ifndef _GAZEBO_GUI_PLOT_PLOTCANVAS_TEST_HH_
+#define _GAZEBO_GUI_PLOT_PLOTCANVAS_TEST_HH_
 
 #include "gazebo/gui/QTestFixture.hh"
 
