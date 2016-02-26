545544a5c794c264e60582780cbbeaadec7b0006
Olivier Crave <olivier.crave@parrot.com>
2016-02-26 11:29:54 +0100

    fix resource leak

--- gazebo/physics/Collision.cc
+++ gazebo/physics/Collision.cc
@@ -73,6 +73,7 @@ void Collision::Fini()
     msgs::Request *msg = msgs::CreateRequest("entity_delete",
         this->GetScopedName()+"__COLLISION_VISUAL__");
     this->requestPub->Publish(*msg, true);
+    delete msg;
   }
 
   Entity::Fini();
