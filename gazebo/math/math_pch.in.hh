e63a14b082233b59480c61e4bc038e29f5abb16f
Peter Mitrano <mitranopeter@gmail.com>
2016-03-13 03:27:46 -0400

    working towards macro for adding pch
    currently the output file name is wrong
    it shouldn't contain the ".in" part

---
+++ gazebo/math/math_pch.in.hh
@@ -0,0 +1,41 @@
+#pragma message("Do not include this file unless generating precompiled headers")
+
+//#include <boost/algorithm/string/classification.hpp>
+//#include <boost/algorithm/string/regex.hpp>
+//#include <boost/algorithm/string/replace.hpp>
+//#include <boost/algorithm/string/split.hpp>
+//#include <boost/assert.hpp>
+//#include <boost/bind.hpp>
+//#include <boost/date_time.hpp>
+//#include <boost/filesystem.hpp>
+//#include <boost/filesystem/operations.hpp>
+//#include <boost/filesystem/path.hpp>
+//#include <boost/function.hpp>
+//#include <boost/iostreams/copy.hpp>
+//#include <boost/iostreams/filter/gzip.hpp>
+//#include <boost/iostreams/filtering_streambuf.hpp>
+//#include <boost/lexical_cast.hpp>
+//#include <boost/thread.hpp>
+//#include <boost/thread/mutex.hpp>
+//#include <boost/unordered_map.hpp>
+//#include <boost/uuid/sha1.hpp>
+
+#include <algorithm>
+#include <assert.h>
+#include <boost/algorithm/string.hpp>
+#include <boost/math/special_functions/fpclassify.hpp>
+#include <boost/math/special_functions/round.hpp>
+#include <boost/random.hpp>
+#include <boost/shared_ptr.hpp>
+#include <cmath>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <math.h>
+#include <string>
+#include <string.h>
+#include <sys/types.h>
+#include <vector>
+
