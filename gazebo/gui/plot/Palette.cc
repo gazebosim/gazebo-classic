a56823d5cda25c6b9ca00401a4f356bb32085779
chapulina <burajiru.no.chapulina@gmail.com>
2016-03-23 10:36:36 -0700

    mutex

--- gazebo/gui/plot/Palette.cc
+++ gazebo/gui/plot/Palette.cc
@@ -15,6 +15,7 @@
  *
 */
 
+#include <mutex>
 #include <set>
 #include <string>
 #include <vector>
@@ -398,6 +399,9 @@ class gazebo::gui::PalettePrivate
 
   /// \brief Communication node.
   public: ignition::transport::Node node;
+
+  /// \brief Mutex to protect the models model update.
+  public: std::mutex modelsMutex;
 };
 
 /////////////////////////////////////////////////
@@ -723,6 +727,8 @@ void Palette::OnIntrospectionUpdate(const gazebo::msgs::Param_V &_msg)
 void Palette::OnIntrospectionUpdate(const std::set<std::string> &_items,
     const bool _result)
 {
+  std::lock_guard<std::mutex> lock(this->dataPtr->modelsMutex);
+
   if (!_result || _items.empty())
   {
     gzerr << "Failure in introspection items update" << std::endl;
