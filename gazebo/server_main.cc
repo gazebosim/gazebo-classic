a804fcda99306ee1fcf2b4f94eb390477fb7ed07
Nate Koenig <natekoenig@gmail.com>
2016-04-07 11:58:55 -0700

    remove nullptr

--- gazebo/server_main.cc
+++ gazebo/server_main.cc
@@ -24,7 +24,7 @@
 //////////////////////////////////////////////////
 int main(int argc, char **argv)
 {
-  std::unique_ptr<gazebo::Server> server = nullptr;
+  std::unique_ptr<gazebo::Server> server;
 
   try
   {
