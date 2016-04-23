ca86ff8e3f613812be55264412c7293bdc07825b
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-04-23 02:23:38 +0200

    Update generator

--- gazebo/common/generator.bash
+++ gazebo/common/generator.bash
@@ -1,20 +1,21 @@
 GAZEBO_ROOT=/home/jrivero/code/gazebo
+# TODO: set iofwd for iostreams. Its own PRs?
 
-for f in $(ls *.hh | grep -v Private); do 
+for f in $(ls *.hh | grep -v Private); do
     # exclusions
     case ${f} in
 	Plugin.hh)
+	Assert
 	    echo "Exception! leave as it"
 	    continue
     esac
 
-    no_ext=${f%.hh} 
-    new=${no_ext}-fwd.hh 
+    no_ext=${f%.hh}
+    new=${no_ext}-fwd.hh
     echo "Generating $new"
     cp ${f} ${new}
     # Update guard for the new file
     sed -i -e "s:_HH_:FWD_HH_:g" ${new}
-    # TODO: set iofwd for iostreams. Its own PRs?
     # TODO: investigate what's going on if there are templates in the
     # header file. Probably need as it.
 
@@ -23,13 +24,18 @@ for f in $(ls *.hh | grep -v Private); do
     # TODO: fix class comment to set: forward declaration doxygen
     # TODO: Remove all comments
     # TODO: Remove public, private or protected lines
+    sed -i -e /.*\brief.*/d ${new}
+    sed -i -e /.*\param.*/d ${new}
+    sed -i -e /.*\return.*/d ${new}
+    sed -i -e /.*\addtogroup.*/d ${new}
+    sed -i -e /.*\private.*/d ${new}
+    sed -i -e /.*\public.*/d ${new}
+    sed -i -e /.*\protected.*/d ${new}
 
+    sed -i -e s:GZ_.*_VISIBLE ::g ${new}
 
     # Add file to mercurial
     hg add ${new}
-
-    header_list="${header_list} ${new}"
-    cmake_list="${full_list}\n${new}"
 
     # Change the include in the headers by the fowarded declaration
     # except for the own .cc file
@@ -38,7 +44,7 @@ for f in $(ls *.hh | grep -v Private); do
 	sed -i -e "s:${f}:${new}:g" ${h}
       fi
     done
-done
 
-echo "Remember to add this list to install headers in the CMAkeLists.txt"
-echo -e ${cmake_list}
+    # Updatte CMakeLists.txt
+    sed -i -e "/${f}/a  ${new}" CMakeLists.txt
+done
