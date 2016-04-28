8b8010180db7d68c7b1ea65fbe158f3b6ca415ab
Nate Koenig <natekoenig@gmail.com>
2016-04-28 08:01:18 -0700

    Update physics documentation

--- gazebo/physics/ode/ODECollision.hh
+++ gazebo/physics/ode/ODECollision.hh
@@ -35,6 +35,10 @@ namespace gazebo
 {
   namespace physics
   {
+
+    /// \addtogroup gazebo_physics_ode
+    /// \{
+
     /// \brief Base class for all ODE collisions.
     class GZ_PHYSICS_VISIBLE ODECollision : public Collision
     {
@@ -107,6 +111,7 @@ namespace gazebo
       /// \brief Function used to set the pose of the ODE object.
       private: void (ODECollision::*onPoseChangeFunc)();
     };
+    /// \}
   }
 }
 #endif
