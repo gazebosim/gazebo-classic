79e78c17778d253f87baa26d2abdd736569173fc
nkoenig <nkoenig@osrfoundation.org>
2016-06-10 13:00:17 -0700

    Fixing build

--- gazebo/physics/Population.cc
+++ gazebo/physics/Population.cc
@@ -349,7 +349,7 @@ void Population::CreatePosesBoxRandom(const PopulationParams &_populParams,
         ignition::math::Rand::DblUniform(0, _populParams.size.z),
         0, 0, 0);
 
-    _poses.push_back((offset + _populParams.pose).pos);
+    _poses.push_back((offset + _populParams.pose.Ign()).Pos());
   }
 
   // Check that we have generated the appropriate number of poses.
@@ -387,7 +387,7 @@ void Population::CreatePosesBoxUniform(const PopulationParams &_populParams,
   // Step2: Cluster the sampled points in 'modelCount' clusters.
   std::vector<ignition::math::Vector3d> centroids;
   std::vector<unsigned int> labels;
-  math::Kmeans kmeans(obs);
+  ignition::math::Kmeans kmeans(obs);
   kmeans.Cluster(_populParams.modelCount, centroids, labels);
 
   // Step3: Create the list of object positions.
@@ -396,7 +396,7 @@ void Population::CreatePosesBoxUniform(const PopulationParams &_populParams,
   {
     igniiton::math::Pose3d p(centroids[i],
         ignition::math::Quaterniond::Identity);
-    _poses.push_back((p + _populParams.pose).pos);
+    _poses.push_back((p + _populParams.pose.Ign()).Pos());
   }
 
   // Check that we have generated the appropriate number of poses.
@@ -417,7 +417,7 @@ void Population::CreatePosesBoxGrid(const PopulationParams &_populParams,
   {
     for (int j = 0; j < _populParams.cols; ++j)
     {
-      _poses.push_back((offset + _populParams.pose).pos);
+      _poses.push_back((offset + _populParams.pose.Ign()).Pos());
       offset.pos.X() += _populParams.step.x;
     }
     offset.Pos.X() = 0;
@@ -446,7 +446,7 @@ void Population::CreatePosesBoxLinearX(const PopulationParams &_populParams,
   {
     offset.pos.X() =
       _populParams.size.x * i / static_cast<double>(_populParams.modelCount);
-    _poses.push_back((offset + _populParams.pose).pos);
+    _poses.push_back((offset + _populParams.pose.Ign()).Pos());
   }
 
   // Check that we have generated the appropriate number of poses.
@@ -470,7 +470,7 @@ void Population::CreatePosesBoxLinearY(const PopulationParams &_populParams,
   {
     offset.Pos().Y() =
       _populParams.size.y * i / static_cast<double>(_populParams.modelCount);
-    _poses.push_back((offset + _populParams.pose).pos);
+    _poses.push_back((offset + _populParams.pose.Ign()).Pos());
   }
 
   // Check that we have generated the appropriate number of poses.
@@ -494,7 +494,7 @@ void Population::CreatePosesBoxLinearZ(const PopulationParams &_populParams,
   {
     offset.Pos.Z() =
       _populParams.size.z * i / static_cast<double>(_populParams.modelCount);
-    _poses.push_back((offset + _populParams.pose).pos);
+    _poses.push_back((offset + _populParams.pose.Ign()).Pos());
   }
 
   // Check that we have generated the appropriate number of poses.
@@ -518,7 +518,7 @@ void Population::CreatePosesCylinderRandom(const PopulationParams &_populParams,
     offset.Pos().X() = r * cos(ang);
     offset.Pos().Y() = r * sin(ang);
     offset.Pos().Z() = ignition::math::Rand::DblUniform(0, _populParams.length);
-    _poses.push_back((offset + _populParams.pose).pos);
+    _poses.push_back((offset + _populParams.pose.Ign()).Pos());
   }
 
   // Check that we have generated the appropriate number of poses.
@@ -552,7 +552,7 @@ void Population::CreatePosesCylinderUniform(
   // Step2: Cluster the sampled points in 'modelCount' clusters.
   std::vector<ignition::math::Vector3d> centroids;
   std::vector<unsigned int> labels;
-  math::Kmeans kmeans(obs);
+  ignition::math::Kmeans kmeans(obs);
   kmeans.Cluster(_populParams.modelCount, centroids, labels);
 
   // Step3: Create the list of object positions.
@@ -561,7 +561,7 @@ void Population::CreatePosesCylinderUniform(
   for (int i = 0; i < _populParams.modelCount; ++i)
   {
     offset.Pos() = centroids[i];
-    _poses.push_back((offset + _populParams.pose).pos);
+    _poses.push_back((offset + _populParams.pose.Ign()).Pos());
   }
 
   // Check that we have generated the appropriate number of poses.
