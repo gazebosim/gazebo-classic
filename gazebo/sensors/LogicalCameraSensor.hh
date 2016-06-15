dcdfb514374fbd11460389271aa1aaaff94b105e
Nathan Koenig <natekoenig@gmail.com>
2016-06-15 16:26:17 +0000

    LogicalCameraSensor.hh edited online with Bitbucket

--- gazebo/sensors/LogicalCameraSensor.hh
+++ gazebo/sensors/LogicalCameraSensor.hh
@@ -97,7 +97,10 @@ namespace gazebo
       public: virtual bool IsActive() const;
 
       // Documentation inherited.
-      // Move this to the public section in gazebo8.
+      // Move this to the public section in gazebo8. Basically move this
+      // function up in this file to be grouped with other public functions
+      // per the Gazebo style guide. This should replace the GetTopic
+      // function.
       public: virtual std::string Topic() const;
 
       // \internal
