7de3e91acef89c8e3b1d7df935bb0843d62b8533
nkoenig <nkoenig@osrfoundation.org>
2016-06-30 16:00:16 -0700

    Updating for qt4

--- gazebo/gui/GuiIface.cc
+++ gazebo/gui/GuiIface.cc
@@ -73,31 +73,36 @@ Q_DECLARE_METATYPE(common::Time)
 
 //////////////////////////////////////////////////
 // QT message handler that pipes qt messages into gazebo's console system.
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
 void messageHandler(QtMsgType _type, const QMessageLogContext &_context,
     const QString &_msg)
 {
-  QByteArray localMsg = _msg.toLocal8Bit();
+  std::string msg = _msg.toStdString();
+  msg += "(" + _context.function + ")";
+#else
+void messageHandler(QtMsgType _type, const char *_msg)
+{
+  const char *msg = _msg;
+#endif
+
   switch (_type)
   {
     case QtDebugMsg:
-      gzdbg << localMsg.constData() << "(" << _context.file
-       << ":" <<  _context.line << " " << _context.function << std::endl;
+      gzdbg << msg << std::endl;
       break;
 #if QT_VERSION >= QT_VERSION_CHECK(5, 5, 0)
     case QtInfoMsg:
-      gzmsg << localMsg.constData() << "(" << _context.file
-       << ":" <<  _context.line << " " << _context.function << std::endl;
+      gzmsg << msg << std::endl;
       break;
 #endif
     case QtWarningMsg:
-      gzwarn << localMsg.constData() << "(" << _context.file
-       << ":" <<  _context.line << " " << _context.function << std::endl;
+      gzwarn << msg << std::endl;
       break;
     case QtFatalMsg:
     case QtCriticalMsg:
-      gzerr << localMsg.constData() << "(" << _context.file
-       << ":" <<  _context.line << " " << _context.function << std::endl;
+      gzerr << msg << std::endl;
     default:
+      gzwarn << "Unknown QT Message type[" << msg << "]" << std::endl;
       break;
   }
 }
@@ -284,7 +289,11 @@ bool gui::load()
   }
 
   // Register custom message handler
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
   qInstallMessageHandler(messageHandler);
+#else
+  qInstallMsgHandler(messageHandler);
+#endif
 
   g_app = new QApplication(g_argc, g_argv);
   set_style();
