d81d7f2f5804625327e090404177e3f0b3cf7b94
nkoenig <devnull@localhost>
2016-07-01 15:10:20 -0700

    remove comment

--- gazebo/gui/MainWindow.cc
+++ gazebo/gui/MainWindow.cc
@@ -339,35 +339,6 @@ void MainWindow::Init()
         << this->height() << "." << std::endl;
     }
   }
-  // Default window size is entire desktop.
-/*  QSize winSize = QApplication::desktop()->screenGeometry().size();
-
-  // Get the size properties from the INI file.
-  int winWidth = getINIProperty<int>("geometry.width", winSize.width());
-  int winHeight = getINIProperty<int>("geometry.height", winSize.height());
-
-  winWidth = winWidth < 0 ? winSize.width() : winWidth;
-  winHeight = winHeight < 0 ? winSize.height() : winHeight;
-
-  // Get the position properties from the INI file.
-  int winXPos = getINIProperty<int>("geometry.x", 0);
-  int winYPos = getINIProperty<int>("geometry.y", 0);
-
-  this->setGeometry(winXPos, winYPos, winWidth, winHeight);
-
-  if (this->width() > winWidth)
-  {
-    gzwarn << "Requested geometry.width of " << winWidth
-           << " but the minimum width of the window is "
-           << this->width() << "." << std::endl;
-  }
-
-  if (this->height() > winHeight)
-  {
-    gzwarn << "Requested geometry.height of " << winHeight
-           << " but the minimum height of the window is "
-           << this->height() << "." << std::endl;
-  }*/
 
   this->dataPtr->worldControlPub =
     this->dataPtr->node->Advertise<msgs::WorldControl>("~/world_control");
