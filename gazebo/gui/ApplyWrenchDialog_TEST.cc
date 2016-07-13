82fdbbc8956d60cc80e1a2effdc256fe370c265b
Steve Peters <scpeters@osrfoundation.org>
2016-07-13 12:33:31 -0700

    Fix homebrew test failure of UNIT_ApplyWrenchDialog_TEST by raising the line where the mouse pointer checks for the torque visual

--- gazebo/gui/ApplyWrenchDialog_TEST.cc
+++ gazebo/gui/ApplyWrenchDialog_TEST.cc
@@ -471,7 +471,8 @@ void ApplyWrenchDialog_TEST::MouseInteractions()
 
   // Find the box's torque visual
   found = false;
-  gazebo::math::Vector2i mousePoint(glWidget->width()/2, glWidget->height()/2);
+  gazebo::math::Vector2i mousePoint(glWidget->width()/2,
+                                    glWidget->height()/2 - 10);
   while (mousePoint.x < glWidget->width())
   {
     gazebo::rendering::VisualPtr vis = cam->GetVisual(mousePoint);
@@ -481,6 +482,13 @@ void ApplyWrenchDialog_TEST::MouseInteractions()
       break;
     }
     mousePoint.x += 5;
+    QTest::mouseMove(glWidget, QPoint(mousePoint.x, mousePoint.y));
+    // Process some events and draw the screen
+    {
+      gazebo::common::Time::MSleep(15);
+      QCoreApplication::processEvents();
+      mainWindow->repaint();
+    }
   }
 
   if (!found)
