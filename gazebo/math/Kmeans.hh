b6221b58e022454d143331c5a50ff24b6e48fd1d
Steve Peters <scpeters@osrfoundation.org>
2016-07-20 12:17:21 -0700

    Use ignition::math::Kmeans and deprecate gazebo::math::Kmeans

--- gazebo/math/Kmeans.hh
+++ gazebo/math/Kmeans.hh
@@ -37,7 +37,7 @@ namespace gazebo
     {
       /// \brief constructor
       /// \param[in] _obs Set of observations to cluster.
-      public: Kmeans(const std::vector<Vector3> &_obs);
+      public: Kmeans(const std::vector<Vector3> &_obs) GAZEBO_DEPRECATED(8.0);
 
       /// \brief Destructor
       public: virtual ~Kmeans();
