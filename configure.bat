9eafd22730b81dfd509bde9609bd6be1b95e0554
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-09-06 22:28:26 +0200

    Added support for ignition-msgs to configure.bat

--- configure.bat
+++ configure.bat
@@ -34,6 +34,7 @@ set CURL_LIBRARY_NAME=libcurl_a
 @set SDFORMAT_PATH=%cd%\..\..\sdformat\build\install\%build_type%
 @set IGNITION-MATH_PATH=%cd%\..\..\ign-math\build\install\%build_type%
 @set IGNITION-TRANSPORT_PATH=%cd%\..\..\ign-transport2\build\install\%build_type%
+@set IGNITION-MSGS_PATH=%cd%\..\..\ign-msgs0\build\install\%build_type%
 
 @set TBB_PATH=%cd%\..\..\tbb43_20141023oss
 @set TBB_LIBRARY_DIR=%TBB_PATH%\lib\intel64\vc12
@@ -60,7 +61,7 @@ set OGRE_LIB_SUFFIX=.lib
 @set PATH=%QT4_BIN_DIR%;%PATH%
 
 cmake -Wno-dev -G "NMake Makefiles"^
-    -DCMAKE_PREFIX_PATH="%SDFORMAT_PATH%;%IGNITION-MATH_PATH%;%IGNITION-TRANSPORT_PATH%"^
+    -DCMAKE_PREFIX_PATH="%SDFORMAT_PATH%;%IGNITION-MATH_PATH%;%IGNITION-TRANSPORT_PATH%;%IGNITION-MSGS_PATH%"^
     -DUSE_EXTERNAL_TINYXML:BOOL=False^
     -DUSE_EXTERNAL_TINYXML2:BOOL=False^
     -DFREEIMAGE_RUNS=1^
