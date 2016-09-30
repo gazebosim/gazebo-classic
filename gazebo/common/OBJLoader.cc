d2bebf9ac2f5f3be03759bb18c93f55c6b015566
Ian Chen <ichen@osrfoundation.org>
2016-09-30 11:13:25 -0700

    create a submesh per material

--- gazebo/common/OBJLoader.cc
+++ gazebo/common/OBJLoader.cc
@@ -86,132 +86,92 @@ Mesh *OBJLoader::Load(const std::string &_filename)
   Mesh *mesh = new Mesh();
   mesh->SetPath(path);
 
-  for (auto s: shapes)
+  for (auto const s: shapes)
   {
-    std::unique_ptr<SubMesh> subMesh(new SubMesh());
-    subMesh->SetName(s.name);
-    subMesh->SetPrimitiveType(SubMesh::TRIANGLES);
-
-    // load faces
-    int index = 0;
-    for (unsigned int j = 0; j < s.mesh.indices.size() / 3; ++j)
+    // obj mesh assigns a material id to each 'face' but gazebo assigns a single
+    // material to each 'submesh'. The strategy here is to identify
+    // the number of unique material ids in each obj shape and create a new
+    // submesh per unique material id
+    std::map<int, SubMesh *> subMeshMatId;
+    for (auto const id :  s.mesh.material_ids)
     {
-      auto idx0 = s.mesh.indices[3*j];
-      auto idx1 = s.mesh.indices[3*j+1];
-      auto idx2 = s.mesh.indices[3*j+2];
-
-      // vertices
-      int vi0 = idx0.vertex_index;
-      int vi1 = idx1.vertex_index;
-      int vi2 = idx2.vertex_index;
-      if (vi0 < 0 || vi1 < 0 || vi2 < 0)
+      if (subMeshMatId.find(id) == subMeshMatId.end())
       {
-        gzerr << "Error loading submesh vertex index" <<std::endl;
-        continue;
-      }
+        std::unique_ptr<SubMesh> subMesh(new SubMesh());
+        subMesh->SetName(s.name);
+        subMesh->SetPrimitiveType(SubMesh::TRIANGLES);
+        subMeshMatId[id] = subMesh.get();
 
-      ignition::math::Vector3d v0(attrib.vertices[3 * vi0],
-                                  attrib.vertices[3 * vi0 + 1],
-                                  attrib.vertices[3 * vi0 + 2]);
-      ignition::math::Vector3d v1(attrib.vertices[3 * vi1],
-                                  attrib.vertices[3 * vi1 + 1],
-                                  attrib.vertices[3 * vi1 + 2]);
-      ignition::math::Vector3d v2(attrib.vertices[3 * vi2],
-                                  attrib.vertices[3 * vi2 + 1],
-                                  attrib.vertices[3 * vi2 + 2]);
-      subMesh->AddVertex(v0);
-      subMesh->AddVertex(v1);
-      subMesh->AddVertex(v2);
-
-      // normals
-      if (attrib.normals.size() > 0)
-      {
-        int ni0 = idx0.vertex_index;
-        int ni1 = idx1.vertex_index;
-        int ni2 = idx2.vertex_index;
-        if (ni0 < 0 || ni1 < 0 || ni2 < 0)
+        Material *mat = nullptr;
+        auto m = materials[id];
+        if (materialIds.find(m.name) != materialIds.end())
         {
-          gzerr << "Error loading submesh normal index" <<std::endl;
+          mat = materialIds[m.name];
         }
         else
         {
-          ignition::math::Vector3d n0(attrib.normals[3 * ni0],
-                                      attrib.normals[3 * ni0 + 1],
-                                      attrib.normals[3 * ni0 + 2]);
-          ignition::math::Vector3d n1(attrib.normals[3 * ni1],
-                                      attrib.normals[3 * ni1 + 1],
-                                      attrib.normals[3 * ni1 + 2]);
-          ignition::math::Vector3d n2(attrib.normals[3 * ni2],
-                                      attrib.normals[3 * ni2 + 1],
-                                      attrib.normals[3 * ni2 + 2]);
-          subMesh->AddNormal(n0);
-          subMesh->AddNormal(n1);
-          subMesh->AddNormal(n2);
+          // Create new material and pass it to mesh who will take ownership of
+          // the object
+          mat = new Material();
+          mat->SetAmbient(Color(m.ambient[0], m.ambient[1], m.ambient[2]));
+          mat->SetDiffuse(Color(m.diffuse[0], m.diffuse[1], m.diffuse[2]));
+          mat->SetSpecular(Color(m.specular[0], m.specular[1], m.specular[2]));
+          mat->SetEmissive(Color(m.emission[0], m.emission[1], m.emission[2]));
+          mat->SetShininess(m.shininess);
+          mat->SetTransparency(1.0 - m.dissolve);
+          mat->SetTextureImage(m.diffuse_texname, path.c_str());
+          materialIds[m.name] = mat;
         }
+        int matIndex = mesh->GetMaterialIndex(mat);
+        if (matIndex < 0)
+          matIndex = mesh->AddMaterial(mat);
+        subMesh->SetMaterialIndex(matIndex);
+        mesh->AddSubMesh(subMesh.release());
       }
-
-      // texcoords
-      if (attrib.texcoords.size() > 0)
-      {
-        int ti0 = idx0.texcoord_index;
-        int ti1 = idx1.texcoord_index;
-        int ti2 = idx2.texcoord_index;
-
-        if (ti0 < 0 || ti1 < 0 || ti2 < 0)
-        {
-          gzerr << "Error loading submesh texcoord index" <<std::endl;
-        }
-        else
-        {
-          ignition::math::Vector2d t0(attrib.texcoords[2 * ti0],
-                                      attrib.texcoords[2 * ti0 + 1]);
-          ignition::math::Vector2d t1(attrib.texcoords[2 * ti1],
-                                      attrib.texcoords[2 * ti1 + 1]);
-          ignition::math::Vector2d t2(attrib.texcoords[2 * ti2],
-                                      attrib.texcoords[2 * ti2 + 1]);
-
-          // the v texcoords seem to be inverted
-          subMesh->AddTexCoord(t0.X(), 1.0-t0.Y());
-          subMesh->AddTexCoord(t1.X(), 1.0-t1.Y());
-          subMesh->AddTexCoord(t2.X(), 1.0-t2.Y());
-        }
-      }
-
-      // indices
-      subMesh->AddIndex(index++);
-      subMesh->AddIndex(index++);
-      subMesh->AddIndex(index++);
     }
 
-    // materials
-    if (!materials.empty() && !s.mesh.material_ids.empty())
+    unsigned int indexOffset = 0;
+    // For each face
+    for (unsigned int f = 0; f < s.mesh.num_face_vertices.size(); ++f)
     {
-      Material *mat = nullptr;
-      auto m = materials[s.mesh.material_ids[0]];
-      if (materialIds.find(m.name) != materialIds.end())
+      // find the submesh that corresponds to the current face material
+      unsigned int matId = s.mesh.material_ids[f];
+      SubMesh *subMesh = subMeshMatId[matId];
+
+      unsigned int fnum = s.mesh.num_face_vertices[f];
+      // For each vertex in the face
+      for (unsigned int v = 0; v < fnum; ++v)
       {
-        mat = materialIds[m.name];
+        auto i = s.mesh.indices[indexOffset + v];
+
+        // vertices
+        int vIdx = i.vertex_index;
+        ignition::math::Vector3d vertex(attrib.vertices[3 * vIdx],
+                                        attrib.vertices[3 * vIdx + 1],
+                                        attrib.vertices[3 * vIdx + 2]);
+        subMesh->AddVertex(vertex);
+
+        // normals
+        if (attrib.normals.size() > 0)
+        {
+          int nIdx = i.normal_index;
+          ignition::math::Vector3d normal(attrib.normals[3 * nIdx],
+                                          attrib.normals[3 * nIdx + 1],
+                                          attrib.normals[3 * nIdx + 2]);
+          subMesh->AddNormal(normal);
+        }
+        // texcoords
+        if (attrib.texcoords.size() > 0)
+        {
+          int tIdx = i.texcoord_index;
+          ignition::math::Vector2d uv(attrib.texcoords[2 * tIdx],
+                                      attrib.texcoords[2 * tIdx + 1]);
+          subMesh->AddTexCoord(uv.X(), 1.0-uv.Y());
+        }
+        subMesh->AddIndex(subMesh->GetIndexCount());
       }
-      else
-      {
-        // Create new material and pass it to mesh who will take ownership of
-        // the object
-        mat = new Material();
-        mat->SetAmbient(Color(m.ambient[0], m.ambient[1], m.ambient[2]));
-        mat->SetDiffuse(Color(m.diffuse[0], m.diffuse[1], m.diffuse[2]));
-        mat->SetSpecular(Color(m.specular[0], m.specular[1], m.specular[2]));
-        mat->SetEmissive(Color(m.emission[0], m.emission[1], m.emission[2]));
-        mat->SetShininess(m.shininess);
-        mat->SetTransparency(1.0 - m.dissolve);
-        mat->SetTextureImage(m.diffuse_texname, path.c_str());
-        materialIds[m.name] = mat;
-      }
-      int matIndex = mesh->GetMaterialIndex(mat);
-      if (matIndex < 0)
-        matIndex = mesh->AddMaterial(mat);
-      subMesh->SetMaterialIndex(matIndex);
+      indexOffset += fnum;
     }
-    mesh->AddSubMesh(subMesh.release());
   }
 
   return mesh;
