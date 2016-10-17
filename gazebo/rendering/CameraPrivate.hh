7dd3a80101201dc53f70d97ff668e30e961ba394
Olivier Crave <olivier.crave@parrot.com>
2016-10-17 09:16:37 +0200

    remove '_' prefix from header guard

--- gazebo/rendering/CameraPrivate.hh
+++ gazebo/rendering/CameraPrivate.hh
@@ -14,8 +14,8 @@
  * limitations under the License.
  *
 */
-#ifndef _GAZEBO_RENDERING_CAMERAPRIVATE_HH_
-#define _GAZEBO_RENDERING_CAMERAPRIVATE_HH_
+#ifndef GAZEBO_RENDERING_CAMERAPRIVATE_HH_
+#define GAZEBO_RENDERING_CAMERAPRIVATE_HH_
 
 #include <deque>
 #include <mutex>
