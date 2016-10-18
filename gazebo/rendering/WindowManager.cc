936bce26a56f5743f015e1f4da81e290ea670c74
Steve Peters <scpeters@osrfoundation.org>
2016-10-17 17:46:49 -0700

    Use __APPLE__ instead of Q_OS_MAC, and don't use QT headers in rendering library

--- gazebo/rendering/WindowManager.cc
+++ gazebo/rendering/WindowManager.cc
@@ -16,10 +16,6 @@
 */
 #include <math.h>
 
-#ifdef  __APPLE__
-# include <QtCore/qglobal.h>
-#endif
-
 #include "gazebo/rendering/ogre_gazebo.h"
 
 #include "gazebo/common/Events.hh"
@@ -85,7 +81,7 @@ int WindowManager::CreateWindow(const std::string &_ogreHandle,
   Ogre::RenderWindow *window = NULL;
 
   // Mac and Windows *must* use externalWindow handle.
-#if defined(Q_OS_MAC) || defined(_MSC_VER)
+#if defined(__APPLE__) || defined(_MSC_VER)
   params["externalWindowHandle"] = _ogreHandle;
 #else
   params["parentWindowHandle"] = _ogreHandle;
