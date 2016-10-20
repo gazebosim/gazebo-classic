de341de4c4495efc4b51c3625d11674d6717a5e3
nkoenig <nkoenig@osrfoundation.org>
2016-10-20 09:31:28 -0700

    Revert change

--- gazebo/rendering/UserCamera.cc
+++ gazebo/rendering/UserCamera.cc
@@ -59,7 +59,7 @@ UserCamera::UserCamera(const std::string &_name, ScenePtr _scene,
   // Set default UserCamera render rate to 120Hz when stereo rendering is
   // enabled. Otherwise use 60Hz.
   // Some padding is added for safety.
-  this->SetRenderRate(_stereoEnabled ? 125.0 : 65.0);
+  this->SetRenderRate(_stereoEnabled ? 130.0 : 70.0);
 
   this->SetUseSDFPose(false);
 }
