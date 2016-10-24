d44dbb812c259fda0e75e03dfd9fa973f6b76236
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-10-24 17:01:49 +0200

    Mark constants as const to fix C2864

--- plugins/ArduCopterPlugin.cc
+++ plugins/ArduCopterPlugin.cc
@@ -142,9 +142,9 @@ class Rotor
   public: double samplingRate;
   public: ignition::math::OnePole<double> velocityFilter;
 
-  public: static double kDefaultRotorVelocitySlowdownSim = 10.0;
-  public: static double kDefaultFrequencyCutoff = 5.0;
-  public: static double kDefaultSamplingRate = 0.2;
+  public: const static double kDefaultRotorVelocitySlowdownSim = 10.0;
+  public: const static double kDefaultFrequencyCutoff = 5.0;
+  public: const static double kDefaultSamplingRate = 0.2;
 };
 
 // Private data class
