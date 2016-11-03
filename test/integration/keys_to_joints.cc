52cc13a2e0723eeef4a9ad6cb573bd4afa9d79bf
chapulina <burajiru.no.chapulina@gmail.com>
2016-11-03 15:23:58 -0700

    vel and force test with simple arm

--- test/integration/keys_to_joints.cc
+++ test/integration/keys_to_joints.cc
@@ -24,7 +24,7 @@ using namespace gazebo;
 class KeysToJoints : public ServerFixture,
                 public testing::WithParamInterface<const char*>
 {
-  /// \brief Key to joint position
+  /// \brief Key to joint commands
   /// \param[in] _physicsEngine Physics engine to use.
   public: void Commands(const std::string &_physicsEngine);
 };
@@ -51,7 +51,7 @@ void KeysToJoints::Commands(const std::string &_physicsEngine)
   if (_physicsEngine == "dart")
     tol = 0.001;
 
-  double target = 0.015;
+  double target = 0.05;
   double current = -0.8;
 
   int maxSleep = 30;
@@ -83,6 +83,101 @@ void KeysToJoints::Commands(const std::string &_physicsEngine)
   // Trigger key to decrease position
   current = joint->GetAngle(0).Radian();
   msg.set_int_value(105);
+  keyboardPub->Publish(msg);
+
+  sleep = 0;
+  while (std::abs(current - joint->GetAngle(0).Radian()) < target &&
+      sleep < maxSleep)
+  {
+    common::Time::MSleep(100);
+    ++sleep;
+  }
+  EXPECT_GT(std::abs(current - joint->GetAngle(0).Radian()), target);
+  EXPECT_GT(current, joint->GetAngle(0).Radian());
+
+  // Velocity teleop
+  joint = model->GetJoint("simple_arm::arm_shoulder_pan_joint");
+  ASSERT_NE(joint, nullptr);
+
+  // Wait to reach initial value
+  current = 0;
+  target = 0.09;
+
+  tol = 1e-5;
+  // TODO: Figure out why ode never stops
+  if (_physicsEngine == "ode")
+    tol = 0.2;
+  else if (_physicsEngine == "dart")
+    tol = 1e-4;
+
+  sleep = 0;
+  while (std::abs(current - joint->GetVelocity(0)) > tol && sleep < maxSleep)
+  {
+    common::Time::MSleep(100);
+    ++sleep;
+  }
+  EXPECT_LT(std::abs(current - joint->GetVelocity(0)), tol);
+
+  // Trigger key to have a positive velocity
+  msg.set_int_value(104);
+  keyboardPub->Publish(msg);
+
+  sleep = 0;
+  while (joint->GetVelocity(0) < target && sleep < maxSleep)
+  {
+    common::Time::MSleep(100);
+    ++sleep;
+  }
+  EXPECT_GT(joint->GetVelocity(0), target);
+
+  // Trigger key to have a negative velocity
+  msg.set_int_value(54);
+  keyboardPub->Publish(msg);
+
+  sleep = 0;
+  while (joint->GetVelocity(0) > -target && sleep < maxSleep)
+  {
+    common::Time::MSleep(100);
+    ++sleep;
+  }
+  EXPECT_LT(joint->GetVelocity(0), -target);
+
+  // Trigger key to stop
+  msg.set_int_value(121);
+  keyboardPub->Publish(msg);
+
+  sleep = 0;
+  while (std::abs(joint->GetVelocity(0)) > tol && sleep < maxSleep)
+  {
+    common::Time::MSleep(100);
+    ++sleep;
+  }
+  EXPECT_LT(std::abs(joint->GetVelocity(0)), tol);
+
+  // Force teleop
+  joint = model->GetJoint("simple_arm::arm_elbow_pan_joint");
+  ASSERT_NE(joint, nullptr);
+
+  target = 0.015;
+
+  // Trigger key to push in positive direction
+  current = joint->GetAngle(0).Radian();
+  msg.set_int_value(106);
+  keyboardPub->Publish(msg);
+
+  sleep = 0;
+  while (std::abs(current - joint->GetAngle(0).Radian()) < target &&
+      sleep < maxSleep)
+  {
+    common::Time::MSleep(100);
+    ++sleep;
+  }
+  EXPECT_GT(std::abs(current - joint->GetAngle(0).Radian()), target);
+  EXPECT_LT(current, joint->GetAngle(0).Radian());
+
+  // Trigger key to decrease position
+  current = joint->GetAngle(0).Radian();
+  msg.set_int_value(117);
   keyboardPub->Publish(msg);
 
   sleep = 0;
