4b317f9ba17406d9c1b8a7ca0125291979c94794
chapulina louise <chapulina louise@osrfoundation.org>
2016-12-14 17:27:02 -0800

    Option to specify a topic for sim events plugin

--- plugins/events/SimEventsPlugin.cc
+++ plugins/events/SimEventsPlugin.cc
@@ -70,9 +70,15 @@ void SimEventsPlugin::Load(physics::WorldPtr _parent, sdf::ElementPtr _sdf)
   // Initialize the node with the world name
   this->node->Init(_parent->GetName());
 
+  // Read topic, if specified
+  std::string topic ="/gazebo/sim_events";
+  if (this->sdf->HasElement("topic"))
+  {
+    topic = this->sdf->Get<std::string>("topic");
+  }
+
   // Create a publisher on the Rest plugin topic
-  this->pub = this->node->Advertise<gazebo::msgs::SimEvent>(
-      "/gazebo/sim_events");
+  this->pub = this->node->Advertise<gazebo::msgs::SimEvent>(topic);
 
   // Subscribe to model spawning
   this->spawnSub = this->node->Subscribe("~/model/info",
