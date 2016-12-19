9a98a7622734356136966a3b92f93f0029970446
nkoenig <nkoenig@osrfoundation.org>
2016-12-19 09:36:45 -0800

    Camera sensor ign test

---
+++ test/integration/camera_sensor_igntransport.cc
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2012-2016 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#include <mutex>
+#include <functional>
+
+#include <ignition/math/Rand.hh>
+#include <ignition/transport/Node.hh>
+
+#include "gazebo/physics/physics.hh"
+#include "gazebo/sensors/sensors.hh"
+#include "gazebo/common/common.hh"
+#include "gazebo/common/Timer.hh"
+#include "gazebo/rendering/Camera.hh"
+#include "gazebo/sensors/CameraSensor.hh"
+
+#include "gazebo/test/ServerFixture.hh"
+#include "scans_cmp.h"
+
+using namespace gazebo;
+class CameraSensorIgnTransport : public ServerFixture
+{
+};
+
+std::mutex mutex;
+
+int imageCount = 0;
+
+/////////////////////////////////////////////////
+void OnNewCameraFrame(const ignition::msgs::ImageStamped &/*_msg*/)
+{
+  std::lock_guard<std::mutex> lock(mutex);
+  ++imageCount;
+}
+
+/////////////////////////////////////////////////
+TEST_F(CameraSensorIgnTransport, WorldReset)
+{
+  Load("worlds/empty_test.world");
+
+  // Make sure the render engine is available.
+  if (rendering::RenderEngine::Instance()->GetRenderPathType() ==
+      rendering::RenderEngine::NONE)
+  {
+    gzerr << "No rendering engine, unable to run camera test\n";
+    return;
+  }
+
+  // spawn sensors of various sizes to test speed
+  std::string modelName = "camera_model";
+  std::string cameraName = "camera_sensor";
+  unsigned int width  = 320;
+  unsigned int height = 240;
+  double updateRate = 10;
+  math::Pose setPose, testPose(
+      math::Vector3(-5, 0, 5), math::Quaternion(0, GZ_DTOR(15), 0));
+  SpawnCamera(modelName, cameraName, setPose.pos,
+      setPose.rot.GetAsEuler(), width, height, updateRate);
+  sensors::SensorPtr sensor = sensors::get_sensor(cameraName);
+  sensors::CameraSensorPtr camSensor =
+    std::dynamic_pointer_cast<sensors::CameraSensor>(sensor);
+
+  // Create the ignition::transport node
+  ignition::transport::Node node;
+
+  // Subscribe to the camera topic
+  node.Subscribe(camSensor->TopicIgn(), &OnNewCameraFrame);
+
+  imageCount = 0;
+  int total_images = 20;
+  common::Timer timer;
+
+  common::Time dt = timer.GetElapsed();
+  timer.Reset();
+  timer.Start();
+
+  while (imageCount < total_images && timer.GetElapsed().Double() < 4)
+    common::Time::MSleep(10);
+  dt = timer.GetElapsed();
+  EXPECT_GE(imageCount, total_images);
+  EXPECT_GT(dt.Double(), 1.0);
+  EXPECT_LT(dt.Double(), 3.0);
+}
+
+
+
+
+
+
+int main(int argc, char **argv)
+{
+  // Set a specific seed to avoid occasional test failures due to
+  // statistically unlikely, but possible results.
+  ignition::math::Rand::Seed(42);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
