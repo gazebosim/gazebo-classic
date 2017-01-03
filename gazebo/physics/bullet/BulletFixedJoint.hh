f8bf26678faf4da204cc86ee1d459afeb6b690bb
chapulina <burajiru.no.chapulina@gmail.com>
2017-01-03 12:15:51 -0800

    remove unused includes

--- gazebo/physics/bullet/BulletFixedJoint.hh
+++ gazebo/physics/bullet/BulletFixedJoint.hh
@@ -18,7 +18,6 @@
 #define _GAZEBO_BULLETFIXEDJOINT_HH_
 
 #include <string>
-#include "gazebo/math/Angle.hh"
 #include "gazebo/math/Vector3.hh"
 #include "gazebo/physics/FixedJoint.hh"
 #include "gazebo/physics/bullet/BulletJoint.hh"
