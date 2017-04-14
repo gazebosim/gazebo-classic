7b0fcd59cb679f91ccbdda37fdc6796c781390e8
Tully Foote <tfoote@osrfoundation.org>
2017-04-13 17:01:26 -0700

    Parameterize the wind based force approximation scalar.
    Default to zero since this is an approximation based on no real value, but if this parameter is set to 1.0 it will provide backwards compatibility.

--- plugins/WindPlugin.cc
+++ plugins/WindPlugin.cc
@@ -121,6 +121,14 @@ void WindPlugin::Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
     }
   }
 
+  if (_sdf->HasElement("force_approximation_scaling_factor"))
+  {
+    sdf::ElementPtr sdfForceApprox =
+      _sdf->GetElement("force_approximation_scaling_factor");
+
+    this->forceApproximationScalingFactor = sdfForceApprox->Get<double>();
+  }
+
   double period = this->world->Physics()->GetMaxStepSize();
 
   this->kMag = period / this->characteristicTimeForWindRise;
@@ -185,6 +193,7 @@ ignition::math::Vector3d WindPlugin::LinearVel(const physics::Wind *_wind,
 /////////////////////////////////////////////////
 void WindPlugin::OnUpdate()
 {
+  // Using the force on mass approximation
   // Get all the models
   physics::Model_V models = this->world->Models();
 
@@ -203,6 +212,7 @@ void WindPlugin::OnUpdate()
 
       // Add wind velocity as a force to the body
       link->AddRelativeForce(link->GetInertial()->Mass() *
+          this->forceApproximationScalingFactor *
           (link->RelativeWindLinearVel() - link->RelativeLinearVel()));
     }
   }
