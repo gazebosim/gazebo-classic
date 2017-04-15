e2942772de61920509eb4c19543ca8fcee9d7a77
Tully Foote <tfoote@osrfoundation.org>
2017-04-14 17:16:00 -0700

    Remove cross coupling of horizontal and vertical wind direction

--- plugins/WindPlugin.cc
+++ plugins/WindPlugin.cc
@@ -111,6 +111,12 @@ void WindPlugin::Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
   {
     sdf::ElementPtr sdfVert = _sdf->GetElement("vertical");
 
+    if (sdfVert->HasElement("time_for_rise"))
+    {
+      this->characteristicTimeForWindRiseVertical =
+        sdfVert->Get<double>("time_for_rise");
+    }
+
     if (sdfVert->HasElement("noise"))
     {
       this->noiseVertical = sensors::NoiseFactory::NewNoiseModel(
@@ -128,6 +134,7 @@ void WindPlugin::Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
   double period = this->world->GetPhysicsEngine()->GetMaxStepSize();
 
   this->kMag = period / this->characteristicTimeForWindRise;
+  this->kMagVertical = period / this->characteristicTimeForWindRiseVertical;
   this->kDir = period / this->characteristicTimeForWindOrientationChange;
 
   wind.SetLinearVelFunc(std::bind(&WindPlugin::LinearVel, this,
@@ -143,10 +150,14 @@ ignition::math::Vector3d WindPlugin::LinearVel(const physics::Wind *_wind,
 {
   // Compute magnitude
   this->magnitudeMean = (1. - this->kMag) * this->magnitudeMean +
-      this->kMag * _wind->LinearVel().Length();
-
+      this->kMag * sqrt(_wind->LinearVel().X() * _wind->LinearVel().X() +
+                        _wind->LinearVel().Y() * _wind->LinearVel().Y());
   double magnitude = this->magnitudeMean;
 
+  // Compute magnitude
+  this->magnitudeMeanVertical = (1. - this->kMagVertical) * this->magnitudeMeanVertical +
+      this->kMagVertical * _wind->LinearVel().Z();
+  
   magnitude += this->magnitudeSinAmplitudePercent * this->magnitudeMean *
     sin(2 * M_PI * this->world->GetSimTime().Double() /
         this->magnitudeSinPeriod);
@@ -179,9 +190,9 @@ ignition::math::Vector3d WindPlugin::LinearVel(const physics::Wind *_wind,
   windVel.Y(magnitude * sin(GZ_DTOR(direction)));
 
   if (this->noiseVertical)
-    windVel.Z(noiseVertical->Apply(this->magnitudeMean));
+    windVel.Z(noiseVertical->Apply(this->magnitudeMeanVertical));
   else
-    windVel.Z(this->magnitudeMean);
+    windVel.Z(this->magnitudeMeanVertical);
 
   return windVel;
 }
