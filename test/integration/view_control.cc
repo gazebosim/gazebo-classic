8592031f943057eafe76cf6e5caa921d86656036
Ian Chen <ichen@osrfoundation.org>
2017-05-12 17:54:34 -0700

    more tweaks

--- test/integration/view_control.cc
+++ test/integration/view_control.cc
@@ -222,7 +222,9 @@ void ViewControlTest::MouseZoomTerrain()
 
   // move camera so that it faces the box but partially occluded by terrain
   cam->SetWorldPose(ignition::math::Pose3d(
-      ignition::math::Vector3d(62.93, -64.88, 9.67),
+      // ignition::math::Vector3d(62.93, -64.88, 9.67),
+      // ignition::math::Vector3d(62.93, -65.38, 9.32)
+      ignition::math::Vector3d(62.93, -65.14, 9.49),
       ignition::math::Vector3d(0, -0.5, 1.57)));
 
   this->ProcessEventsAndDraw(mainWindow);
