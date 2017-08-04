ed145ba95d4993ca4036fc3743fef9687c7f22bc
Steve Peters <scpeters@openrobotics.org>
2017-08-04 14:12:57 -0700

    Re-attach at end of DetachUnpaused to make seg-fault more likely

--- test/integration/harness.cc
+++ test/integration/harness.cc
@@ -301,6 +301,18 @@ void Harness::DetachUnpaused(const std::string &_physicsEngine)
     common::Time::MSleep(10);
   }
   EXPECT_EQ(model->GetJoint("joint1"), nullptr);
+
+  // Now re-attach it at a new location
+  auto attachPub = this->node->Advertise<msgs::Pose>("~/box/harness/attach");
+  ignition::math::Pose3d newPose(1, 2, 3, 0.1, 0.2, 0.3);
+  EXPECT_NE(newPose, model->WorldPose());
+  msgs::Pose msgPose;
+  msgs::Set(&msgPose, newPose);
+  attachPub->Publish(msgPose);
+  common::Time::MSleep(150);
+  EXPECT_NE(model->GetJoint("joint1"), nullptr);
+  VEC_EXPECT_NEAR(newPose.Pos(), model->WorldPose().Pos(), 2e-2);
+  EXPECT_EQ(newPose.Rot(), model->WorldPose().Rot());
 }
 
 TEST_P(Harness, DetachUnpaused)
