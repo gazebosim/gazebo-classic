115a9a6dd11fe1902e257aaeb5ebec6825ca840e
nkoenig <nkoenig@osrfoundation.org>
2017-10-18 09:56:56 -0700

    Work in progress

--- gazebo/common/FBXLoader.cc
+++ gazebo/common/FBXLoader.cc
@@ -40,49 +40,73 @@ union SinglePropertyValue
 
 class FBXProperty
 {
+  public: enum class Type
+          {
+            Float = 0,
+            Double,
+            Int64,
+            Int32,
+            Int16,
+            Bool,
+            String,
+            FloatArray,
+            DoubleArray,
+            Int64Array,
+            Int32Array,
+            BoolArray,
+            Undefined
+          };
+
   public: bool Parse(std::ifstream &_in)
   {
-    char type;
-    _in.read(&type, sizeof(type));
-    switch (type)
+    char ctype;
+    _in.read(&ctype, sizeof(ctype));
+    switch (ctype)
     {
       case 'F':
+        this->type = Type::Float;
         _in.read(reinterpret_cast<char*>(&this->value.flt), 4);
-        std::cout << "F[" << this->value.flt << "]\n";
+        // std::cout << "F[" << this->value.flt << "]\n";
         break;
       case 'D':
+        this->type = Type::Double;
         _in.read(reinterpret_cast<char*>(&this->value.dbl), 8);
-        std::cout << "D[" << this->value.dbl << "]\n";
+        // std::cout << "D[" << this->value.dbl << "]\n";
         break;
       case 'L':
+        this->type = Type::Int64;
         _in.read(reinterpret_cast<char*>(&this->value.int64), 8);
-        std::cout << "L[" << this->value.int64 << "]\n";
+        // std::cout << "L[" << this->value.int64 << "]\n";
         break;
       case 'I':
+        this->type = Type::Int32;
         _in.read(reinterpret_cast<char*>(&this->value.int32), 4);
-        std::cout << "I[" << this->value.int32 << "]\n";
+        // std::cout << "I[" << this->value.int32 << "]\n";
         break;
       case 'Y':
+        this->type = Type::Int16;
         _in.read(reinterpret_cast<char*>(&this->value.int16), 2);
-        std::cout << "Y[" << this->value.int16 << "]\n";
+        // std::cout << "Y[" << this->value.int16 << "]\n";
         break;
       case 'C':
+        this->type = Type::Bool;
         int8_t d;
         _in.read(reinterpret_cast<char*>(&d), 1);
         this->value.b = d & 1;
-        std::cout << "C[" << this->value.b << "]\n";
+        // std::cout << "C[" << this->value.b << "]\n";
         break;
       // 'S': String and 'R': raw bytes.
       // For now, we are shoving this data into an std::string.
       case 'S':
       case 'R':
         {
+          this->type = Type::String;
           uint32_t strLen;
           _in.read(reinterpret_cast<char*>(&strLen), sizeof(strLen));
-          std::string str;
-          str.resize(strLen);
-          _in.read(&str[0], strLen);
-          std::cout << "S/R[" << str << "]\n";
+          this->stringValue;
+          this->stringValue.resize(strLen);
+          _in.read(&this->stringValue[0], strLen);
+          // std::cout << "S/R[" << str << "]\n";
           break;
         }
       // 'f', 'd', 'l', 'i', and 'b' represent value arrays of their
@@ -94,12 +118,11 @@ class FBXProperty
       case 'i':
       case 'b':
         {
-          uint32_t arrayLen;
           uint32_t encoding;
           uint32_t compressedLen;
 
           // Read in the length of the array
-          _in.read(reinterpret_cast<char*>(&arrayLen), sizeof(arrayLen));
+          _in.read(reinterpret_cast<char*>(&this->arrayLen), sizeof(this->arrayLen));
 
           // Read encoding (0: not compressed, 1: compressed)
           _in.read(reinterpret_cast<char*>(&encoding), sizeof(encoding));
@@ -108,56 +131,61 @@ class FBXProperty
           _in.read(reinterpret_cast<char*>(&compressedLen),
                    sizeof(compressedLen));
 
-          std::cout << "Array len[" << arrayLen << "] Encoding["
-                    << encoding << "] COmpressedLen[" << compressedLen << "]\n";
+          // std::cout << "Array len[" << this->arrayLen << "] Encoding["
+          //       << encoding << "] COmpressedLen[" << compressedLen << "]\n";
 
           if (encoding == 0)
           {
-            switch (type)
+            switch (ctype)
             {
               case 'f':
                 {
-                  float *data = new float[arrayLen];
+                  this->type = Type::FloatArray;
+                  float *data = new float[this->arrayLen];
                   _in.read(reinterpret_cast<char*>(&data),
-                      arrayLen * sizeof(data[0]));
+                      this->arrayLen * sizeof(data[0]));
                   delete [] data;
                   break;
                 }
               case 'd':
                 {
-                  double *data = new double[arrayLen];
+                  this->type = Type::DoubleArray;
+                  double *data = new double[this->arrayLen];
                   _in.read(reinterpret_cast<char*>(&data),
-                      arrayLen * sizeof(data[0]));
+                      this->arrayLen * sizeof(data[0]));
                   delete [] data;
                   break;
                 }
               case 'l':
                 {
-                  int64_t *data = new int64_t[arrayLen];
+                  this->type = Type::Int64Array;
+                  int64_t *data = new int64_t[this->arrayLen];
                   _in.read(reinterpret_cast<char*>(&data),
-                      arrayLen * sizeof(data[0]));
+                      this->arrayLen * sizeof(data[0]));
                   delete [] data;
                   break;
                 }
               case 'i':
                 {
-                  int32_t *data = new int32_t[arrayLen];
+                  this->type = Type::Int32Array;
+                  int32_t *data = new int32_t[this->arrayLen];
                   _in.read(reinterpret_cast<char*>(&data),
-                      arrayLen * sizeof(data[0]));
+                      this->arrayLen * sizeof(data[0]));
                   delete [] data;
                   break;
                 }
               case 'b':
                 {
-                  int8_t *data = new int8_t[arrayLen];
+                  this->type = Type::BoolArray;
+                  int8_t *data = new int8_t[this->arrayLen];
                   _in.read(reinterpret_cast<char*>(&data),
-                      arrayLen * sizeof(data[0]));
+                      this->arrayLen * sizeof(data[0]));
                   delete [] data;
                   break;
                 }
               default:
                 {
-                  gzerr << "Unknown array type[" << type << "]\n";
+                  gzerr << "Unknown array type[" << ctype << "]\n";
                 }
             }
           }
@@ -175,24 +203,43 @@ class FBXProperty
               return false;
             }
 
-            uint32_t stride = 0;
-            switch (type)
+            uint32_t stride = 1;
+            switch (ctype)
             {
               case 'f':
+                std::cout << "Parsing a float array\n";
+                this->type = Type::FloatArray;
+                stride = 4;
+                break;
               case 'i':
+                std::cout << "Parsing an int32 array\n";
+                this->type = Type::Int32Array;
                 stride = 4;
                 break;
               case 'd':
-              case 'l':
+                std::cout << "Parsing a double array\n";
+                this->type = Type::DoubleArray;
                 stride = 8;
+                break;
+              case 'l':
+                std::cout << "Parsing a int64 array\n";
+                this->type = Type::Int64Array;
+                stride = 8;
+                break;
+              case 'b':
+                std::cout << "Parsing a bool array\n";
+                this->type = Type::BoolArray;
+                stride = 1;
                 break;
               default:
                 return false;
             }
 
-            uint32_t uncompressedLen = arrayLen * stride;
+            uint32_t uncompressedLen = this->arrayLen * stride;
             char *inData = new char[compressedLen];
-            char *outData = new char[uncompressedLen];
+            if (this->arrayData)
+              delete [] this->arrayData;
+            this->arrayData = new char[uncompressedLen];
 
             _in.read(&inData[0], compressedLen);
 
@@ -206,7 +253,7 @@ class FBXProperty
             // uInt is defined by zlib. It should be a typedef of unsigned
             // int
             zstream.avail_out = uncompressedLen;
-            zstream.next_out = reinterpret_cast<Bytef*>(outData);
+            zstream.next_out = reinterpret_cast<Bytef*>(this->arrayData);
             const int ret = inflate(&zstream, Z_FINISH);
 
             if (ret != Z_STREAM_END && ret != Z_OK) {
@@ -221,7 +268,7 @@ class FBXProperty
         }
       default:
         {
-          gzerr << "Unknown property type[" << type << "]\n";
+          gzerr << "Unknown property type[" << ctype << "]\n";
           return false;
         }
     }
@@ -229,7 +276,83 @@ class FBXProperty
     return true;
   }
 
+  public: void Print(const std::string &_prefix)
+  {
+    switch (this->type)
+    {
+      case Type::Float:
+        std::cout << _prefix << "Flt: " << this->value.flt << std::endl;
+        break;
+      case Type::Double:
+        std::cout << _prefix << "Dbl: " << this->value.dbl << std::endl;
+        break;
+      case Type::Int32:
+        std::cout << _prefix << "I32: " << this->value.int32 << std::endl;
+        break;
+      case Type::Int64:
+        std::cout << _prefix << "I64: " << this->value.int64 << std::endl;
+        break;
+      case Type::Bool:
+        std::cout << _prefix << "Bln: " << this->value.b << std::endl;
+        break;
+      case Type::String:
+        std::cout << _prefix << "Str: " << this->stringValue << std::endl;
+        break;
+      case Type::FloatArray:
+        {
+          std::cout << "Float Array[" << this->arrayLen << "]\n";
+          const float *f = reinterpret_cast<const float*>(this->arrayData);
+          for (uint32_t i = 0; i < this->arrayLen; ++i)
+            std::cout << "F[" << f[i] << "] ";
+          std::cout << std::endl;
+          break;
+        }
+      case Type::DoubleArray:
+        {
+          std::cout << "Double Array[" << this->arrayLen << "]\n";
+          const double *f = reinterpret_cast<const double*>(this->arrayData);
+          for (uint32_t i = 0; i < this->arrayLen; ++i)
+            std::cout << "D[" << f[i] << "] ";
+          std::cout << std::endl;
+          break;
+        }
+      case Type::Int32Array:
+        {
+          std::cout << "Int32 Array[" << this->arrayLen << "]\n";
+          const int32_t *f = reinterpret_cast<const int32_t*>(this->arrayData);
+          for (uint32_t i = 0; i < this->arrayLen; ++i)
+            std::cout << "I[" << f[i] << "] ";
+          std::cout << std::endl;
+          break;
+        }
+      case Type::Int64Array:
+        {
+          std::cout << "Int64 Array[" << this->arrayLen << "]\n";
+          const int64_t *f = reinterpret_cast<const int64_t*>(this->arrayData);
+          for (uint32_t i = 0; i < this->arrayLen; ++i)
+            std::cout << "L[" << f[i] << "] ";
+          std::cout << std::endl;
+          break;
+        }
+      case Type::BoolArray:
+        {
+          const int8_t *f = reinterpret_cast<const int8_t*>(this->arrayData);
+          for (uint32_t i = 0; i < this->arrayLen; ++i)
+            std::cout << "B[" << static_cast<int>(f[i]) << "] ";
+          std::cout << std::endl;
+          break;
+        }
+      default:
+        std::cout << "Unknown type!!!\n";
+        break;
+    }
+  }
+
+  public: Type type = Type::Undefined;
   public: SinglePropertyValue value;
+  public: std::string stringValue = "";
+  public: char *arrayData = nullptr;
+  public: uint32_t arrayLen = 0;
 };
 
 class FBXNode
@@ -241,7 +364,6 @@ class FBXNode
     uint32_t propertyCount = 0;
     uint32_t propertyListLen = 0;
     uint8_t nameLen = 0;
-    std::string name = "";
 
     _in.read(reinterpret_cast<char*>(&endOffset), sizeof(endOffset));
     _in.read(reinterpret_cast<char*>(&propertyCount), sizeof(propertyCount));
@@ -250,50 +372,72 @@ class FBXNode
 
     _in.read(reinterpret_cast<char*>(&nameLen), sizeof(nameLen));
 
-    name.resize(nameLen);
-    _in.read(reinterpret_cast<char*>(&name[0]), nameLen);
+    this->name.resize(nameLen);
+    _in.read(reinterpret_cast<char*>(&this->name[0]), nameLen);
 
-    std::cout << "Off[" << endOffset << "] PC["
-              << propertyCount << "] PLL[" << propertyListLen << "] NameLen["
-              << static_cast<int>(nameLen) << "] Name[" << name << "]\n";
+    // std::cout << "Off[" << endOffset << "] PC["
+    //           << propertyCount << "] PLL[" << propertyListLen << "] NameLen["
+    //           << static_cast<int>(nameLen) << "] Name[" << this->name << "]\n";
 
     if (endOffset == 0)
     {
-      std::cout << "This is the NULL record.\n";
-      return false;
+      // std::cout << "This is the NULL record.\n";
+      return true;
     }
 
     // Read the properties
     for (uint32_t i = 0; i < propertyCount; ++i)
     {
       FBXProperty *property = new FBXProperty;
+      std::cout << "Parsing prop\n";
       if (property->Parse(_in))
       {
-        if (i == 0)
-          this->id = property->int32;
-
         this->properties.push_back(property);
       }
       else
       {
+        std::cout << "Deleteing property\n";
         delete property;
-        break;
+        // break;
       }
     }
 
     while (endOffset > _in.tellg())
     {
-      std::cout << "Has children. EndOffset[" << endOffset << "] Pos["
-                << _in.tellg() << "}\n";
-      FBXNode node;
-      if (!node.Parse(_in))
-        return true;
+      // std::cout << "Has children. EndOffset[" << endOffset << "] Pos["
+      //           << _in.tellg() << "}\n";
+      FBXNode *node = new FBXNode;
+      if (node->Parse(_in))
+      {
+        this->children.push_back(node);
+      }
+      else
+      {
+        std::cout << "Deleting node\n";
+        delete node;
+        // return true;
+      }
     }
     return true;
   }
 
+  public: void Print(const std::string _prefix)
+  {
+    std::cout << _prefix << "Node Name: " << this->name << "\n";
+    for (const auto p : this->properties)
+    {
+      p->Print(_prefix + "  ");
+    }
+
+    for (const auto n : this->children)
+    {
+      n->Print(_prefix + "  ");
+    }
+  }
+
+  public: std::string name;
   public: std::vector<FBXProperty*> properties;
-  public: std::vectr<FBXN attributeType;
+  public: std::vector<FBXNode*> children;
 };
 
 /////////////////////////////////////////////////
@@ -331,15 +475,21 @@ Mesh *FBXLoader::Load(const std::string &_filename)
   int fileSize = stat_buf.st_size;
   std::cout << "File Size[" << fileSize << "]\n";
 
+  std::vector<FBXNode*> nodes;
   while (true)
   {
-    std::cout << "-----------------------------\n";
-    FBXNode node;
-    node.Parse(file);
+    FBXNode *node = new FBXNode;
+    if (node->Parse(file))
+      nodes.push_back(node);
+    else
+      delete node;
 
     if (static_cast<int>(file.tellg()) + 160 + 16 >= fileSize)
       break;
   }
+
+  for (const auto n : nodes)
+    n->Print("");
 
 
   Mesh *mesh = new Mesh();
