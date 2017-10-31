91ff9b8de725c7f1a8439796c0df1436edf04940
Shane Loretz <sloretz@osrfoundation.org>
2017-10-31 10:00:39 -0700

    Use converting constructor

--- gazebo/rendering/ApplyWrenchVisual.cc
+++ gazebo/rendering/ApplyWrenchVisual.cc
@@ -118,10 +118,8 @@ void ApplyWrenchVisual::Load()
   ignition::math::Color matAmbient, matDiffuse, matSpecular, matEmissive;
   rendering::Material::MaterialAsColor(dPtr->unselectedMaterial,
       matAmbient, matDiffuse, matSpecular, matEmissive);
-  common::Color gzAmbient(matAmbient.R(), matAmbient.G(), matAmbient.B(),
-      matAmbient.A());
   dPtr->forceText.Load(this->Name()+"__FORCE_TEXT__",
-      "0N", "Arial", 0.03, gzAmbient);
+      "0N", "Arial", 0.03, matAmbient);
   dPtr->forceText.SetShowOnTop(true);
 
   dPtr->forceText.MovableObject::getUserObjectBindings().setUserAny(
@@ -183,7 +181,7 @@ void ApplyWrenchVisual::Load()
 
   // Torque text
   dPtr->torqueText.Load(this->Name()+"__TORQUE_TEXT__",
-      "0Nm", "Arial", 0.03, gzAmbient);
+      "0Nm", "Arial", 0.03, matAmbient);
   dPtr->torqueText.SetShowOnTop(true);
 
   dPtr->torqueText.MovableObject::getUserObjectBindings().setUserAny(
