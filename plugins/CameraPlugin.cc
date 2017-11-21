8b9bcf8e55357eb721e5fa39b2a5fe4ccaf2316a
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-11-21 18:05:15 +0000

    Remove empty line

--- plugins/CameraPlugin.cc
+++ plugins/CameraPlugin.cc
@@ -35,7 +35,6 @@ CameraPlugin::CameraPlugin()
 /////////////////////////////////////////////////
 CameraPlugin::~CameraPlugin()
 {
-
   this->parentSensor.reset();
   this->camera->DisconnectNewImageFrame(this->newFrameConnection);
   this->camera.reset();
