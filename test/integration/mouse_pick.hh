89d1faba3f90c28604a1479810d5360d3f83a800
Ian Chen <ichen@osrfoundation.org>
2017-12-15 22:02:45 -0800

    fix inserting models

--- test/integration/mouse_pick.hh
+++ test/integration/mouse_pick.hh
@@ -33,6 +33,9 @@ class MousePickingTest : public QTestFixture
 
   /// \brief Verify models can be picked.
   private slots: void Transparency();
+
+  /// \brief Testing picking a visual with invalid mesh.
+  private slots: void InvalidMesh();
 };
 
 #endif
