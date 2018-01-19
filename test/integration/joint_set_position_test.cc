dc6c3fd36834ea6afd9343545381549801e32eef
Steve Peters <scpeters@openrobotics.org>
2018-01-18 18:08:57 -0800

    update error messages for bullet tests

--- test/integration/joint_set_position_test.cc
+++ test/integration/joint_set_position_test.cc
@@ -602,7 +602,7 @@ void JointKinematicTest::SetPositionTranslating(
 {
   if (_physicsEngine == "bullet")
   {
-    gzerr << "Bullet Joint::SetPosition affected by issue #1194.\n";
+    gzerr << "BulletLink::AddForce is not implemented (issue #1476).\n";
     return;
   }
 
@@ -709,7 +709,7 @@ void JointKinematicTest::SetPositionRotating(const std::string &_physicsEngine)
 {
   if (_physicsEngine == "bullet")
   {
-    gzerr << "Bullet Joint::SetPosition affected by issue #1194.\n";
+    gzerr << "BulletLink::AddTorque is not implemented (issue #1476).\n";
     return;
   }
 
