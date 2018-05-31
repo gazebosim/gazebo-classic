b9d224d255278f309458e081fc1e031b28af849d
Michael Carroll <michael@openrobotics.org>
2018-05-31 17:23:08 -0500

    cpplint cleanup.

--- gazebo/util/ProfileManager.cc
+++ gazebo/util/ProfileManager.cc
@@ -1,11 +1,30 @@
+/*
+ * Copyright (C) 2018 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <cmath>
+
 #include "gazebo/util/ProfileManager.hh"
 #include "gazebo/util/ProfileIterator.hh"
 #include "gazebo/util/ProfileManagerPrivate.hh"
 
-#include <cmath>
-
-namespace gazebo {
-  namespace util {
+namespace gazebo
+{
+  namespace util
+  {
     ProfileManager::ProfileManager():
       dataPtr(new ProfileManagerPrivate)
     {
@@ -13,47 +32,47 @@ namespace gazebo {
       this->dataPtr->ResetTime = gazebo::common::Time::Zero;
     }
 
-    ProfileManager::~ProfileManager() {
-      this->CleanupMemory();
-    }
-
-    void ProfileManager::Start_Profile( const char * name )
+    ProfileManager::~ProfileManager()
     {
-      int threadIndex = GetCurrentThreadIndex2();
-      if ((threadIndex<0) || threadIndex >= PROFILER_MAX_THREAD_COUNT)
-        return;
-
-      if (name != this->dataPtr->gCurrentNodes[threadIndex]->Get_Name()) {
-        this->dataPtr->gCurrentNodes[threadIndex] = this->dataPtr->gCurrentNodes[threadIndex]->Get_Sub_Node( name );
-      }
-      this->dataPtr->gCurrentNodes[threadIndex]->Call();
-    }
-
-    void ProfileManager::Stop_Profile( void )
-    {
-      int threadIndex = GetCurrentThreadIndex2();
-      if ((threadIndex<0) || threadIndex >= PROFILER_MAX_THREAD_COUNT)
-        return;
-
-      // Return will indicate whether we should back up to our parent (we may
-      // be profiling a recursive function)
-      if (this->dataPtr->gCurrentNodes[threadIndex]->Return()) {
-        this->dataPtr->gCurrentNodes[threadIndex] = this->dataPtr->gCurrentNodes[threadIndex]->Get_Parent();
-      }
-    }
-
-    void ProfileManager::CleanupMemory(void)
-    {
-      for (int i=0;i<PROFILER_MAX_THREAD_COUNT;i++)
+      for (int i = 0; i < PROFILER_MAX_THREAD_COUNT; i++)
       {
         this->dataPtr->gRoots[i].CleanupMemory();
       }
     }
 
-    void ProfileManager::Reset( void )
+    void ProfileManager::Start_Profile(const char * name)
     {
       int threadIndex = GetCurrentThreadIndex2();
-      if ((threadIndex<0) || threadIndex >= PROFILER_MAX_THREAD_COUNT)
+      if ((threadIndex < 0) || threadIndex >= PROFILER_MAX_THREAD_COUNT)
+        return;
+
+      auto& current_node = this->dataPtr->gCurrentNodes[threadIndex];
+      if (name != current_node->Get_Name())
+      {
+        current_node = current_node->Get_Sub_Node(name);
+      }
+      current_node->Call();
+    }
+
+    void ProfileManager::Stop_Profile(void)
+    {
+      int threadIndex = GetCurrentThreadIndex2();
+      if ((threadIndex < 0) || threadIndex >= PROFILER_MAX_THREAD_COUNT)
+        return;
+
+      // Return will indicate whether we should back up to our parent (we may
+      // be profiling a recursive function)
+      auto& current_node = this->dataPtr->gCurrentNodes[threadIndex];
+      if (current_node->Return())
+      {
+        current_node = current_node->Get_Parent();
+      }
+    }
+
+    void ProfileManager::Reset(void)
+    {
+      int threadIndex = GetCurrentThreadIndex2();
+      if ((threadIndex < 0) || threadIndex >= PROFILER_MAX_THREAD_COUNT)
         return;
       this->dataPtr->gRoots[threadIndex].Reset();
       this->dataPtr->gRoots[threadIndex].Call();
@@ -61,32 +80,33 @@ namespace gazebo {
       this->dataPtr->ResetTime.SetToWallTime();
     }
 
-    void ProfileManager::Increment_Frame_Counter( void )
+    void ProfileManager::Increment_Frame_Counter(void)
     {
       this->dataPtr->FrameCounter++;
     }
 
-    int ProfileManager::Get_Frame_Count_Since_Reset( void )  
-    { 
-      return this->dataPtr->FrameCounter; 
+    int ProfileManager::Get_Frame_Count_Since_Reset(void)
+    {
+      return this->dataPtr->FrameCounter;
     }
 
-    float ProfileManager::Get_Time_Since_Reset( void )
+    float ProfileManager::Get_Time_Since_Reset(void)
     {
       auto cur_time = gazebo::common::Time::GetWallTime();
       auto delta_t = cur_time - this->dataPtr->ResetTime;
-      return delta_t.Float() * 1000; 
+      return delta_t.Float() * 1000;
     }
 
-    ProfileIterator *	ProfileManager::Get_Iterator( void )
-    { 
+    ProfileIterator * ProfileManager::Get_Iterator(void)
+    {
         int threadIndex = GetCurrentThreadIndex2();
-        if ((threadIndex<0) || threadIndex >= PROFILER_MAX_THREAD_COUNT)
+        if ((threadIndex < 0) || threadIndex >= PROFILER_MAX_THREAD_COUNT)
           return 0;
-        return new ProfileIterator( &this->dataPtr->gRoots[threadIndex]); 
+        return new ProfileIterator(&this->dataPtr->gRoots[threadIndex]);
     }
 
-    void ProfileManager::Release_Iterator(ProfileIterator* iterator) {
+    void ProfileManager::Release_Iterator(ProfileIterator* iterator)
+    {
       delete iterator;
     }
 
@@ -97,27 +117,27 @@ namespace gazebo {
       if (profileIterator->Is_Done())
         return;
 
-      float accumulated_time=0;
+      float accumulated_time = 0;
 
-      float parent_time;
-      if (profileIterator->Is_Root()) {
-        parent_time = Get_Time_Since_Reset();
-      } else {
-        parent_time = profileIterator->Get_Current_Parent_Total_Time();
-      }
+      float parent_time = profileIterator->Is_Root() ?
+        Get_Time_Since_Reset() :
+        profileIterator->Get_Current_Parent_Total_Time();
+
+      auto space = [](int spacing){
+        for (int i = 0; i < spacing; i++) {
+          printf(".");
+        }
+      };
 
       int frames_since_reset = Get_Frame_Count_Since_Reset();
-      for (int i=0;i<spacing;i++)	{
-        printf(".");
-      }
 
+      space(spacing);
       printf("----------------------------------\n");
+      space(spacing);
 
-      for (int i=0;i<spacing;i++)	printf(".");
-      printf("Profiling: %s (total running time: %.3f ms) ---\n",	profileIterator->Get_Current_Parent_Name(), parent_time );
+      printf("Profiling: %s (total running time: %.3f ms) ---\n", profileIterator->Get_Current_Parent_Name(), parent_time);
+
       float totalTime = 0.f;
-
-
       int numChildren = 0;
 
       for (int i = 0; !profileIterator->Is_Done(); i++,profileIterator->Next())
@@ -127,15 +147,19 @@ namespace gazebo {
         accumulated_time += current_total_time;
         float fraction = fabsf(parent_time) > 1e-9 ? (current_total_time / parent_time) * 100 : 0.f;
 
-        for (int j=0;j<spacing;j++)	{
-          printf(".");
-        }
-        printf("%d -- %s (%.2f %%) :: %.3f ms / frame (%d calls)\n",i, profileIterator->Get_Current_Name(), fraction,(current_total_time / (double)frames_since_reset),profileIterator->Get_Current_Total_Calls());
+        space(spacing);
+
+        const char* name = profileIterator->Get_Current_Name();
+        const float time_per_frame = (current_total_time / (double)frames_since_reset);
+        const int total_calls = profileIterator->Get_Current_Total_Calls();
+
+        printf("%d -- %s (%.2f %%) :: %.3f ms / frame (%d calls)\n", i, name, fraction, time_per_frame, total_calls);
         totalTime += current_total_time;
         //recurse into children
       }
 
-      for (int i=0;i<spacing;i++)	printf(".");
+      space(spacing);
+
       printf("%s (%.3f %%) :: %.3f ms\n", "Unaccounted:", fabsf(parent_time) > 1e-9 ? ((parent_time - accumulated_time) / parent_time) * 100 : 0.f, parent_time - accumulated_time);
 
       for (int i=0;i<numChildren;i++)
@@ -146,13 +170,12 @@ namespace gazebo {
       }
     }
 
-    void	ProfileManager::dumpAll()
+    void ProfileManager::dumpAll()
     {
       ProfileIterator* profileIterator = 0;
       profileIterator = ProfileManager::Get_Iterator();
-      dumpRecursive(profileIterator,0);
+      dumpRecursive(profileIterator, 0);
       ProfileManager::Release_Iterator(profileIterator);
     }
-
   }
 }
--- gazebo/util/ProfileManager.hh
+++ gazebo/util/ProfileManager.hh
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2018 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
 #ifndef _GAZEBO_UTIL_PROFILEMANAGER_HH_
 #define _GAZEBO_UTIL_PROFILEMANAGER_HH_
 
@@ -12,22 +29,21 @@ namespace gazebo {
 
     ///The Manager for the Profile system
     class GZ_UTIL_VISIBLE ProfileManager :
-      public SingletonT<ProfileManager> 
+      public SingletonT<ProfileManager>
     {
       private: ProfileManager();
       private: virtual ~ProfileManager();
-      public: void Start_Profile( const char * name );
-      public: void Stop_Profile( void );
 
-      public: void CleanupMemory(void);
-      public: void Reset( void );
-      public: void Increment_Frame_Counter( void );
-      public: int Get_Frame_Count_Since_Reset( void );
-      public: float Get_Time_Since_Reset( void );
+      public: void Start_Profile(const char * name);
+      public: void Stop_Profile(void);
 
-      public: ProfileIterator * Get_Iterator( void ); 
-      public: void Release_Iterator( ProfileIterator * iterator );
+      public: void Reset(void);
+      public: void Increment_Frame_Counter(void);
+      public: int Get_Frame_Count_Since_Reset(void);
+      public: float Get_Time_Since_Reset(void);
 
+      public: ProfileIterator * Get_Iterator(void);
+      public: void Release_Iterator(ProfileIterator * iterator);
       public: void dumpRecursive(ProfileIterator* profileIterator, int spacing);
       public: void dumpAll();
 
