5e3cef44ce76bea5bd00aee785deed26e17d8b24
Tetsuya Idota <tidota@openrobotics.org>
2018-06-05 16:07:03 -0700

    Encapsulate data in a class

--- test/integration/flash_light_plugin.cc
+++ test/integration/flash_light_plugin.cc
@@ -15,18 +15,18 @@
  *
 */
 
-#include <string>
-#include <sstream>
 #include <cmath>
+#include <sstream>
+#include <string>
 
 #include "gazebo/test/ServerFixture.hh"
 
 using namespace gazebo;
-class FlashLightPluginTest : public ServerFixture
-{
-};
 
-// information to record for the lights in the enviornment
+// Information to record for the lights in the enviornment.
+// duration: The actual duration time of the flashlight.
+// interval: The actual interval time of the flashlight.
+// lastUpdate: The time at which the light was last checked.
 struct RecordInfo
 {
   double duration;
@@ -34,29 +34,45 @@ struct RecordInfo
   common::Time lastUpdate;
 };
 
-struct RecordInfo flashLight[4];
-bool called;
-
-void InitRec()
+class FlashLightPluginTest : public ServerFixture
 {
-  common::Time currentTime = physics::get_world()->SimTime();
-  flashLight[0].duration = -1;
-  flashLight[1].duration = -1;
-  flashLight[2].duration = -1;
-  flashLight[3].duration = -1;
-  flashLight[0].interval = -1;
-  flashLight[1].interval = -1;
-  flashLight[2].interval = -1;
-  flashLight[3].interval = -1;
-  flashLight[0].lastUpdate = currentTime;
-  flashLight[1].lastUpdate = currentTime;
-  flashLight[2].lastUpdate = currentTime;
-  flashLight[3].lastUpdate = currentTime;
-}
+  // Constructor.
+  public: FlashLightPluginTest(){}
+
+  // Destructor.
+  public: ~FlashLightPluginTest(){}
+
+  // Callback for light/modify topic
+  public: void lightCb(ConstLightPtr &_msg);
+
+  // Initialize all records.
+  protected: void InitRec();
+
+  // Check the records.
+  protected: void CheckRec();
+
+  // Four units of records
+  // (since there are supposed to be four lights in the environment.)
+  protected: struct RecordInfo flashLight[4];
+
+  // True if the callback function has been called.
+  protected: bool called;
+
+  // The time to start recording.
+  protected: common::Time startTime;
+
+  // The time to end recording.
+  protected: common::Time endTime;
+
+  // Mutex to protect data from racing.
+  protected: std::mutex mutex;
+
+  // Gazebo transport subscriber.
+  protected: transport::SubscriberPtr sub;
+};
 
 //////////////////////////////////////////////////
-// Callback for light/modify topic
-void lightCb(ConstLightPtr &_msg)
+void FlashLightPluginTest::lightCb(ConstLightPtr &_msg)
 {
   // Determine which light is to be updated
   int indx;
@@ -76,29 +92,86 @@ void lightCb(ConstLightPtr &_msg)
   // Get the current time
   common::Time currentTime = physics::get_world()->SimTime();
 
+
   // Update to flash
+  std::lock_guard<std::mutex> lk(this->mutex);
   if (_msg->range() > 0)
   {
-    flashLight[indx].interval
-      = currentTime.Double() - flashLight[indx].lastUpdate.Double();
+    this->flashLight[indx].interval
+      = currentTime.Double() - this->flashLight[indx].lastUpdate.Double();
   }
   // Update to dim
   else
   {
-    flashLight[indx].duration
-      = currentTime.Double() - flashLight[indx].lastUpdate.Double();
+    this->flashLight[indx].duration
+      = currentTime.Double() - this->flashLight[indx].lastUpdate.Double();
   }
 
   // Update the last update time
-  flashLight[indx].lastUpdate = currentTime;
+  this->flashLight[indx].lastUpdate = currentTime;
 
-  called = true;
+  this->called = true;
 }
 
 //////////////////////////////////////////////////
-TEST_F(FlashLightPluginTest, Blinking)
+void FlashLightPluginTest::InitRec()
 {
-  this->Load("worlds/flash_light_plugin_demo.world", true);
+  std::lock_guard<std::mutex> lk(this->mutex);
+  this->startTime = physics::get_world()->SimTime();
+  this->flashLight[0].duration = -1;
+  this->flashLight[1].duration = -1;
+  this->flashLight[2].duration = -1;
+  this->flashLight[3].duration = -1;
+  this->flashLight[0].interval = -1;
+  this->flashLight[1].interval = -1;
+  this->flashLight[2].interval = -1;
+  this->flashLight[3].interval = -1;
+  this->flashLight[0].lastUpdate = this->startTime;
+  this->flashLight[1].lastUpdate = this->startTime;
+  this->flashLight[2].lastUpdate = this->startTime;
+  this->flashLight[3].lastUpdate = this->startTime;
+  this->called = false;
+}
+
+//////////////////////////////////////////////////
+void FlashLightPluginTest::CheckRec()
+{
+  std::lock_guard<std::mutex> lk(this->mutex);
+  this->endTime = physics::get_world()->SimTime();
+  double lastUpdate0 = this->flashLight[0].lastUpdate.Double();
+  double lastUpdate1 = this->flashLight[1].lastUpdate.Double();
+  double lastUpdate2 = this->flashLight[2].lastUpdate.Double();
+  double lastUpdate3 = this->flashLight[3].lastUpdate.Double();
+
+  // Make sure if the function was called
+  EXPECT_TRUE(this->called);
+
+  // Verify only the supposed lights are updated
+  // NOTE: Taking some errors caused by callback functions into consideration.
+  //       Here, it is considered to be passed if the error is less than or
+  //       equal to 0.01 sec.
+  // NOTE: The first and second must have been updated within their phases.
+  EXPECT_GE(lastUpdate0, this->endTime.Double() - 0.41);
+  EXPECT_GE(lastUpdate1, this->endTime.Double() - 0.06);
+  // NOTE: It is supposed to stop updating the third and forth lights just
+  //       after the beginning.
+  EXPECT_LE(lastUpdate2, this->startTime.Double() + 0.01);
+  EXPECT_LE(lastUpdate3, this->startTime.Double() + 0.01);
+
+  // Verify the lights blinking at the supposed duration and interval.
+  // NOTE: Taking some errors caused by callback functions into consideration.
+  //       Here, it is considered to be passed if the error is less than or
+  //       equal to 0.01 sec.
+  EXPECT_NEAR(this->flashLight[0].duration, 0.1, 0.01);
+  EXPECT_NEAR(this->flashLight[0].interval, 0.4, 0.01);
+  EXPECT_NEAR(this->flashLight[1].duration, 0.05, 0.01);
+  EXPECT_NEAR(this->flashLight[1].interval, 0.05, 0.01);
+}
+
+//////////////////////////////////////////////////
+TEST_F(FlashLightPluginTest, blinkingCheck)
+{
+  this->Load("worlds/flash_light_plugin_demo.world");
   auto world = physics::get_world();
   ASSERT_NE(nullptr, world);
 
@@ -107,54 +180,52 @@ TEST_F(FlashLightPluginTest, Blinking)
   ASSERT_NE(nullptr, model);
 
   // Initialize the time in the records
-  InitRec();
-
-  // Subscribe to plugin notifications
-  called = false;
-  transport::SubscriberPtr sceneSub
-    = this->node->Subscribe("~/light/modify", &lightCb);
+  this->InitRec();
 
   // Let the plugin blink the lights for a while
-  common::Time sTime = world->SimTime();
-  flashLight[0].lastUpdate = sTime;
-  flashLight[1].lastUpdate = sTime;
-  flashLight[2].lastUpdate = sTime;
-  flashLight[3].lastUpdate = sTime;
-  world->Step(5000);
-  common::Time eTime = world->SimTime();
-  double lastUpdate0 = flashLight[0].lastUpdate.Double();
-  double lastUpdate1 = flashLight[1].lastUpdate.Double();
-  double lastUpdate2 = flashLight[2].lastUpdate.Double();
-  double lastUpdate3 = flashLight[3].lastUpdate.Double();
+  common::Time::MSleep(1000);
 
-  // Make sure if the function was called
-  EXPECT_TRUE(called);
+  {
+    std::lock_guard<std::mutex> lk(this->mutex);
+    // Make sure that the function has never been called
+    EXPECT_FALSE(this->called);
+    // Make sure that they has never been updated.
+    EXPECT_EQ(
+      this->flashLight[0].lastUpdate.Double(), this->startTime.Double());
+    EXPECT_EQ(
+      this->flashLight[1].lastUpdate.Double(), this->startTime.Double());
+    EXPECT_EQ(
+      this->flashLight[2].lastUpdate.Double(), this->startTime.Double());
+    EXPECT_EQ(
+      this->flashLight[3].lastUpdate.Double(), this->startTime.Double());
+  }
 
-  // Verify only the supposed lights are updated
-  // NOTE: Taking some errors caused by callback functions into consideration.
-  //       Here, it is considered to be passed if the error is less than or
-  //       equal to 0.01 sec.
-  // NOTE: The first and second must have been updated within their phases.
-  EXPECT_TRUE(fabs(lastUpdate0 - eTime.Double()) <= 0.41);
-  EXPECT_TRUE(fabs(lastUpdate1 - eTime.Double()) <= 0.06);
-  // NOTE: It is supposed to stop updating the third and forth lights just
-  //       after the beginning.
-  EXPECT_TRUE(fabs(lastUpdate2 - sTime.Double()) <= 0.01);
-  EXPECT_TRUE(fabs(lastUpdate3 - sTime.Double()) <= 0.01);
+  // Subscribe to plugin notifications
+  this->sub
+    = this->node->Subscribe("~/light/modify", &FlashLightPluginTest::lightCb,
+                            dynamic_cast<FlashLightPluginTest*>(this));
 
-  // Verify the lights blinking at the supposed duration and interval.
-  // NOTE: Taking some errors caused by callback functions into consideration.
-  //       Here, it is considered to be passed if the error is less than or
-  //       equal to 0.01 sec.
-  EXPECT_TRUE(fabs(flashLight[0].duration - 0.1) <= 0.01);
-  EXPECT_TRUE(fabs(flashLight[0].interval - 0.4) <= 0.01);
-  EXPECT_TRUE(fabs(flashLight[1].duration - 0.05) <= 0.01);
-  EXPECT_TRUE(fabs(flashLight[1].interval - 0.05) <= 0.01);
+  // Initialize the records
+  this->InitRec();
+
+  // Let the plugin blink the lights for a while
+  common::Time::MSleep(2000);
+
+  // Check the records
+  this->CheckRec();
+
+  // The second trial.
+  this->InitRec();
+
+  common::Time::MSleep(2000);
+
+  this->CheckRec();
 }
 
 //////////////////////////////////////////////////
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
+
   return RUN_ALL_TESTS();
 }
