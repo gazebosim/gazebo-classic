2acc3a385a2eae44cf0fc42cefdd29c352b5bdf8
Ian Chen <ichen@osrfoundation.org>
2018-11-06 11:50:18 -0800

    disable skyx in ssao plugin

--- plugins/AmbientOcclusionVisualPlugin.cc
+++ plugins/AmbientOcclusionVisualPlugin.cc
@@ -62,6 +62,16 @@ namespace gazebo
         Ogre::Material *_originalMaterial, uint16_t /*_lodIndex*/,
         const Ogre::Renderable * /*_rend*/)
     {
+      // skip skyx
+      std::string matStr =_originalMaterial->getName();
+      if (matStr.find("SkyX") != std::string::npos)
+      {
+        auto tech = _originalMaterial->createTechnique();
+        tech->setSchemeName(_schemeName);
+        tech->removeAllPasses();
+        return tech;
+      }
+
       // ignore transparent / semi-tranparent materials with alpha rejection set
       Ogre::Technique *origTech = _originalMaterial->getTechnique(0);
       if (origTech)
