f13f44cbdf0560d4890bb9b50bc1442fe88af647
peci1 <peci1@seznam.cz>
2018-12-14 01:38:58 +0100

    Nitpicks in tests.

--- test/plugins/PluginInterfaceTest.cc
+++ test/plugins/PluginInterfaceTest.cc
@@ -89,20 +89,20 @@ void PluginInterfaceTest::TestLoadParam(sdf::ElementPtr &_sdf) const
     float f = 3.14f;
 
     LoadParam(_sdf, "float", f, 0.0f);
-    EXPECT_NEAR(f, 3.14f, 0.0f);
+    EXPECT_FLOAT_EQ(f, 3.14f);
 
     LoadParam(_sdf, "float_nonexistent", f, 1.0f);
-    EXPECT_NEAR(f, 1.0f, 0.0f);
+    EXPECT_FLOAT_EQ(f, 1.0f);
   }
 
   {
     double d = 3.14;
 
     LoadParam(_sdf, "float", d, 0.0);
-    EXPECT_NEAR(d, 3.14, 0.0);
+    EXPECT_DOUBLE_EQ(d, 3.14);
 
     LoadParam(_sdf, "float_nonexistent", d, 1.0);
-    EXPECT_NEAR(d, 1.0, 0.0);
+    EXPECT_DOUBLE_EQ(d, 1.0);
   }
 
   {
