4f0519feca763ac807d4cfac25e52a15fa1db9e4
Sean Yen <seanyen@microsoft.com>
2019-01-29 23:56:26 -0800

    fix

--- gazebo/sensors/SensorManager.hh
+++ gazebo/sensors/SensorManager.hh
@@ -32,7 +32,7 @@
 #include "gazebo/util/system.hh"
 
 /// \brief Explicit instantiation for typed SingletonT.
-GZ_SINGLETON_DECLARE(GZ_SENSORS_VISIBLE, gazebo::sensors::FuelModelDatabase)
+GZ_SINGLETON_DECLARE(GZ_SENSORS_VISIBLE, gazebo::sensors::SensorManager)
 
 namespace gazebo
 {
