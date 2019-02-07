4a7da7528c9fbd1aa9af60b522fdc018c8c7952b
Koon Peng <koonpeng@openrobotics.org>
2019-02-07 10:07:40 +0800

    remove use of exceptions

--- gazebo/physics/Actor.cc
+++ gazebo/physics/Actor.cc
@@ -658,11 +658,11 @@ void Actor::Update()
   this->lastTraj = tinfo->id;
 
   ignition::math::Matrix4d rootTrans = ignition::math::Matrix4d::Identity;
-  try
+  auto iter = frame.find(skelMap[this->skeleton->GetRootNode()->GetName()]);
+  if (iter != frame.end())
   {
-    rootTrans = frame.at(skelMap[this->skeleton->GetRootNode()->GetName()]);
+    rootTrans = frame[skelMap[this->skeleton->GetRootNode()->GetName()]];
   }
-  catch (const std::out_of_range&) { }
 
   ignition::math::Vector3d rootPos = rootTrans.Translation();
   ignition::math::Quaterniond rootRot = rootTrans.Rotation();
