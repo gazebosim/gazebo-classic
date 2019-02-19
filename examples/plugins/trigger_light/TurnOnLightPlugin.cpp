d1adb937e0c3096628000e722e7153e8ccc9f569
Michael X. Grey <grey@openrobotics.org>
2019-02-19 17:39:37 +0800

    Update the trigger_light plugin example to use ignition-transport

--- examples/plugins/trigger_light/TurnOnLightPlugin.cpp
+++ examples/plugins/trigger_light/TurnOnLightPlugin.cpp
@@ -19,9 +19,9 @@
 /// Doxygen comments and PIMPL are omitted to reduce the amount of text.
 
 #include "gazebo/common/Plugin.hh"
-#include "gazebo/msgs/msgs.hh"
 #include "gazebo/physics/World.hh"
-#include "gazebo/transport/Node.hh"
+#include "ignition/msgs.hh"
+#include "ignition/transport/Node.hh"
 
 namespace gazebo
 {
@@ -30,29 +30,29 @@ namespace gazebo
     public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf) override
     {
       gzmsg << "Loading Example plugin\n";
-      // Transport initialization
-      this->gzNode = transport::NodePtr(new transport::Node());
-      this->gzNode->Init();
 
       // Subscribe to ContainPlugin output
-      std::string topic("~/contain_example/contain");
-      this->containSub = this->gzNode->Subscribe(
-        topic, &TurnOnLightPlugin::OnContainPluginMsg, this);
+      std::string topic("contain_example/contain");
+      std::function<void(const ignition::msgs::Boolean &)> cb =
+          [=](const ignition::msgs::Boolean &_msg){
+        TurnOnLightPlugin::OnContainPluginMsg(_msg);
+      };
+      const bool containSub = this->node.Subscribe(topic, cb);
       if (!containSub)
       {
         gzerr << "Failed to subscribe to [" << topic << "]\n";
       }
 
       // Make a publisher for the light topic
-      this->lightPub = this->gzNode->Advertise<msgs::Light>("~/light/modify");
+      this->lightPub = this->node.Advertise<msgs::Light>("light/modify");
     }
 
-    public: void OnContainPluginMsg(ConstIntPtr &_msg)
+    public: void OnContainPluginMsg(const ignition::msgs::Boolean &_msg)
     {
       msgs::Light lightMsg;
       lightMsg.set_name("post_light");
       // Turn light on when the entity enters the box, and off when it leaves
-      if (1 == _msg->data())
+      if (_msg.data())
       {
         gzmsg << "Turning on light\n";
         lightMsg.set_range(15.0);
@@ -62,12 +62,11 @@ namespace gazebo
         gzmsg << "Turning off light\n";
         lightMsg.set_range(0.0);
       }
-      this->lightPub->Publish(lightMsg);
+      this->lightPub.Publish(lightMsg);
     }
 
-    private: transport::NodePtr gzNode;
-    private: transport::SubscriberPtr containSub;
-    private: transport::PublisherPtr lightPub;
+    private: ignition::transport::Node node;
+    private: ignition::transport::Node::Publisher lightPub;
   };
   GZ_REGISTER_WORLD_PLUGIN(TurnOnLightPlugin);
 }  // namespace gazebo
