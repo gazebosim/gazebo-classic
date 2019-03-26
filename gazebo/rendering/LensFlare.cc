b454ef741b8d77b5bdfe396b9b59ccc0ecfab63f
Terry Welsh <terence.m.welsh@nasa.gov>
2019-03-26 14:13:05 -0700

    Lens flare uses light's visual's world pose instead of its local rotation.

--- gazebo/rendering/LensFlare.cc
+++ gazebo/rendering/LensFlare.cc
@@ -60,10 +60,7 @@ namespace gazebo
       /// \param[in] _light Pointer to directional light
       public: void SetLight(LightPtr _light)
       {
-        this->dir = ignition::math::Quaterniond(_light->Rotation()) *
-            _light->Direction();
-        // set light world pos to be far away
-        this->lightWorldPos = -this->dir * 100000.0;
+        this->light = _light;
       }
 
       /// \brief Set the scale of lens flare.
@@ -115,6 +112,19 @@ namespace gazebo
         params->setNamedConstant("lightPos", Conversions::Convert(pos));
         params->setNamedConstant("scale",
             static_cast<Ogre::Real>(lensFlareScale));
+
+        // use light's world position for lens flare position
+        if (this->light->Type() == "directional")
+        {
+          // Directional lights misuse position as a direction.
+          // The large multiplier is for occlusion testing and assumes the light
+          // is very far away. Larger values cause the light to disappear on
+          // some frames for some unknown reason.
+          this->lightWorldPos = -(this->light->WorldPose().Rot() *
+                                  light->Direction()) * 100000.0;
+        }
+        else
+          this->lightWorldPos = this->light->WorldPose().Pos();
       }
 
       /// \brief Get the lens flare position and scale for a normal camera
@@ -305,6 +315,9 @@ namespace gazebo
             static_cast<double>(rays);
         return s;
       };
+
+      /// \brief Pointer to light
+      private: LightPtr light;
 
       /// \brief Pointer to camera
       private: CameraPtr camera;
