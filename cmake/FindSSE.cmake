3b44bb6fa83d58d822c2f175184b0a9f37922891
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-06-08 17:13:04 +0200

    Fix SSE check for ARM

--- cmake/FindSSE.cmake
+++ cmake/FindSSE.cmake
@@ -2,7 +2,7 @@
 # the project is compiled.
 
 message(WARNING "CMAKE_SYSTEM_PROCESSOR ${CMAKE_SYSTEM_PROCESSOR}")
-IF (${CMAKE_SYSTEM_PROCESSOR} MATCHES "arm")
+IF (${CMAKE_SYSTEM_PROCESSOR} MATCHES "arm" OR ${CMAKE_SYSTEM_PROCESSOR} MATCHES "aarch64")
    set(SSE2_FOUND   false CACHE BOOL "SSE2 available on host")
    set(SSE3_FOUND   false CACHE BOOL "SSE3 available on host")
    set(SSSE3_FOUND  false CACHE BOOL "SSSE3 available on host")
