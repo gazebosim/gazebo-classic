2c72414de1971af9185b7ab76e42fc5559341efa
ahcorde <ahcorde@gmail.com>
2020-07-03 16:39:15 +0200

    Improved error message
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- gazebo/rendering/Visual.cc
+++ gazebo/rendering/Visual.cc
@@ -800,7 +800,8 @@ void Visual::SetScale(const math::Vector3 &_scale)
     this->dataPtr->sceneNode->setScale(
         Conversions::Convert(this->dataPtr->scale));
   } else {
-    gzerr << Name() << " Scale contains NaNs. Collisions may not visualize properly." << std::endl;
+    gzerr << Name() << ": Size of the collision contains one or several zeros." <<
+      " Collisions may not visualize properly." << std::endl;
   }
   // Scale selection object in case we have one attached. Other children were
   // scaled from UpdateGeomSize
