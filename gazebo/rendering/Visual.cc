c27d029c0c5e9d49756f14e73f708a8f51190c8b
ahcorde <ahcorde@gmail.com>
2020-07-13 10:40:36 +0200

    Fixed else brackets
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- gazebo/rendering/Visual.cc
+++ gazebo/rendering/Visual.cc
@@ -799,7 +799,9 @@ void Visual::SetScale(const math::Vector3 &_scale)
   {
     this->dataPtr->sceneNode->setScale(
         Conversions::Convert(this->dataPtr->scale));
-  } else {
+  }
+  else
+  {
     gzerr << GetName() << ": Size of the collision contains one or several zeros." <<
       " Collisions may not visualize properly." << std::endl;
   }
