dd1e89d17353477e190eb89fd56b683b7e44ed67
ahcorde <ahcorde@gmail.com>
2020-07-13 10:38:43 +0200

    Fixed else brackets introduced in #2770
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- gazebo/rendering/Visual.cc
+++ gazebo/rendering/Visual.cc
@@ -817,7 +817,9 @@ void Visual::SetScale(const ignition::math::Vector3d &_scale)
   {
     this->dataPtr->sceneNode->setScale(
         Conversions::Convert(this->dataPtr->scale));
-  } else {
+  }
+  else
+  {
     gzerr << Name() << ": Size of the collision contains one or several zeros." <<
       " Collisions may not visualize properly." << std::endl;
   }
