af30f7414596026b8f62a09685ea11d0d7c60c78
Sean Yen <seanyen@microsoft.com>
2020-08-10 18:01:58 -0700

    [Gazebo9] More enhancement for Windows build (#2789)

--- gazebo/common/Material.cc
+++ gazebo/common/Material.cc
@@ -98,6 +98,10 @@ void Material::SetTextureImage(const std::string &_tex,
       }
     }
   }
+
+  // normalize the path
+  this->texImage = boost::filesystem::path(this->texImage)
+    .make_preferred().string();
 }
 
 //////////////////////////////////////////////////
