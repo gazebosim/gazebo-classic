718b6a859df911252621d03c11116478caf0ed7e
Ian Chen <ichen@osrfoundation.org>
2021-04-14 15:10:55 -0700

    extend spot light shadows demo with more spot lights
    Signed-off-by: Ian Chen <ichen@osrfoundation.org>

--- media/materials/programs/spotlight_shadow_demo_fp.glsl
+++ media/materials/programs/spotlight_shadow_demo_fp.glsl
@@ -1,7 +1,30 @@
 #version 120
 
 uniform sampler2D shadowMap0;
+uniform sampler2D shadowMap1;
+uniform sampler2D shadowMap2;
+uniform sampler2D shadowMap3;
+uniform sampler2D shadowMap4;
+uniform sampler2D shadowMap5;
+uniform sampler2D shadowMap6;
+uniform sampler2D shadowMap7;
+uniform sampler2D shadowMap8;
+uniform sampler2D shadowMap9;
+uniform sampler2D shadowMap10;
+uniform sampler2D shadowMap11;
+
 varying vec4 lightSpacePos0;
+varying vec4 lightSpacePos1;
+varying vec4 lightSpacePos2;
+varying vec4 lightSpacePos3;
+varying vec4 lightSpacePos4;
+varying vec4 lightSpacePos5;
+varying vec4 lightSpacePos6;
+varying vec4 lightSpacePos7;
+varying vec4 lightSpacePos8;
+varying vec4 lightSpacePos9;
+varying vec4 lightSpacePos10;
+varying vec4 lightSpacePos11;
 
 varying vec4 worldPos;
 varying vec4 worldViewPos;
@@ -35,7 +58,19 @@ void main()
   vec4 outputCol = vec4(1.0, 0.0, 0.0, 1.0);
 
   // grey shadows
-  f += ShadowSimple(shadowMap0, lightSpacePos0);
+
+  // shadowMap0-2 and lightSpacePos0-2 are reserved for directional light
+
+  // render spot light shadows
+  f += ShadowSimple(shadowMap3, lightSpacePos3);
+  f += ShadowSimple(shadowMap4, lightSpacePos4);
+  f += ShadowSimple(shadowMap5, lightSpacePos5);
+  f += ShadowSimple(shadowMap6, lightSpacePos6);
+  f += ShadowSimple(shadowMap7, lightSpacePos7);
+  f += ShadowSimple(shadowMap8, lightSpacePos8);
+  f += ShadowSimple(shadowMap9, lightSpacePos9);
+  f += ShadowSimple(shadowMap10, lightSpacePos10);
+
   f = clamp(f, 0.0f, 1.0f);
   if (f > 0.0f)
     outputCol = vec4(0.2, 0.2, 0.2, 1.0);
--- media/materials/programs/spotlight_shadow_demo_vp.glsl
+++ media/materials/programs/spotlight_shadow_demo_vp.glsl
@@ -4,11 +4,32 @@ uniform mat4 worldMatrix;
 uniform mat4 worldViewMatrix;
 uniform mat4 viewProjMatrix;
 
-uniform mat4 texViewProjMatrix0;
-varying vec4 lightSpacePos0;
-
 varying vec4 worldPos;
 varying vec4 worldViewPos;
+
+uniform mat4 texViewProjMatrix0;
+uniform mat4 texViewProjMatrix1;
+uniform mat4 texViewProjMatrix2;
+uniform mat4 texViewProjMatrix3;
+uniform mat4 texViewProjMatrix4;
+uniform mat4 texViewProjMatrix5;
+uniform mat4 texViewProjMatrix6;
+uniform mat4 texViewProjMatrix7;
+uniform mat4 texViewProjMatrix8;
+uniform mat4 texViewProjMatrix9;
+uniform mat4 texViewProjMatrix10;
+
+varying vec4 lightSpacePos0;
+varying vec4 lightSpacePos1;
+varying vec4 lightSpacePos2;
+varying vec4 lightSpacePos3;
+varying vec4 lightSpacePos4;
+varying vec4 lightSpacePos5;
+varying vec4 lightSpacePos6;
+varying vec4 lightSpacePos7;
+varying vec4 lightSpacePos8;
+varying vec4 lightSpacePos9;
+varying vec4 lightSpacePos10;
 
 void main()
 {
@@ -18,5 +39,14 @@ void main()
   worldViewPos = worldViewMatrix * gl_Vertex;
 
   lightSpacePos0 = texViewProjMatrix0 * worldPos;
+  lightSpacePos1 = texViewProjMatrix1 * worldPos;
+  lightSpacePos2 = texViewProjMatrix2 * worldPos;
+  lightSpacePos3 = texViewProjMatrix3 * worldPos;
+  lightSpacePos4 = texViewProjMatrix4 * worldPos;
+  lightSpacePos5 = texViewProjMatrix5 * worldPos;
+  lightSpacePos6 = texViewProjMatrix6 * worldPos;
+  lightSpacePos7 = texViewProjMatrix7 * worldPos;
+  lightSpacePos8 = texViewProjMatrix8 * worldPos;
+  lightSpacePos9 = texViewProjMatrix9 * worldPos;
+  lightSpacePos10 = texViewProjMatrix10 * worldPos;
 }
-
