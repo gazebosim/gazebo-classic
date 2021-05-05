7b87882efa3e4e208c062f1db155e005d0a7aeb4
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-05-05 16:21:42 +0200

    Backport commit to copyright assigment from #2978

--- test/regression/2875_connect_pub_to_sub_crash.cc
+++ test/regression/2875_connect_pub_to_sub_crash.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Amazon.com Inc or its affiliates. All rights reserved.
+ * Copyright (C) 2021 Open Source Robotics Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
