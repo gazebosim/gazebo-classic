f951b0003b107f24c265efb79578c50ce98997b7
William Lew <WilliamMilesLew@gmail.com>
2021-06-07 09:28:14 -0700

    Added world matrix comparison to remove heightmap self-shadows
    Signed-off-by: William Lew <WilliamMilesLew@gmail.com>

--- media/materials/programs/shadow_caster_fp.glsl
+++ media/materials/programs/shadow_caster_fp.glsl
@@ -1,4 +1,5 @@
 uniform vec4 camera_position;
+uniform mat4 world_matrix;
 
 // uniform vec4 depth_range;
 varying vec4 vertex_depth;
@@ -10,7 +11,13 @@ void main()
   // Linear
   // float depth = (vertex_depth.z - depth_range.x) / depth_range.w;
 
-  if (length(camera_position.xyz) == 0) {
+  vec4 magicVector1 = vec4(1.0, 0.0, 0.0, 0.0);
+  vec4 magicVector2 = vec4(0.0, 1.0, 0.0, 0.0);
+  vec4 magicVector3 = vec4(0.0, 0.0, 1.0, 0.0);
+
+  bool matchingVectors = (magicVector1 == world_matrix[0] && magicVector2 == world_matrix[1] && magicVector3 == world_matrix[2]);
+
+  if (length(camera_position.xyz) == 0 && matchingVectors) {
     depth = 1.0;
   }
 
