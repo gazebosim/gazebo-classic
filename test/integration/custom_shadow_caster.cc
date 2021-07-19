64a1bdb72bb2eef05a531c7876f8dd9d901da5f1
William Lew <WilliamMilesLew@gmail.com>
2021-07-19 08:41:53 -0700

    Added integration test for custom shadow caster materials
    Signed-off-by: William Lew <WilliamMilesLew@gmail.com>

--- test/integration/custom_shadow_caster.cc
+++ test/integration/custom_shadow_caster.cc
@@ -22,11 +22,129 @@ class CustomShadowCasterTest : public ServerFixture
 {
 };
 
+std::mutex mutex;
+
+unsigned char* img = NULL;
+int imageCount = 0;
+std::string pixelFormat = "";
+
 /////////////////////////////////////////////////
-// \brief
+void OnNewCameraFrame(int* _imageCounter, unsigned char* _imageDest,
+                  const unsigned char *_image,
+                  unsigned int _width, unsigned int _height,
+                  unsigned int _depth,
+                  const std::string &_format)
+{
+  std::lock_guard<std::mutex> lock(mutex);
+  pixelFormat = _format;
+  memcpy(_imageDest, _image, _width * _height * _depth);
+  *_imageCounter += 1;
+}
+
+/////////////////////////////////////////////////
+// \brief Use the "Gazebo/shadow_caster" material for shadow
+/// casting. Shadows will be enabled from the material.
+TEST_F(CustomShadowCasterTest, DefaultShadowCaster)
+{
+  Load("worlds/default_shadow_caster.world");
+
+  // Make sure the render engine is available.
+  if (rendering::RenderEngine::Instance()->GetRenderPathType() ==
+      rendering::RenderEngine::NONE)
+  {
+    gzerr << "No rendering engine, unable to run camera test\n";
+    return;
+  }
+
+  sensors::CameraSensorPtr sensor =
+      std::dynamic_pointer_cast<sensors::CameraSensor>(
+      sensors::get_sensor("camera_normal"));
+  EXPECT_TRUE(sensor != nullptr);
+
+  unsigned int width  = 320;
+  unsigned int height = 240;
+
+  imageCount = 0;
+  img = new unsigned char[width * height*3];
+  event::ConnectionPtr c =
+    sensor->Camera()->ConnectNewImageFrame(
+        std::bind(&::OnNewCameraFrame, &imageCount, img,
+        std::placeholders::_1, std::placeholders::_2, std::placeholders::_3,
+        std::placeholders::_4, std::placeholders::_5));
+
+  // Get some images
+  while (imageCount < 10)
+  {
+    common::Time::MSleep(10);
+  }
+
+  unsigned int colorSum = 0;
+  for (unsigned int y = 0; y < height; ++y)
+  {
+    for (unsigned int x = 0; x < width*3; x+=3)
+    {
+      unsigned int r = img[(y*width*3) + x];
+      unsigned int g = img[(y*width*3) + x + 1];
+      unsigned int b = img[(y*width*3) + x + 2];
+      colorSum += r + g + b;
+    }
+  }
+
+  EXPECT_GT(18000u, colorSum);
+  EXPECT_LT(14000u, colorSum);
+}
+
+/////////////////////////////////////////////////
+/// \brief Use the "Gazebo/shadow_caster_ignore_heightmap" material for shadow
+/// casting. Shadows will be disabled from the material.
 TEST_F(CustomShadowCasterTest, CustomShadowCaster)
 {
-  Load("worlds/empty.world");
+  Load("worlds/custom_shadow_caster.world");
+
+  // Make sure the render engine is available.
+  if (rendering::RenderEngine::Instance()->GetRenderPathType() ==
+      rendering::RenderEngine::NONE)
+  {
+    gzerr << "No rendering engine, unable to run camera test\n";
+    return;
+  }
+
+  sensors::CameraSensorPtr sensor =
+      std::dynamic_pointer_cast<sensors::CameraSensor>(
+      sensors::get_sensor("camera_normal"));
+  EXPECT_TRUE(sensor != nullptr);
+
+  unsigned int width  = 320;
+  unsigned int height = 240;
+
+  imageCount = 0;
+  img = new unsigned char[width * height*3];
+  event::ConnectionPtr c =
+    sensor->Camera()->ConnectNewImageFrame(
+        std::bind(&::OnNewCameraFrame, &imageCount, img,
+        std::placeholders::_1, std::placeholders::_2, std::placeholders::_3,
+        std::placeholders::_4, std::placeholders::_5));
+
+  // Get some images
+  while (imageCount < 10)
+  {
+    common::Time::MSleep(10);
+  }
+
+  unsigned int colorSum = 0;
+  for (unsigned int y = 0; y < height; ++y)
+  {
+    for (unsigned int x = 0; x < width*3; x+=3)
+    {
+      unsigned int r = img[(y*width*3) + x];
+      unsigned int g = img[(y*width*3) + x + 1];
+      unsigned int b = img[(y*width*3) + x + 2];
+      colorSum += r + g + b;
+    }
+  }
+
+  EXPECT_GT(31000u, colorSum);
+  EXPECT_LT(27000u, colorSum);
 }
 
 /////////////////////////////////////////////////
