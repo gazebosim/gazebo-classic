2d7982c9288916fe0c4a637423cb9916c73be2da
Steve Peters <scpeters@openrobotics.org>
2022-01-11 12:17:17 -0800

    Fix focal builds: use python3 with check_test_ran.py (#3155)
    * Use find_package(Python3) with newer cmake
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- cmake/GazeboTestUtils.cmake
+++ cmake/GazeboTestUtils.cmake
@@ -78,7 +78,7 @@ macro (gz_build_tests)
 
     # Check that the test produced a result and create a failure if it didn't.
     # Guards against crashed and timed out tests.
-    add_test(check_${BINARY_NAME} ${PROJECT_SOURCE_DIR}/tools/check_test_ran.py
+    add_test(check_${BINARY_NAME} ${PYTHON_EXECUTABLE} ${PROJECT_SOURCE_DIR}/tools/check_test_ran.py
       ${CMAKE_BINARY_DIR}/test_results/${BINARY_NAME}.xml)
 
     if(GAZEBO_RUN_VALGRIND_TESTS AND VALGRIND_PROGRAM)
