9442fa686a0f05eba070b05df2400111554d46f0
Jose Luis Rivero <jrivero@osrfoundation.org>
2024-02-21 22:22:49 +0100

    Fix manpage installation when GZ_CLI_EXECUTABLE_NAME is used (#3370)
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- cmake/Man.cmake
+++ cmake/Man.cmake
@@ -14,21 +14,34 @@ if (NOT GZIP)
 else (NOT GZIP)
   message (STATUS "Looking for gzip to generate manpages - found")
 
+  # macro can also be called with a third argument that contains a modified name
+  # of the manpage to be installed.
   macro(roffman _source _section)
+    set(_extra_macro_args ${ARGN})
+    list(LENGTH _extra_macro_args _num_extra_macro_args)
+
+    if (_num_extra_macro_args EQUAL 0)
+      set(_destination _source)
+    elseif(_num_extra_macro_args EQUAL 1)
+      set(_destination ${_extra_macro_args})
+    else()
+      message(FATAL_ERROR "roffman macro called with unexpected number of extra arguments '${ARGN}'")
+    endif()
+
     add_custom_command(
-      OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${_source}.${_section}.gz
+      OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${_destination}.${_section}.gz
       COMMAND ${GZIP} -c ${CMAKE_CURRENT_SOURCE_DIR}/${_source}.${_section}.roff
-        > ${CMAKE_CURRENT_BINARY_DIR}/${_source}.${_section}.gz
+        > ${CMAKE_CURRENT_BINARY_DIR}/${_destination}.${_section}.gz
       DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${_source}.${_section}.roff
     )
 
     set(MANPAGE_TARGET "man-${_source}")
 
     add_custom_target(${MANPAGE_TARGET}
-      DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${_source}.${_section}.gz)
+      DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${_destination}.${_section}.gz)
     add_dependencies(man ${MANPAGE_TARGET})
 
-    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${_source}.${_section}.gz
+    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${_destination}.${_section}.gz
       DESTINATION share/man/man${_section}
     )
   endmacro(roffman _source _section)
